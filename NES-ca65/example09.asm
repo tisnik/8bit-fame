5bf899ae2a3feae857c33e159e24406a9d32c2d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-11 08:41:44 +0200

    NES assembly example #9: macro to clear palette

---
+++ NES-ca65/example09.asm
@@ -0,0 +1,177 @@
+; ---------------------------------------------------------------------
+; Kostra programu pro herní konzoli NES
+; Nastavení barvové palety, zvýšení intenzity barvy
+; Vynulování barvové palety makrem
+;
+; Založeno na příkladu https://github.com/depp/ctnes/tree/master/nesdev/01
+; Viz též článek na https://www.moria.us/blog/2018/03/nes-development
+; Audio https://raw.githubusercontent.com/iliak/nes/master/doc/apu_ref.txt
+; ---------------------------------------------------------------------
+ 
+; Jména řídicích registrů použitých v kódu
+PPUCTRL         = $2000
+PPUMASK         = $2001
+PPUSTATUS       = $2002
+PPUADDR         = $2006
+PPUDATA         = $2007
+
+; Další důležité adresy
+PALETTE         = $3f00
+
+
+; ---------------------------------------------------------------------
+; Definice maker
+; ---------------------------------------------------------------------
+
+.macro setup_cpu
+        ; nastavení stavu CPU
+        sei                     ; zákaz přerušení
+        cld                     ; vypnutí dekadického režimu (není podporován)
+
+        ldx #$ff
+        txs                     ; vrchol zásobníku nastaven na 0xff (první stránka)
+.endmacro
+
+.macro wait_for_frame
+:       bit PPUSTATUS            ; test obsahu registru PPUSTATUS 
+        bpl :-                   ; skok, pokud je příznak N nulový
+.endmacro
+
+.macro clear_ram
+        lda #$00                ; vynulování registru A
+:       sta $000, x             ; vynulování X-tého bajtu v nulté stránce
+        sta $100, x
+        sta $200, x
+        sta $300, x
+        sta $400, x
+        sta $500, x
+        sta $600, x
+        sta $700, x             ; vynulování X-tého bajtu v sedmé stránce
+        inx                     ; přechod na další bajt
+        bne :-                  ; po přetečení 0xff -> 0x00 konec smyčky
+.endmacro
+
+; vynulování barvové palety
+.macro clear_palette
+	lda PPUSTATUS   ; reset záchytného registru
+	lda #>PALETTE   ; nastavení adresy pro barvovou paletu $3f00
+	sta PPUADDR
+	lda #<PALETTE   ; nižší bajt adresy
+	sta PPUADDR
+
+	ldx #$20        ; počitadlo barev v paletě: 16+16
+	lda #$00        ; vynulování každé barvy
+
+:
+	sta PPUDATA     ; zápis barvy
+	dex             ; snížení hodnoty počitadla
+	bne :-
+.endmacro
+
+
+
+; ---------------------------------------------------------------------
+; Definice hlavičky obrazu ROM
+; ---------------------------------------------------------------------
+
+; Size of PRG in units of 16 KiB.
+prg_npage = 1
+
+; Size of CHR in units of 8 KiB.
+chr_npage = 1
+
+; INES mapper number.
+mapper = 0
+
+; Mirroring (0 = horizontal, 1 = vertical)
+mirroring = 1
+
+.segment "HEADER"
+        .byte $4e, $45, $53, $1a
+        .byte prg_npage
+        .byte chr_npage
+        .byte ((mapper & $0f) << 4) | (mirroring & 1)
+        .byte mapper & $f0
+
+
+
+; ---------------------------------------------------------------------
+; Blok paměti s definicí dlaždic 8x8 pixelů
+; ---------------------------------------------------------------------
+
+.segment "CHR0a"
+.segment "CHR0b"
+
+
+.code
+
+; ---------------------------------------------------------------------
+; Programový kód rutin pro NMI, RESET a IRQ volaných automaticky CPU
+;
+; viz též https://www.pagetable.com/?p=410
+; ---------------------------------------------------------------------
+
+; Obslužná rutina pro NMI (nemaskovatelné přerušení, vertical blank)
+
+.proc nmi
+        rti                     ; návrat z přerušení
+.endproc
+
+
+
+; Obslužná rutina pro IRQ (maskovatelné přerušení)
+
+.proc irq
+        rti                     ; návrat z přerušení
+.endproc
+
+
+
+; Obslužná rutina pro RESET
+
+.proc reset
+        ; nastavení stavu CPU
+        setup_cpu
+
+        ; nastavení řídicích registrů
+        ldx #$00
+        stx PPUCTRL             ; nastavení PPUCTRL = 0
+        stx PPUMASK             ; nastavení PPUMASK = 0
+
+        ; čekání na vnitřní inicializaci PPU (dva snímky)
+        wait_for_frame
+        wait_for_frame
+
+        ; vymazání obsahu RAM
+        clear_ram
+
+        ; čekání na další snímek
+        wait_for_frame
+
+        ; vynulování barvové palety
+        clear_palette  ; expanze makra
+
+        lda #%10000000 ; vysoká intenzita modré barvy
+        sta PPUMASK
+
+        ; vlastní herní smyčka je prozatím prázdná
+game_loop:
+        jmp game_loop           ; nekonečná smyčka (později rozšíříme)
+.endproc
+
+
+
+; ---------------------------------------------------------------------
+; Tabulka vektorů CPU
+; ---------------------------------------------------------------------
+
+.segment "VECTORS"
+.addr nmi
+.addr reset
+.addr irq
+
+
+
+; ---------------------------------------------------------------------
+; Finito
+; ---------------------------------------------------------------------
