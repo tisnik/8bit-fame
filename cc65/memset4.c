d793fd27919cc6eb71e5fe2a448eb85ea72395eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-14 07:59:39 +0200

    Memset function, variant with while loop

---
+++ cc65/memset4.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+void memset8(uint8_t * dest, const uint8_t c, uint8_t n)
+{
+    while (n > 0) {
+        *dest++ = c;
+        n--;
+    }
+}
+
+int main(void)
+{
+    uint8_t *dest = (uint8_t *) 0x0600;
+    uint8_t bytes = 0xff;
+    uint8_t fill = 0x00;
+    memset8(dest, fill, bytes);
+
+    return 0;
+}
