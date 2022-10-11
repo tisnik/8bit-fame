351ac8cd38a00c961b364481503ed39b7b9799d4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-11 09:29:23 +0200

    Script to make BASIC-based slides: 2nd version

--- slides/make_slides.py
+++ slides/make_slides.py
@@ -2,9 +2,19 @@ import sys
 
 
 def main(argv):
-    pass
+    # check if input and output files are specified
+    if len(argv) < 2:
+        print("Error - not enough arguments")
+        return 1
+
+    # process input file
+    input_file = argv[1]
+    output_file = argv[2]
 
 
+
+# If this script is started from command line, run the `main` function which is
+# entry point to the processing.
 if __name__ == "__main__":
     result = main(sys.argv)
     sys.exit(result)
