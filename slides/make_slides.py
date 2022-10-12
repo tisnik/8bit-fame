a1f45f8370bc83a2257908ee21b766aa7f5410c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-12 09:13:24 +0200

    Script to make BASIC-based slides: 3rd version

--- slides/make_slides.py
+++ slides/make_slides.py
@@ -10,7 +10,17 @@ def main(argv):
     # process input file
     input_file = argv[1]
     output_file = argv[2]
+    process(input_file, output_file)
 
+
+
+def process(input_file, output_file):
+    """Open input and output files + start processing."""
+    with open(input_file, "r") as fin:
+        with open(output_file, "w") as fout:
+            for line in fin.readlines():
+                line = line.strip()
+                print(line)
 
 
 # If this script is started from command line, run the `main` function which is
