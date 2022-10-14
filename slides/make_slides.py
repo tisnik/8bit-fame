cc6a8b8db27cd2f43339c72855341c10509da6c6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-14 08:03:33 +0200

    Script to make BASIC-based slides: 5th version

--- slides/make_slides.py
+++ slides/make_slides.py
@@ -3,6 +3,8 @@ import sys
 
 # special files to be inserted and appended into output slides
 STARTUP_FILE = "startup.bas"
+INIT_FILE = "init.bas"
+
 
 
 def main(argv):
@@ -17,20 +19,28 @@ def main(argv):
     process(input_file, output_file, STARTUP_FILE)
 
 
-
-def process(input_file, output_file, startup_file):
-    """Open input and output files + start processing."""
+def process(input_file, output_file, startup_file, init_file):
+    """Open input and output files and start processing."""
     with open(output_file, "w") as fout:
         # insert startup code into the output file
-        with open(startup_file, "r") as fin:
-            for line in fin.readlines():
-                line = line.strip()
-                print(line, file=fout)
+        insert_file(fout, startup_file)
 
+        # process input file and generate slide section
         with open(input_file, "r") as fin:
             for line in fin.readlines():
                 line = line.strip()
                 print(line)
+
+        # insert init code into the output file
+        insert_file(fout, init_file)
+
+
+def insert_file(fout, filename):
+    """Insert content of selected file into the output file."""
+    with open(filename, "r") as fin:
+        for line in fin.readlines():
+            line = line.strip()
+            print(line, file=fout)
 
 
 # If this script is started from command line, run the `main` function which is
