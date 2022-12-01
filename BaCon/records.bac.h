28f5cbd3d1df5bf65c505fa2f4547b9cf82054cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:42 +0100

    Source code records.bas transpilled into C code

---
+++ BaCon/records.bac.h
@@ -0,0 +1,38 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "records.bac.generic.h"
+#include "records.bac.functions.h"
+#line 1 "records.bac"
+#line 2 "records.bac"
+#line 3 "records.bac"
+#line 4 "records.bac"
+#line 5 "records.bac"
+#line 6 "records.bac"
+#line 7 "records.bac"
+#line 9 "records.bac"
+typedef struct
+{
+#line 10 "records.bac"
+	long id;
+#line 11 "records.bac"
+	char *name__b2c__string_var;
+#line 12 "records.bac"
+	char *surname__b2c__string_var;
+#line 13 "records.bac"
+} users_TYPE;
+typedef users_TYPE users_type;
+users_TYPE users[(uint64_t) 10 + 0] = { 0 };
+
+#line 15 "records.bac"
+#line 16 "records.bac"
+#line 17 "records.bac"
+#line 19 "records.bac"
+#line 20 "records.bac"
+#line 21 "records.bac"
+#line 23 "records.bac"
+#line 24 "records.bac"
+#line 25 "records.bac"
+#line 27 "records.bac"
+long i;
+
+#line 28 "records.bac"
+#line 29 "records.bac"
