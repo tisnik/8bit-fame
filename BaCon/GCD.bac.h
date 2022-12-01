0ae63aab2462c5ff94ac1571768545a934577d5e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:41 +0100

    Source code GCD.bas transpilled into C code

---
+++ BaCon/GCD.bac.h
@@ -0,0 +1,35 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "GCD.bac.generic.h"
+#include "GCD.bac.functions.h"
+#line 1 "GCD.bac"
+#line 2 "GCD.bac"
+#line 3 "GCD.bac"
+#line 4 "GCD.bac"
+#line 5 "GCD.bac"
+#line 6 "GCD.bac"
+#line 7 "GCD.bac"
+#line 8 "GCD.bac"
+#line 10 "GCD.bac"
+#line 11 "GCD.bac"
+long X;
+
+#line 12 "GCD.bac"
+#line 13 "GCD.bac"
+long Y;
+
+#line 15 "GCD.bac"
+int __b2c__label_floatarray_LOOP;
+int __b2c__label_stringarray_LOOP;
+
+#line 17 "GCD.bac"
+#line 18 "GCD.bac"
+#line 19 "GCD.bac"
+#line 20 "GCD.bac"
+#line 22 "GCD.bac"
+#line 23 "GCD.bac"
+#line 24 "GCD.bac"
+#line 25 "GCD.bac"
+#line 27 "GCD.bac"
+#line 28 "GCD.bac"
+#line 29 "GCD.bac"
+#line 30 "GCD.bac"
