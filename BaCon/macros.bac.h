b972287d8a430926ddebe10544dc5b8bf060b3ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:41 +0100

    Source code macros.bas transpilled into C code

---
+++ BaCon/macros.bac.h
@@ -0,0 +1,46 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "macros.bac.generic.h"
+#include "macros.bac.functions.h"
+#line 1 "macros.bac"
+#line 2 "macros.bac"
+#line 3 "macros.bac"
+#line 4 "macros.bac"
+#line 5 "macros.bac"
+#line 6 "macros.bac"
+#line 7 "macros.bac"
+#line 11 "macros.bac"
+long A[(uint64_t) 20 + 0] = { 0 };
+
+#line 13 "macros.bac"
+long I;
+
+#line 14 "macros.bac"
+#line 15 "macros.bac"
+#line 17 "macros.bac"
+#line 19 "macros.bac"
+#line 20 "macros.bac"
+#line 21 "macros.bac"
+long J;
+
+#line 22 "macros.bac"
+#line 23 "macros.bac"
+long X;
+
+#line 24 "macros.bac"
+#line 25 "macros.bac"
+#line 26 "macros.bac"
+#line 27 "macros.bac"
+#line 28 "macros.bac"
+#line 30 "macros.bac"
+#line 31 "macros.bac"
+#line 33 "macros.bac"
+#line 34 "macros.bac"
+#line 36 "macros.bac"
+int __b2c__label_floatarray_PRINT_ARRAY;
+int __b2c__label_stringarray_PRINT_ARRAY;
+
+#line 37 "macros.bac"
+#line 38 "macros.bac"
+#line 39 "macros.bac"
+#line 40 "macros.bac"
+#line 41 "macros.bac"
