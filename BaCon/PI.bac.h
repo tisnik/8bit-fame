7c37097479307756e2e918ae033becd3c5aa8c54
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:41 +0100

    Source code PI.bas transpilled into C code

---
+++ BaCon/PI.bac.h
@@ -0,0 +1,38 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "PI.bac.generic.h"
+#include "PI.bac.functions.h"
+#line 1 "PI.bac"
+#line 2 "PI.bac"
+#line 3 "PI.bac"
+#line 4 "PI.bac"
+#line 5 "PI.bac"
+#line 6 "PI.bac"
+#line 8 "PI.bac"
+long N = 1;
+
+#line 10 "PI.bac"
+long I;
+
+#line 11 "PI.bac"
+#line 12 "PI.bac"
+#line 13 "PI.bac"
+#line 14 "PI.bac"
+#line 15 "PI.bac"
+#line 16 "PI.bac"
+#line 17 "PI.bac"
+#line 19 "PI.bac"
+#line 20 "PI.bac"
+#line 21 "PI.bac"
+#line 23 "PI.bac"
+int __b2c__label_floatarray_PI_COMP;
+int __b2c__label_stringarray_PI_COMP;
+
+#line 24 "PI.bac"
+double PI_VAL;
+
+#line 25 "PI.bac"
+long J;
+
+#line 26 "PI.bac"
+#line 27 "PI.bac"
+#line 28 "PI.bac"
