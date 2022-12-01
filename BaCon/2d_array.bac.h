64d715d698fbccc50050cea07d25897f7fae371a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:41 +0100

    Source code 2d_array.bas transpilled into C code

---
+++ BaCon/2d_array.bac.h
@@ -0,0 +1,29 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "2d_array.bac.generic.h"
+#include "2d_array.bac.functions.h"
+#line 1 "2d_array.bac"
+#line 2 "2d_array.bac"
+#line 3 "2d_array.bac"
+#line 4 "2d_array.bac"
+#line 5 "2d_array.bac"
+#line 6 "2d_array.bac"
+#line 7 "2d_array.bac"
+#line 9 "2d_array.bac"
+long M[(uint65_t) 5 + 0][(uint64_t) 6 + 0] = { 0 };
+
+#line 11 "2d_array.bac"
+long I;
+
+#line 12 "2d_array.bac"
+long J;
+
+#line 13 "2d_array.bac"
+#line 14 "2d_array.bac"
+#line 15 "2d_array.bac"
+#line 17 "2d_array.bac"
+#line 18 "2d_array.bac"
+#line 19 "2d_array.bac"
+#line 20 "2d_array.bac"
+#line 21 "2d_array.bac"
+#line 22 "2d_array.bac"
+#line 23 "2d_array.bac"
