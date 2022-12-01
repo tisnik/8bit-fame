830131e9993b91aca4b59fa08c25e2b0db7416e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:42 +0100

    Source code while_wend_pi.bas transpilled into C code

---
+++ BaCon/while_wend_pi.bac.h
@@ -0,0 +1,34 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "while_wend_pi.bac.generic.h"
+#include "while_wend_pi.bac.functions.h"
+#line 1 "while_wend_pi.bac"
+#line 2 "while_wend_pi.bac"
+#line 3 "while_wend_pi.bac"
+#line 4 "while_wend_pi.bac"
+#line 5 "while_wend_pi.bac"
+#line 6 "while_wend_pi.bac"
+#line 7 "while_wend_pi.bac"
+#line 10 "while_wend_pi.bac"
+long N;
+
+#line 11 "while_wend_pi.bac"
+#line 12 "while_wend_pi.bac"
+#line 13 "while_wend_pi.bac"
+#line 14 "while_wend_pi.bac"
+#line 15 "while_wend_pi.bac"
+#line 17 "while_wend_pi.bac"
+#line 18 "while_wend_pi.bac"
+int __b2c__label_floatarray_COMPUTE_PI;
+int __b2c__label_stringarray_COMPUTE_PI;
+
+#line 19 "while_wend_pi.bac"
+double PI_VAL;
+
+#line 20 "while_wend_pi.bac"
+long J;
+
+#line 21 "while_wend_pi.bac"
+#line 22 "while_wend_pi.bac"
+#line 23 "while_wend_pi.bac"
+#line 24 "while_wend_pi.bac"
+#line 25 "while_wend_pi.bac"
