3afb53696b5268cdb276aab8f8abc548d255feb2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:41 +0100

    Source code factorial.bas transpilled into C code

---
+++ BaCon/factorial.bac.h
@@ -0,0 +1,31 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "factorial.bac.generic.h"
+#include "factorial.bac.functions.h"
+#line 1 "factorial.bac"
+#line 2 "factorial.bac"
+#line 3 "factorial.bac"
+#line 4 "factorial.bac"
+#line 5 "factorial.bac"
+#line 6 "factorial.bac"
+#line 7 "factorial.bac"
+#line 9 "factorial.bac"
+long N;
+
+#line 10 "factorial.bac"
+#line 11 "factorial.bac"
+#line 12 "factorial.bac"
+#line 13 "factorial.bac"
+#line 15 "factorial.bac"
+#line 16 "factorial.bac"
+int __b2c__label_floatarray_FACTORIAL;
+int __b2c__label_stringarray_FACTORIAL;
+
+#line 17 "factorial.bac"
+long FACT;
+
+#line 18 "factorial.bac"
+long I;
+
+#line 19 "factorial.bac"
+#line 20 "factorial.bac"
+#line 21 "factorial.bac"
