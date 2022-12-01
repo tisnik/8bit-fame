d5af56604ff471b6a97ea47b6ab730914bfa3a07
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:41 +0100

    Source code bubble_sort.bas transpilled into C code

---
+++ BaCon/bubble_sort.bac.h
@@ -0,0 +1,45 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "bubble_sort.bac.generic.h"
+#include "bubble_sort.bac.functions.h"
+#line 1 "bubble_sort.bac"
+#line 2 "bubble_sort.bac"
+#line 3 "bubble_sort.bac"
+#line 4 "bubble_sort.bac"
+#line 5 "bubble_sort.bac"
+#line 6 "bubble_sort.bac"
+#line 10 "bubble_sort.bac"
+long A[(uint64_t) 20 + 0] = { 0 };
+
+#line 12 "bubble_sort.bac"
+long I;
+
+#line 13 "bubble_sort.bac"
+#line 14 "bubble_sort.bac"
+#line 16 "bubble_sort.bac"
+#line 18 "bubble_sort.bac"
+#line 19 "bubble_sort.bac"
+#line 20 "bubble_sort.bac"
+long J;
+
+#line 21 "bubble_sort.bac"
+#line 22 "bubble_sort.bac"
+long X;
+
+#line 23 "bubble_sort.bac"
+#line 24 "bubble_sort.bac"
+#line 25 "bubble_sort.bac"
+#line 26 "bubble_sort.bac"
+#line 27 "bubble_sort.bac"
+#line 29 "bubble_sort.bac"
+#line 30 "bubble_sort.bac"
+#line 32 "bubble_sort.bac"
+#line 33 "bubble_sort.bac"
+#line 35 "bubble_sort.bac"
+int __b2c__label_floatarray_PRINT_ARRAY;
+int __b2c__label_stringarray_PRINT_ARRAY;
+
+#line 36 "bubble_sort.bac"
+#line 37 "bubble_sort.bac"
+#line 38 "bubble_sort.bac"
+#line 39 "bubble_sort.bac"
+#line 40 "bubble_sort.bac"
