6269cbb68c6ecff6ec0153063d21c4e0d38abda5
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-12-01 13:40:41 +0100

    Source code 1d_array.bas transpilled into C code

---
+++ BaCon/1d_array.bac.h
@@ -0,0 +1,22 @@
+/* Created with BaCon 4.4.1 - (c) Peter van Eerten - MIT License */
+#include "1d_array.bac.generic.h"
+#include "1d_array.bac.functions.h"
+#line 1 "1d_array.bac"
+#line 2 "1d_array.bac"
+#line 3 "1d_array.bac"
+#line 4 "1d_array.bac"
+#line 5 "1d_array.bac"
+#line 6 "1d_array.bac"
+#line 7 "1d_array.bac"
+#line 9 "1d_array.bac"
+long A[(uint64_t) 10 + 0] = { 0 };
+
+#line 11 "1d_array.bac"
+long I;
+
+#line 12 "1d_array.bac"
+#line 13 "1d_array.bac"
+#line 15 "1d_array.bac"
+#line 16 "1d_array.bac"
+#line 17 "1d_array.bac"
+#line 18 "1d_array.bac"
