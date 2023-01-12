6de369f202b5d914e5911d8b27cc1f87f51e0ed4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-01-12 09:33:00 +0100

    NES sprites via NESlib from C

---
+++ NES-cc65/08_Sprites_A/sprites.c
@@ -0,0 +1,68 @@
+#include "neslib.h"
+
+#define ATTRIBUTE_TABLE 0x23c0
+
+#pragma bss-name(push, "ZEROPAGE")
+int i;
+int address;
+
+// barvy pozadí
+const unsigned char background_palette[] = {
+    0x22, 0x29, 0x1a, 0x0F, 0x22, 0x36, 0x17, 0x0F,
+    0x22, 0x30, 0x21, 0x0F, 0x22, 0x27, 0x17, 0x0F
+};
+
+// barvy spritů
+const unsigned char sprite_palette[] = {
+    0x22, 0x16, 0x27, 0x18, 0x22, 0x1A, 0x30, 0x27,
+    0x22, 0x16, 0x30, 0x27, 0x22, 0x0F, 0x36, 0x17
+};
+
+
+void fill_in_ppu_ram(void)
+{
+    vram_adr(NTADR_A(0, 0));
+    for (i = 0; i < 32 * 30; i++) {
+        vram_put(36);
+    }
+}
+
+void fill_in_attributes(void)
+{
+    vram_adr(ATTRIBUTE_TABLE);
+
+    vram_fill(0, 16);
+    vram_fill(0x55, 16);
+    vram_fill(0xAA, 16);
+    vram_fill(0xFF, 16);
+}
+
+void game_loop(void)
+{
+    while (1) {
+        ppu_wait_nmi();
+        oam_clear();
+        oam_spr(10, 10, 0, 0);
+        oam_spr(18, 10, 1, 0);
+        oam_spr(10, 18, 2, 0);
+        oam_spr(18, 18, 3, 0);
+        oam_spr(10, 20, 4, 0);
+        oam_spr(18, 20, 5, 0);
+        oam_spr(10, 28, 6, 0);
+        oam_spr(18, 28, 7, 0);
+    }
+}
+
+void main(void)
+{
+    ppu_off();
+    pal_bg(background_palette);
+    pal_spr(sprite_palette);
+    bank_bg(1);
+    bank_spr(0);
+    fill_in_ppu_ram();
+    fill_in_attributes();
+    ppu_on_all();
+
+    game_loop();
+}
