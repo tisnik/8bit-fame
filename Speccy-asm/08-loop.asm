9beb21186e3b3583e8e64d284c77933e632e3f41
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-02-09 08:48:56 +0100

    Simple loop, 1st approach

---
+++ Speccy-asm/08-loop.asm
@@ -0,0 +1,19 @@
+ATTRIBUTE_ADR equ $5800
+ENTRY_POINT   equ $8000
+
+	org ENTRY_POINT
+
+start:
+	ld hl, ATTRIBUTE_ADR  ; adresa pro zápis
+        ld a, 0               ; zapisovaná hodnota
+	ld b, 0               ; počitadlo smyčky
+
+loop:
+	ld (hl),a             ; zápis hodnoty na adresu (HL)
+	inc hl                ; zvýšení adresy
+	inc a                 ; zvýšení zapisované hodnoty
+	dec b                 ; snížení hodnoty počitadla
+	jp NZ, loop           ; skok pokud se ještě nedosáhlo nuly
+	ret
+
+end ENTRY_POINT
