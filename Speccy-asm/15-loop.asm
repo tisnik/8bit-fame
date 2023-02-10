f6a839f4986a50841813505fb892056e5b0fc755
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-02-10 07:49:08 +0100

    Loop with 16bit counter, better alternative

---
+++ Speccy-asm/15-loop.asm
@@ -0,0 +1,19 @@
+ATTRIBUTE_ADR equ $5800
+ENTRY_POINT   equ $8000
+
+	org ENTRY_POINT
+
+start:
+	ld hl, ATTRIBUTE_ADR  ; adresa pro zápis
+	ld a, 2               ; počet opakování bloku s 256 zápisy
+	ld b, 0
+
+loop:
+	ld (hl),l             ; zápis hodnoty na adresu (HL)
+	inc hl                ; zvýšení adresy i zapisované hodnoty
+	djnz loop             ; vnitřní smyčka: blok s 256 zápisy
+	dec a                 ; počitadlo vnější smyčky
+	jp NZ, loop           ; skok pokud se ještě nedosáhlo nuly
+	ret
+
+end ENTRY_POINT
