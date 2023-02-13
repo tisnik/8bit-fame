b9f04d17775c1bc5eed2e08de84ebf9a63610d58
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-02-13 07:56:59 +0100

    Loop variant with changing just L-part of address

---
+++ Speccy-asm/17-loop.asm
@@ -0,0 +1,18 @@
+ATTRIBUTE_ADR equ $5800
+ENTRY_POINT   equ $8000
+
+	org ENTRY_POINT
+
+start:
+	ld hl, ATTRIBUTE_ADR  ; adresa pro zápis
+	ld b, l               ; zapisovaná hodnota + počitadlo smyčky
+
+loop:
+	ld (hl),l             ; zápis hodnoty na adresu (HL)
+	inc l                 ; zvýšení adresy i zapisované hodnoty
+	djnz loop             ; kombinace dec b + jp NZ, loop
+	                      ; snížení hodnoty počitadla
+	                      ; skok pokud se ještě nedosáhlo nuly
+	ret
+
+end ENTRY_POINT
