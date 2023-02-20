65bc9f1303fc581fdfc1fbc5ba0e86cb06d1aee3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-02-20 08:06:34 +0100

    Example #28: print whole string onto screen (better variant)

---
+++ Speccy-asm/28-print-string.asm
@@ -0,0 +1,24 @@
+ENTRY_POINT      equ $8000
+ROM_OPEN_CHANNEL equ $1601
+
+	org ENTRY_POINT
+
+start:
+	ld   A,2              ; číslo kanálu
+	call ROM_OPEN_CHANNEL ; otevření kanálu číslo 2 (screen)
+
+	ld   HL, TEXT         ; adresa prvního znaku v řetězci
+
+LOOP:
+	ld   A, (HL)          ; načíst kód znaku z řetězce
+	and  a                ; test na kód znak s kódem 0
+	ret  Z                ; ukončit program na konci řetězce
+
+	rst  0x10             ; zavolání rutiny v ROM
+	inc  HL               ; přechod na další znak
+	jr   LOOP
+
+; nulou ukončený řetězec
+TEXT:	db "Hello, Speccy!", 0
+
+end ENTRY_POINT
