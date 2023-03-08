c535c8ec2c060a387f99c36cb18474b25737ef55
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-08 07:32:58 +0100

    Example 47: drawing char using unrolled loop

---
+++ Speccy-asm/47-draw-char-at-unrolled.asm
@@ -0,0 +1,110 @@
+SCREEN_ADR      equ $4000
+CHAR_ADR        equ $3c00
+ENTRY_POINT     equ $8000
+
+	org ENTRY_POINT
+
+start:
+	ld b, 15                 ; x-ová souřadnice
+	ld c, 12                 ; y-ová souřadnice
+	call calc_char_address   ; výpočet adresy
+	ld a, 'A'                ; kód vykreslovaného znaku
+	call draw_char           ; zavolat subrutinu pro vykreslení znaku
+	ld a, 'B'                ; kód vykreslovaného znaku
+	call draw_char           ; zavolat subrutinu pro vykreslení znaku
+
+	ld b, 31                 ; x-ová souřadnice
+	ld c, 23                 ; y-ová souřadnice
+	call calc_char_address   ; výpočet adresy
+	ld a, '?'                ; kód vykreslovaného znaku
+	call draw_char           ; zavolat subrutinu pro vykreslení znaku
+
+finish:
+	jr finish               ; žádný návrat do systému
+
+
+calc_char_address:
+        ; parametry:
+	; B - x-ová souřadnice (ve znacích, ne pixelech)
+	; C - y-ová souřadnice (ve znacích, ne pixelech)
+	;
+	; návratové hodnoty:
+	; DE - adresa pro zápis bloku
+	;
+	; vzor adresy:
+	; 0 1 0 Y4 Y3 0 0 0 | Y2 Y1 Y0 X4 X3 X2 X1 X0
+	ld  a, c
+	and %00000111         ; pouze spodní tři bity y-ové souřadnice (řádky 0..7)
+	rrca
+	rrca
+	rrca                  ; nyní jsou čísla řádků v horních třech bitech
+	or  b                 ; připočítat x-ovou souřadnici
+	ld  e, a              ; máme spodní bajt adresy
+	                      ; Y2 Y1 Y0 X4 X3 X2 X1 X0
+
+	ld  a, c              ; y-ová souřadnice
+	and %00011000         ; dva bity s indexem "bloku" 0..3 (dolní tři bity už máme zpracovány)
+	or  %01000000         ; "posun" do obrazové paměti (na 0x4000)
+	ld  d, a              ; máme horní bajt adresy
+	                      ; 0 1 0 Y5 Y4 0 0 0
+	ret                   ; návrat z podprogramu
+
+
+draw_char:
+	ld bc, CHAR_ADR          ; adresa, od níž začínají masky znaků
+        ld h, c                  ; C je nulové, protože CHAR_ADR=0x3c00
+        ld l, a                  ; kód znaku je nyní ve dvojici HL
+
+        add  hl, hl              ; 2x
+        add  hl, hl              ; 4x
+        add  hl, hl              ; 8x
+        add  hl, bc              ; přičíst bázovou adresu masek znaků
+
+	ld c, d
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        ld   a,(hl)              ; načtení jednoho bajtu z masky
+        ld  (de),a               ; zápis hodnoty na adresu (DE)
+        inc  l                   ; posun na další bajt masky (nemusíme řešit přetečení do vyššího bajtu)
+        inc  d                   ; posun na definici dalšího obrazového řádku
+
+        inc   e
+        ret   z                  ; D+=8,E=E+1=0
+        ld    d, c
+        ret                      ; D=D,E=E+1
+
+end ENTRY_POINT
