1963202a44eb7ee97e3c8912ee2d894be39080db
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-09 08:31:09 +0100

    Added link to article and the whole serie

--- Speccy-asm/02-blinking-attribute.asm
+++ Speccy-asm/02-blinking-attribute.asm
@@ -1,3 +1,16 @@
+; Example that is used in following article:
+;    Kouzlo minimalismu potřetí: vývoj her a dem pro slavné ZX Spectrum
+;    https://www.root.cz/clanky/kouzlo-minimalismu-potreti-vyvoj-her-a-dem-pro-slavne-zx-spectrum/
+;
+; This article is part of series:
+;    "Vývoj pro slavné ZX Spectrum"
+;    https://www.root.cz/serialy/vyvoj-pro-slavne-zx-spectrum/
+;
+; Repository:
+;    https://github.com/tisnik/8bit-fame
+
+
+
 	org $8000
 
 start:
