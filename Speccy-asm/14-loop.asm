ea716e306207b3357bb2155284474f92e580c40a
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-10 07:38:49 +0100

    Added link to article and the whole serie

--- Speccy-asm/14-loop.asm
+++ Speccy-asm/14-loop.asm
@@ -1,3 +1,16 @@
+; Example that is used in following article:
+;    Vývoj pro ZX Spectrum: mikroprocesor Zilog Z80 a smyčky v assembleru
+;    https://www.root.cz/clanky/vyvoj-pro-zx-spectrum-mikroprocesor-zilog-z80-a-smycky-v-assembleru/
+;
+; This article is part of series:
+;    "Vývoj pro slavné ZX Spectrum"
+;    https://www.root.cz/serialy/vyvoj-pro-slavne-zx-spectrum/
+;
+; Repository:
+;    https://github.com/tisnik/8bit-fame
+
+
+
 ATTRIBUTE_ADR equ $5800
 ENTRY_POINT   equ $8000
 
