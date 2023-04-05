dbaa604efa8c0c151fc1f5768f99387a9db7746b
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-05 07:52:15 +0200

    Added link to article and the whole serie

--- Speccy-asm/31-attributes.asm
+++ Speccy-asm/31-attributes.asm
@@ -1,3 +1,15 @@
+; Example that is used in following article:
+;    Vývoj her a dem pro ZX Spectrum: vlastní vykreslovací subrutiny
+;    https://www.root.cz/clanky/vyvoj-her-a-dem-pro-zx-spectrum-vlastni-vykreslovaci-subrutiny/
+;
+; This article is part of series:
+;    "Vývoj pro slavné ZX Spectrum"
+;    https://www.root.cz/serialy/vyvoj-pro-slavne-zx-spectrum/
+;
+; Repository:
+;    https://github.com/tisnik/8bit-fame
+;
+
 ENTRY_POINT      equ $8000
 ROM_OPEN_CHANNEL equ $1601
 ROM_PRINT        equ $203C
