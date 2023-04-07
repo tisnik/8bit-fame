8d9bb81bfc4b7db2db8f043fe4e9db10f8aab5f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-07 09:50:59 +0200

    Description added into example #27

--- Speccy-asm/27-print-string.asm
+++ Speccy-asm/27-print-string.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #27:
+;    Print whole string onto screen, unoptimized version
 
 ENTRY_POINT      equ $8000
 ROM_OPEN_CHANNEL equ $1601
