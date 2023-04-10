4ddadb4c5d7eb56ad16621882e8e4759d7fab6af
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-10 08:47:46 +0200

    Added link to article

--- Speccy-asm/67-print-flags-1.asm
+++ Speccy-asm/67-print-flags-1.asm
@@ -1,3 +1,15 @@
+; Example that is used in following article:
+;    Zobrazení čísel a zpracování příznaků mikroprocesoru Zilog Z80
+;    https://www.root.cz/clanky/zobrazeni-cisel-a-zpracovani-priznaku-mikroprocesoru-zilog-z80/
+;
+; This article is part of series:
+;    "Vývoj pro slavné ZX Spectrum"
+;    https://www.root.cz/serialy/vyvoj-pro-slavne-zx-spectrum/
+;
+; Repository:
+;    https://github.com/tisnik/8bit-fame
+;
+
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
 OUT_NUM_1     equ $1A1B
