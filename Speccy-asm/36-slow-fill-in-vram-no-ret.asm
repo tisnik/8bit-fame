8886b4f4f5ec69958811e9135c9274e9d291911f
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-14 11:28:41 +0200

    Added description

--- Speccy-asm/36-slow-fill-in-vram-no-ret.asm
+++ Speccy-asm/36-slow-fill-in-vram-no-ret.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #36:
+;    Fill-in the whole screen by ink color, RET optimization (no return to system).
 
 SCREEN_ADR    equ $4000
 ENTRY_POINT   equ $8000
