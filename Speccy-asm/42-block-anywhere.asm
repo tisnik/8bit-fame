e8de00d76dc18351d459061595492ab7d8be92a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-19 08:01:48 +0200

    Added description for this example

--- Speccy-asm/42-block-anywhere.asm
+++ Speccy-asm/42-block-anywhere.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #42:
+;    Draw any character anywhere on screen using own drawing routine.
 
 SCREEN_ADR      equ $4000
 ENTRY_POINT     equ $8000
