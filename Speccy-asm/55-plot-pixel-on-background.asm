0fc538742503b5dfb0d4d0a0fc7888e2ae24b9ce
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-01 18:10:19 +0200

    Added description

--- Speccy-asm/55-plot-pixel-on-background.asm
+++ Speccy-asm/55-plot-pixel-on-background.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #55:
+;    Drawing real pixel without erasing background.
 
 SCREEN_ADR      equ $4000
 CHAR_ADR        equ $3c00
