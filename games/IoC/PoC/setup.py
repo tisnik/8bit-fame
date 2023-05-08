c1d296f2436e2fd05025bf6a7964ff9754d65b94
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-08 12:58:11 +0200

    Setup for Python package

---
+++ games/IoC/PoC/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as fin:
+    readme = fin.read()
+
+with open('LICENSE') as fin:
+    license = fin.read()
+
+
+setup(
+    name="Inversion of Control",
+    version="0.1.0",
+    description="Simple game based on famous Pac Man",
+    long_description=readme,
+    author="Pavel Tisnovsky",
+    author_email="tisnik@centrum.cz",
+    url="https://github.com/tisnik/8bit-fame/tree/master/games/IoC/PoC",
+    license=license,
+    packages=find_packages(exclude=("tests", "docs"))
+)
