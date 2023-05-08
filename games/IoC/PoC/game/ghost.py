2bb4a94fbcc397af39d3b512ece07a228328ecad
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-08 12:47:19 +0200

    Added license

--- games/IoC/PoC/game/ghost.py
+++ games/IoC/PoC/game/ghost.py
@@ -1,4 +1,16 @@
-from game import IMAGES_PATH
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from setup import IMAGES_PATH
 
 import pygame
 import os
