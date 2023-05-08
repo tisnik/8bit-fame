ee9a79691ca44c4818684b0b741b1140b83307ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-08 16:32:49 +0200

    Direction enumeration

---
+++ games/IoC/PoC/game/direction.py
@@ -0,0 +1,19 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from enum import Enum
+
+class Direction(Enum):
+    LEFT = 1
+    RIGHT = 2
+    UP = 3
+    DOWN =4
