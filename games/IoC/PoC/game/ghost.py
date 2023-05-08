bca0f58c2ef101005b11ce4e09047fb27cf8429d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-08 16:39:06 +0200

    Four sprites for all four directions

--- games/IoC/PoC/game/ghost.py
+++ games/IoC/PoC/game/ghost.py
@@ -11,6 +11,7 @@
 #
 
 from setup import IMAGES_PATH
+from direction import Direction
 
 import pygame
 import os
@@ -18,8 +19,18 @@ import os
 
 class Ghost:
     def __init__(self, display, filename_prefix):
+        self._direction = Direction.UP
         self._display = display
-        self._sprite = pygame.image.load(os.path.join(IMAGES_PATH, filename_prefix + "_left.png"))
+
+        self._sprites = {}
+        self._sprites[Direction.LEFT] = self.loadImage(filename_prefix, "left")
+        self._sprites[Direction.RIGHT] = self.loadImage(filename_prefix, "right")
+        self._sprites[Direction.UP] = self.loadImage(filename_prefix, "up")
+        self._sprites[Direction.DOWN] = self.loadImage(filename_prefix, "down")
 
     def draw(self):
-        self._display.blit(self._sprite, (10, 10))
+        self._display.blit(self._sprites[self._direction], (10, 10))
+
+    def loadImage(self, filename_prefix, filename_suffix):
+        filename = f"{filename_prefix}_{filename_suffix}.png"
+        return pygame.image.load(os.path.join(IMAGES_PATH, filename))
