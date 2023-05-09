10ccf46d0d37199d0ac2c1449ca53930defcc741
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-09 07:46:01 +0200

    Added description

--- Speccy-asm/80-print-hex-digit-overflow.asm
+++ Speccy-asm/80-print-hex-digit-overflow.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #80:
+;    Print hexadecimal digit, based on naive conditional jump (overflowed numbers).
 
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
