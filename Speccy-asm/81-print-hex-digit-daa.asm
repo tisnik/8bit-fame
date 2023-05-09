f98ecd5ca1e528c80d6cff840095d6bb035cef6c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-09 07:46:01 +0200

    Added description

--- Speccy-asm/81-print-hex-digit-daa.asm
+++ Speccy-asm/81-print-hex-digit-daa.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #81:
+;    Print hexadecimal digit, based on DAA instruction trick.
 
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
