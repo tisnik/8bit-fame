fb2a100b622f4f2f95a9be6da5ee2c1cd19c602b
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-09 07:46:01 +0200

    Added description

--- Speccy-asm/79-print-hex-digit-jmp.asm
+++ Speccy-asm/79-print-hex-digit-jmp.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #79:
+;    Print hexadecimal digit, based on naive conditional jump.
 
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
