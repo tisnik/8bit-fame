e00efb6468f7b1be3e86b1441c4f6ec066c59954
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-09 08:23:01 +0200

    Named colors

---
+++ games/IoC/PoC/game/colors.py
@@ -0,0 +1,23 @@
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from enum import Enum
+
+class Colors(Enum):
+    BLACK = (0, 0, 0)
+    BLUE = (0, 0, 255)
+    CYAN = (0, 255, 255)
+    GREEN = (0, 255, 0)
+    YELLOW = (255, 255, 0)
+    RED = (255, 0, 0)
+    MAGENTA = (255, 0, 255)
+    WHITE = (255, 255, 255)
