218b6e055be29dcdbb82f9fd8bb015dd8b97e0c3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-10 16:52:08 +0200

    Drawing scared ghosts

--- games/IoC/PoC/game/ghost.py
+++ games/IoC/PoC/game/ghost.py
@@ -12,14 +12,37 @@
 
 from direction import Direction
 from sprite import Sprite
+from enum import Enum
+
+
+class Scared(Enum):
+    SCARED1 = 5
+    SCARED2 = 6
 
 
 class Ghost(Sprite):
     def __init__(self, display, filename_prefix):
         super(Ghost, self).__init__(display)
 
+        self._scared = False
+        self._scared_tick = 0
+
         self._sprites = {}
         self._sprites[Direction.LEFT] = self.loadImage(filename_prefix, "left")
         self._sprites[Direction.RIGHT] = self.loadImage(filename_prefix, "right")
         self._sprites[Direction.UP] = self.loadImage(filename_prefix, "up")
         self._sprites[Direction.DOWN] = self.loadImage(filename_prefix, "down")
+        self._sprites[Scared.SCARED1] = self.loadImage("ghost", "scared_1")
+        self._sprites[Scared.SCARED2] = self.loadImage("ghost", "scared_2")
+
+    def setScared(self, scared):
+        self._scared = scared
+
+    def draw(self):
+        if self._scared:
+            if self._scared_tick == 0:
+                self._display.blit(self._sprites[Scared.SCARED1], (self._x, self._y))
+            else:
+                self._display.blit(self._sprites[Scared.SCARED2], (self._x, self._y))
+        else:
+            self._display.blit(self._sprites[self._direction], (self._x, self._y))
