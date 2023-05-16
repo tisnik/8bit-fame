d74eee8bbcd00b7c1da27bc6b9200b684f87f525
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-16 08:36:52 +0200

    About screen implementation, including event loop

--- games/IoC/PoC/game/about_screen.py
+++ games/IoC/PoC/game/about_screen.py
@@ -10,3 +10,47 @@
 #      Pavel Tisnovsky
 #
 
+import pygame
+
+from colors import Colors
+
+
+class AboutScreen:
+    BACKGROUND_COLOR = Colors.BLACK.value
+    TITLE_COLOR = (255, 255, 255)
+    STATISTIC_COLOR = (120, 120, 255)
+
+    def __init__(self, display, resources):
+        # pre-render game title
+        self._title = resources.bigFont.render("About", True,
+                                               AboutScreen.TITLE_COLOR,
+                                               AboutScreen.BACKGROUND_COLOR)
+        self._display = display
+        self._clock = pygame.time.Clock()
+
+    def draw(self):
+        """Draw about screen."""
+        self._display.fill(Colors.BLACK.value)
+        self.drawTitle()
+
+    def drawTitle(self):
+        """Draw the title."""
+        x = self._display.get_width() / 2 - self._title.get_width() / 2
+        y = 0
+        self._display.blit(self._title, (x, y))
+
+    def eventLoop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.locals.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.locals.KEYDOWN:
+                    if event.key == pygame.locals.K_ESCAPE:
+                        return
+                    if event.key == pygame.locals.K_RETURN:
+                        return
+
+            self.draw()
+            pygame.display.update()
+            self._clock.tick(8)
