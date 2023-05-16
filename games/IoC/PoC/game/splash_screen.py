a4d3746068e8996acb9675d3855ab0e145ba199e
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-16 08:56:59 +0200

    Refactoring: use the given resources

--- games/IoC/PoC/game/splash_screen.py
+++ games/IoC/PoC/game/splash_screen.py
@@ -13,6 +13,7 @@
 #
 
 import os
+import sys
 import pygame
 
 from colors import Colors
@@ -22,41 +23,38 @@ from main_menu import MainMenu
 class SplashScreen:
     """Splash screen with title, welcome animation, and a main menu."""
     CYCLE_DIRECTION_COUNTER_START_VALUE = 3
+
+    # colors used on splash screen
     BACKGROUND_COLOR = Colors.BLACK.value
     TITLE_COLOR = (255, 255, 255)
     MENU_COLOR = (120, 120, 255)
     CREDITS_COLOR = (140, 140, 140)
 
-    # TODO: take from config
-    FONT_FILE_NAME = "fonts/FreeSans.ttf"
-
-    def __init__(self, display, images_path, filename_prefix, frames_count, ghost):
-        # load fonts
-        self._big_font = pygame.font.Font(SplashScreen.FONT_FILE_NAME, 60)
-        self._normal_font = pygame.font.Font(SplashScreen.FONT_FILE_NAME, 40)
-        self._small_font = pygame.font.Font(SplashScreen.FONT_FILE_NAME, 20)
+    def __init__(self, display, resources, images_path, filename_prefix, frames_count, ghost):
+        """Initialize the splash screen."""
+        # fonts and other required resources are taken from resources object.
 
         # pre-render game title
-        self._title = self._big_font.render("Inversion of Control", True,
-                                            SplashScreen.TITLE_COLOR,
-                                            SplashScreen.BACKGROUND_COLOR)
+        self._title = resources.bigFont.render("Inversion of Control", True,
+                                               SplashScreen.TITLE_COLOR,
+                                               SplashScreen.BACKGROUND_COLOR)
 
         # pre-render all menu items onto surfaces
         self._menu = (
-            self.renderMenuItem("Start new game"),
-            self.renderMenuItem("Settings"),
-            self.renderMenuItem("Statistic"),
-            self.renderMenuItem("About"),
-            self.renderMenuItem("Quit"),
+            self.renderMenuItem(resources, "Start new game"),
+            self.renderMenuItem(resources, "Settings"),
+            self.renderMenuItem(resources, "Statistic"),
+            self.renderMenuItem(resources, "About"),
+            self.renderMenuItem(resources, "Quit"),
         )
 
         # actually selected menu item
         self._selected_menu_item = 0
 
-        self._credits = self._small_font.render("2023 Markétka Tišnovská, Pavel Tišnovský",
-                                                True,
-                                                SplashScreen.CREDITS_COLOR,
-                                                SplashScreen.BACKGROUND_COLOR)
+        self._credits = resources.smallFont.render("2023 Markétka Tišnovská, Pavel Tišnovský",
+                                                   True,
+                                                   SplashScreen.CREDITS_COLOR,
+                                                   SplashScreen.BACKGROUND_COLOR)
         self._display = display
 
         self._ghost = ghost
@@ -73,10 +71,10 @@ class SplashScreen:
 
         self._clock = pygame.time.Clock()
 
-    def renderMenuItem(self, text):
-        return self._normal_font.render(text, True,
-                                       SplashScreen.MENU_COLOR,
-                                       SplashScreen.BACKGROUND_COLOR)
+    def renderMenuItem(self, resources, text):
+        return resources.normalFont.render(text, True,
+                                           SplashScreen.MENU_COLOR,
+                                           SplashScreen.BACKGROUND_COLOR)
 
     def draw(self):
         """Draw splash screen."""
@@ -88,7 +86,7 @@ class SplashScreen:
         self.drawCredits()
 
     def drawTitle(self):
-        """Draw the title."""
+        """Draw the title onto splash screen."""
         x = self._display.get_width() / 2 - self._title.get_width() / 2
         y = 0
         self._display.blit(self._title, (x, y))
@@ -160,6 +158,7 @@ class SplashScreen:
         self._display.blit(self._credits, (x, y))
 
     def eventLoop(self):
+        """Implementation of event loop that just waits for keypress or window close operation."""
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.locals.QUIT:
@@ -175,6 +174,7 @@ class SplashScreen:
                     if event.key == pygame.locals.K_RETURN:
                         return self.getSelectedMenuItem()
 
+            # all events has been processed - redraw the screen
             self.draw()
             pygame.display.update()
             self._clock.tick(8)
