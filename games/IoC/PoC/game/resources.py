36764848640b587d851394e61f99ea64ffbe7057
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-16 08:37:28 +0200

    Resources for fonts on one place

--- games/IoC/PoC/game/resources.py
+++ games/IoC/PoC/game/resources.py
@@ -10,3 +10,33 @@
 #      Pavel Tisnovsky
 #
 
+import os
+
+import pygame
+
+
+class Resources:
+    FONT_FILE_NAME = "FreeSans.ttf"
+
+    def __init__(self, configuration):
+        self.loadFonts(configuration)
+
+    def loadFonts(self, configuration):
+        fontDirectory = configuration["paths"]["fonts"]
+        fullFontFileName = os.path.join(fontDirectory, Resources.FONT_FILE_NAME)
+
+        self._big_font = pygame.font.Font(fullFontFileName, 60)
+        self._normal_font = pygame.font.Font(fullFontFileName, 40)
+        self._small_font = pygame.font.Font(fullFontFileName, 20)
+
+    @property
+    def bigFont(self):
+        return self._big_font
+
+    @property
+    def normalFont(self):
+        return self._normal_font
+
+    @property
+    def smallFont(self):
+        return self._small_font
