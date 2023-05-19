9a21910ba04952f10d03ab29f5e53b46e537d3dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-19 12:31:08 +0200

    Sprites on statistic screen

--- games/IoC/PoC/game/statistic_screen.py
+++ games/IoC/PoC/game/statistic_screen.py
@@ -18,6 +18,8 @@ import sys
 import pygame
 
 from colors import Colors
+from pacman import PacMan
+from ghost import Ghost
 
 
 class StatisticScreen:
@@ -36,6 +38,26 @@ class StatisticScreen:
         self._title = resources.bigFont.render("Statistic", True,
                                                StatisticScreen.TITLE_COLOR,
                                                StatisticScreen.BACKGROUND_COLOR)
+        # pre-render base texts
+        self._overall = resources.normalFont.render("Overall", True,
+                                                    StatisticScreen.STATISTIC_COLOR,
+                                                    StatisticScreen.BACKGROUND_COLOR)
+        
+        self._pacman = PacMan(display, resources, "pacman")
+        self._pacman.moveTo(100, 200)
+
+        self._red_ghost = Ghost(display, resources, "ghost_red")
+        self._red_ghost.moveTo(100, 250)
+
+        self._cyan_ghost = Ghost(display, resources, "ghost_cyan")
+        self._cyan_ghost.moveTo(100, 300)
+
+        self._green_ghost = Ghost(display, resources, "ghost_green")
+        self._green_ghost.moveTo(100, 350)
+
+        self._pink_ghost = Ghost(display, resources, "ghost_pink")
+        self._pink_ghost.moveTo(100, 400)
+
         self._display = display
         self._clock = pygame.time.Clock()
         self._statistic = statistic
@@ -44,12 +66,28 @@ class StatisticScreen:
         """Draw statistic screen."""
         self._display.fill(Colors.BLACK.value)
         self.drawTitle()
+        self.drawStatistic()
+        self._pacman.draw()
+        self._red_ghost.draw()
+        self._cyan_ghost.draw()
+        self._green_ghost.draw()
+        self._pink_ghost.draw()
+
+        self._pacman.tick()
+        self._red_ghost.cycleDirection()
+        self._cyan_ghost.cycleDirection()
+        self._green_ghost.cycleDirection()
+        self._pink_ghost.cycleDirection()
 
     def drawTitle(self):
         """Draw the title onto statistic screen."""
         x = self._display.get_width() / 2 - self._title.get_width() / 2
         y = 0
         self._display.blit(self._title, (x, y))
+
+    def drawStatistic(self):
+        """Draw statistic onto screen."""
+        self._display.blit(self._overall, (100, 100))
 
     def eventLoop(self):
         """Event loop for Statistic screen that just waits for keypress or window close action."""
@@ -67,4 +105,4 @@ class StatisticScreen:
             # all events has been processed - redraw the screen
             self.draw()
             pygame.display.update()
-            self._clock.tick(8)
+            self._clock.tick(5)
