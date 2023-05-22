f2e04168b35ee8520db6b5bae9af4056180fa0b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-05-22 08:53:54 +0200

    Stub for maze representation

---
+++ games/IoC/PoC/game/maze.py
@@ -0,0 +1,21 @@
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+"""Representation of maze in a game."""
+
+class Maze:
+    """Representation of maze in a game."""
+
+    def __init__(self):
+        pass
