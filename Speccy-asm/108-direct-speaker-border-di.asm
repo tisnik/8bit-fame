8bc02e176c60b21ed84a49cfad679380c1e52594
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-05 07:43:45 +0200

    Direct controlling the speaker/beeper + border color with interrupts disabled

---
+++ Speccy-asm/108-direct-speaker-border-di.asm
@@ -0,0 +1,27 @@
+ENTRY_POINT   equ $8000
+BEEPER_PORT   equ $fe
+
+
+
+	org ENTRY_POINT
+
+
+start:
+	ld   A, %00000111              ; původní hodnota zapisovaná na port
+	di                             ; zakázat přerušení
+loop:
+	xor  %00010001                 ; negace prvního a čtvrtého bitu
+	out  (BEEPER_PORT), A          ; zápis na port
+	call short_delay               ; počkat
+	jr   loop                      ; a opakovat celé znovu
+
+
+short_delay:
+	; zpožďovací rutina
+	; mění B (což nám nevadí)
+        ld   b, 98                    ; počitadlo zpožďovací smyčky
+delay_loop:
+	djnz delay_loop                ; opakovat smyčku s počitadlem v B
+	ret                            ; návrat z podprogramu
+
+end ENTRY_POINT
