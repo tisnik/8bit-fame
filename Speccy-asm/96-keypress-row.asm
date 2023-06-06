7b108aa30ce60f941925a35f3f2208f2a6bd7995
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-06 07:38:19 +0200

    Added description

--- Speccy-asm/96-keypress-row.asm
+++ Speccy-asm/96-keypress-row.asm
@@ -8,6 +8,9 @@
 ;
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
+;
+; Example #96:
+;    Keypress detection for all five keys in one row.
 
 ATTRIBUTE_ADR equ $5800
 ENTRY_POINT   equ $8000
