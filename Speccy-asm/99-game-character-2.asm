7d3bc401dc95a52966dac09cc48012f7ddba3376
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-09 15:52:01 +0200

    Added description

--- Speccy-asm/99-game-character-2.asm
+++ Speccy-asm/99-game-character-2.asm
@@ -8,6 +8,9 @@
 ;
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
+;
+; Example #99:
+;    Move game character (represented as color attribute) by keyboard; updated macros usage.
 
 ATTRIBUTE_ADR equ $5800
 ENTRY_POINT   equ $8000
