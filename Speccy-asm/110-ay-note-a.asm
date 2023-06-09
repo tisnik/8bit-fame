0e94527c942e8f4d1bcce2257282227ace8a774f
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-09 14:38:42 +0200

    A note on AY, version #2

---
+++ Speccy-asm/110-ay-note-a.asm
@@ -0,0 +1,38 @@
+ENTRY_POINT        equ $8000
+AY_SELECT_REGISTER equ $fffd
+AY_DATA_REGISTER   equ $bffd
+
+
+
+org ENTRY_POINT
+
+start:
+	ld      d, 0                   ; výška tónu (spodní bajt)
+	ld      e, 242
+	call    ay_write               ; zápis hodnoty do AY
+
+	ld      d, 1                   ; výška tónu (horní bajt)
+	ld      e, 7
+	call    ay_write               ; zápis hodnoty do AY
+
+	ld      d, 8                   ; hlasitost kanálu A
+	ld      e, 15                  ; maximální hlasitost
+	call    ay_write               ; zápis hodnoty do AY
+
+	ld      d, 7                   ; povolení výstupu z kanálu A
+	ld      e, %11111110           ; bitová maska
+	call    ay_write               ; zápis hodnoty do AY
+
+loop:
+	jr      loop                   ; nechceme návrat do BASICu
+
+
+ay_write:
+	ld      bc, AY_SELECT_REGISTER ; zápis do výběrového registru AY
+	out     (c), d                 ; index vybraného registru
+	ld      bc,AY_DATA_REGISTER    ; zápis do datového registru AY
+	out     (c), e                 ; zapisovaná hodnota
+	ret                            ; návrat ze subrutiny
+
+
+end ENTRY_POINT
