fe933fcb6c6a72710d7a664c41fa8db43accfcf0
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-16 09:20:03 +0200

    Two note played on AY-3-8912

---
+++ Speccy-asm/111-ay-two-notes.asm
@@ -0,0 +1,65 @@
+ENTRY_POINT        equ $8000
+AY_SELECT_REGISTER equ $fffd
+AY_DATA_REGISTER   equ $bffd
+
+
+
+org ENTRY_POINT
+
+start:
+	; nota A4 = 440.00 Hz
+	ld      a, 0                   ; výška tónu (spodní bajt) pro kanál A
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 251
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	ld      a, 1                   ; výška tónu (horní bajt) pro kanál A
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 0
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	; nota C4 = 261.63 Hz
+	ld      a, 2                   ; výška tónu (spodní bajt) pro kanál B
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 167
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	ld      a, 3                   ; výška tónu (horní bajt) pro kanál B
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 1
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	ld      a, 8                   ; hlasitost kanálu A
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 15                  ; maximální hlasitost
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	ld      a, 9                   ; hlasitost kanálu B
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 10
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	ld      a, 7                   ; povolení výstupu z kanálu A i kanálu B
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, %11111100           ; bitová maska
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+loop:
+	jr loop                        ; nechceme návrat do BASICu
+
+
+end ENTRY_POINT
