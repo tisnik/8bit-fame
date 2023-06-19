77e57ac94af4f273eb6561451190b1612f415854
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-19 10:09:12 +0200

    Low noise generated by AY

---
+++ Speccy-asm/115-ay-low-noise.asm
@@ -0,0 +1,35 @@
+ENTRY_POINT        equ $8000
+AY_SELECT_REGISTER equ $fffd
+AY_DATA_REGISTER   equ $bffd
+
+
+
+org ENTRY_POINT
+
+start:
+	ld      a, 5                   ; výška šumu
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 63
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	ld      a, 8                   ; hlasitost kanálu A
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, 15                  ; maximální hlasitost
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+	ld      a, 7                   ; povolení výstupu z kanálu A
+      	ld      bc, AY_SELECT_REGISTER
+	out     (c), a
+	ld      a, %11110111           ; bitová maska
+	ld      bc, AY_DATA_REGISTER
+	out     (c), a                 ; zápis hodnoty do AY
+
+loop:
+	jr loop                        ; nechceme návrat do BASICu
+
+
+end ENTRY_POINT
