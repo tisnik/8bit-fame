b47f9682dfbb64b7f8bec5e45ff47393dc42d5f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-03 09:10:47 +0200

    Link to article

--- Speccy-asm/103-beep.asm
+++ Speccy-asm/103-beep.asm
@@ -1,3 +1,15 @@
+; Example that is used in following article:
+;    Programujeme zvuky a hudbu na ZX Spectru
+;    https://www.root.cz/clanky/programujeme-zvuky-a-hudbu-na-zx-spectru/
+;
+; This article is part of series:
+;    "Vývoj pro slavné ZX Spectrum"
+;    https://www.root.cz/serialy/vyvoj-pro-slavne-zx-spectrum/
+;
+; Repository:
+;    https://github.com/tisnik/8bit-fame
+;
+
 ENTRY_POINT   equ $8000
 BEEPER        equ $03B5
 
