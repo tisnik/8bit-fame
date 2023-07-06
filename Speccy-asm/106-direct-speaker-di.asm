9dffb138cd7263b6a2e712dc1e5a9c7442ff47b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-06 09:10:52 +0200

    Added description

--- Speccy-asm/106-direct-speaker-di.asm
+++ Speccy-asm/106-direct-speaker-di.asm
@@ -9,6 +9,9 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #106:
+;    Direct speaker control via port.
+;    Interrupt is disabled during play.
 
 ENTRY_POINT   equ $8000
 BEEPER_PORT   equ $fe
