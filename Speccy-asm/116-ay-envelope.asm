0ac4653c86560af31e9e18b5e5e56d0d899cb129
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-14 10:34:33 +0200

    Added description

--- Speccy-asm/116-ay-envelope.asm
+++ Speccy-asm/116-ay-envelope.asm
@@ -9,6 +9,8 @@
 ; Repository:
 ;    https://github.com/tisnik/8bit-fame
 ;
+; Example #116:
+;    Play tone with non-constant envelope on AY-3-8912 chip.
 
 ENTRY_POINT        equ $8000
 AY_SELECT_REGISTER equ $fffd
