ae3f3671f5c83106dd4b151185ef2b0a3152f2df
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-07-24 11:33:11 +0200

    Renamed: offset to index

--- Speccy-asm/125-shifted-sprite.asm
+++ Speccy-asm/125-shifted-sprite.asm
@@ -15,8 +15,8 @@ ENTRY_POINT     equ $8000
 	org ENTRY_POINT
 
 
-draw_shifted_sprite MACRO offset
-	ld hl, SPRITE_ADR+(offset*24*4) ; adresa, od níž začíná maska spritu
+draw_shifted_sprite MACRO index
+	ld hl, SPRITE_ADR+(index*24*4) ; adresa, od níž začíná maska spritu
 	call draw_sprite
 endm
 
