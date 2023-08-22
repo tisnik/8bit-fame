33addc3db23eb46e89256b8bd92029d1342aa2f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-22 18:08:31 +0200

    Added link to source code in Git

--- Speccy-asm/02-blinking-attribute.asm
+++ Speccy-asm/02-blinking-attribute.asm
@@ -11,6 +11,9 @@
 ;
 ; Example #2:
 ;    Blinking color attribute
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/02-blinking-attribute.asm
 
 
 
