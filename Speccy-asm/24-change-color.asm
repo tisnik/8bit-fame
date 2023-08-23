e4f329e8b065a321352b03dce63c6a9a5b2b1738
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-23 09:25:03 +0200

    Added link to source code in Git

--- Speccy-asm/24-change-color.asm
+++ Speccy-asm/24-change-color.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #24:
 ;    Change character colors via control characters
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/24-change-color.asm
+
+
 
 ENTRY_POINT      equ $8000
 ROM_OPEN_CHANNEL equ $1601
