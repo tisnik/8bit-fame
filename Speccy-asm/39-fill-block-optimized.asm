c8c8aea1f3cc62e28971ab90047b4b8fb2c18c7e
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-23 09:25:04 +0200

    Added link to source code in Git

--- Speccy-asm/39-fill-block-optimized.asm
+++ Speccy-asm/39-fill-block-optimized.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #39:
 ;    Fill-in the 8x8 block of pixels by specified pattern, optimized variant.
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/39-fill-block-optimized.asm
+
+
 
 SCREEN_ADR      equ $4000
 ENTRY_POINT     equ $8000
