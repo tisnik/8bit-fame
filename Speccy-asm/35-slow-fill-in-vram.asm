ee2f7912a74131c95b3f0e4b638151c8f50331db
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-23 09:25:04 +0200

    Added link to source code in Git

--- Speccy-asm/35-slow-fill-in-vram.asm
+++ Speccy-asm/35-slow-fill-in-vram.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #35:
 ;    Fill-in the whole screen by ink color.
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/35-slow-fill-in-vram.asm
+
+
 
 SCREEN_ADR    equ $4000
 ENTRY_POINT   equ $8000
