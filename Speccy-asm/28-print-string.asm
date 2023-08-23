825cc4b9df05250bdec4d0ce2e55d512ed7f6445
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-23 09:25:03 +0200

    Added link to source code in Git

--- Speccy-asm/28-print-string.asm
+++ Speccy-asm/28-print-string.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #28:
 ;    Print whole string onto screen, optimized version
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/28-print-string.asm
+
+
 
 ENTRY_POINT      equ $8000
 ROM_OPEN_CHANNEL equ $1601
