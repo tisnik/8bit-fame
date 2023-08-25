6cb684ac8a5b3a2b1fe749a4518d3b5fd140b67a
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-25 08:37:53 +0200

    Added link to source code in Git

--- Speccy-asm/63-print-number-C.asm
+++ Speccy-asm/63-print-number-C.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #63:
 ;    Print largest supported number (9999) using standard ROM call.
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/63-print-number-C.asm
+
+
 
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
