84e700c65f71f29e51a2db010e14d3fbed268134
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-25 08:37:53 +0200

    Added link to source code in Git

--- Speccy-asm/67-print-flags-1.asm
+++ Speccy-asm/67-print-flags-1.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #67:
 ;    Print flags after arithmetic operation 1+2.
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/67-print-flags-1.asm
+
+
 
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
