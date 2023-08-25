cfad7bc3ae7878905c9cd5cd747d499065bc072f
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-25 08:37:53 +0200

    Added link to source code in Git

--- Speccy-asm/62-print-number-B.asm
+++ Speccy-asm/62-print-number-B.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #62:
 ;    Print negative number using standard ROM call.
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/62-print-number-B.asm
+
+
 
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
