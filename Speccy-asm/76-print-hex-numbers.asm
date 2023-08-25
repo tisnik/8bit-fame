68b5339ea10eb91ee28046cb8ee4410fa8588c78
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-25 08:37:54 +0200

    Added link to source code in Git

--- Speccy-asm/76-print-hex-numbers.asm
+++ Speccy-asm/76-print-hex-numbers.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #76:
 ;    Print sequence of hexadecimal numbers.
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/76-print-hex-numbers.asm
+
+
 
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
