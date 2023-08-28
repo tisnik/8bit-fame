2d106e94c796c836fd8b91a91bedff378dbdf38b
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-28 07:57:51 +0200

    Added link to source code in Git

--- Speccy-asm/87-copy-ascii-table-C.asm
+++ Speccy-asm/87-copy-ascii-table-C.asm
@@ -11,6 +11,11 @@
 ;
 ; Example #87:
 ;    Print ASCII table on screen + copy it to second part of screen using LDI+JP instructions.
+;
+; This source code is available at:
+;    https://github.com/tisnik/8bit-fame/blob/master/Speccy-asm/87-copy-ascii-table-C.asm
+
+
 
 SCREEN_ADR          equ $4000
 SCREEN_BLOCK_SIZE   equ 32*64
