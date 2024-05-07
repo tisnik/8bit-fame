5f72e03e3a6c8bec51dceb4fb92ce1d5766458a9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-07 12:02:46 +0200

    Importing examples into articles

---
+++ tools/import_into_article.py
@@ -0,0 +1,22 @@
+with open("x") as fin:
+    lines = fin.readlines()
+
+for line in lines:
+    line = line.strip()
+    print("<pre>")
+    with open("EndBASIC/" + line) as fin:
+        x = fin.read()
+        print(x.strip())
+    print("</pre>\n")
+    a = f"https://github.com/tisnik/most-popular-python-libs/blob/master/containers/{line}"
+    print(f"<p><div class=\"rs-tip-major\">Zdroj: <a href=\"{a}\">{a}</a></div></p>\n")
+
+    line = line.replace(".bas", ".txt")
+    try:
+        print("<pre>")
+        with open(line) as fin:
+            x = fin.read()
+            print(x.strip())
+        print("</pre>\n")
+    except Exception as e:
+        pass
