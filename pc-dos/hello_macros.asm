db5525d55b2a8c757fc7a769158cf9d13b0f0087
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-01 08:17:39 +0200

    Usage of macror

---
+++ pc-dos/hello_macros.asm
@@ -0,0 +1,40 @@
+; Program typu "Hello, world!" urceny pro DOS a prelozitelny assemblerem NASM
+;
+; preklad pomoci:
+;     nasm -f bin -o hello.com hello_macros.asm
+;
+; nebo pouze:
+;     nasm -o hello.com hello_macros.asm
+
+ 
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        mov     ah, 0x4c
+        int     0x21
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; tisk retezce na obrazovku
+%macro print 1
+        mov     dx, %1
+        mov     ah, 9
+        int     0x21
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        print message
+        wait_key
+        exit
+
+        ; retezec ukonceny znakem $
+message db "Hello, world!", 0x0d, 0x0a, "$"
