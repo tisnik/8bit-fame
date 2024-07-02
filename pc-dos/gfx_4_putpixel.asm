d3fe6d9b4a768cd2e0e556f54eba901983052e81
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-02 08:55:40 +0200

    Put pixel operation in gfx mode #4

---
+++ pc-dos/gfx_4_putpixel.asm
@@ -0,0 +1,48 @@
+; Vykresleni pixelu tou nejpomalejsi cestou - pres funkci BIOSu
+;
+; preklad pomoci:
+;     nasm -f bin -o gfx_4.com gfx_4_putpixel.asm
+;
+; nebo pouze:
+;     nasm -o gfx_4.com gfx_4_putpixel.asm
+
+ 
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        mov     ah, 0x4c
+        int     0x21
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; nastaveni grafickeho rezimu
+%macro gfx_mode 1
+        mov     ah, 0
+        mov     al, %1
+        int     0x10
+%endmacro
+
+; vykresleni pixelu pres BIOS
+%macro put_pixel 3
+        mov     ah, 0xc
+        xor     bx, bx    ; cislo stranky
+        mov     cx, %1    ; sloupec (X)
+        mov     dx, %2    ; radek (Y)
+        mov     al, %3    ; barva pixelu
+        int     0x10
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        gfx_mode 4
+        put_pixel 160, 100, 1
+        wait_key
+        exit
