c90bf6284b58b8f118bf79c1c3be955e7d68b527
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-02 08:55:17 +0200

    Line in graphics mode #4

---
+++ pc-dos/gfx_4_line.asm
@@ -0,0 +1,51 @@
+; Vykresleni usecky tou nejpomalejsi cestou - pres funkci BIOSu
+;
+; preklad pomoci:
+;     nasm -f bin -o gfx_4.com gfx_4_putpixel.asm
+;
+; nebo pouze:
+;     nasm -o gfx_4.com gfx_4_putpixel.asm
+
+ 
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        mov     ah, 0x4c
+        int     0x21
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; nastaveni grafickeho rezimu
+%macro gfx_mode 1
+        mov     ah, 0
+        mov     al, %1
+        int     0x10
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        gfx_mode 4
+
+        mov     ah, 0xc
+        mov     al, 1     ; barva pixelu
+        xor     bx, bx    ; cislo stranky
+        mov     cx, 0     ; sloupec
+        mov     dx, 0     ; radek
+opak:
+        int     0x10
+        inc     al
+        inc     cx        ; x+=1
+        inc     dx        ; y+=1
+        cmp     cx, 200   ; kontrola, zda jsme dosahli posledni souradnice
+        jne     opak      ; pokud ne, dalsi iterace
+
+        wait_key
+        exit
