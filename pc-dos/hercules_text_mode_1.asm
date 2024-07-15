f32d353b4d07578b8378998054f959920fa307ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-15 09:41:31 +0200

    Hercules graphics card text mode

---
+++ pc-dos/hercules_text_mode_1.asm
@@ -0,0 +1,41 @@
+; Textovy rezim karty Hercules s rozlisenim 80x25 znaku.
+;
+; preklad pomoci:
+;     nasm -f bin -o hercules.com hercules_text_mode_1.asm
+;
+; nebo pouze:
+;     nasm -o hercules.com hercules_text_mode_1.asm
+
+
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        mov     ah, 0x4c
+        int     0x21
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        mov ax, 0xb000
+        mov es, ax
+        mov di, 0       ; nyni ES:DI obsahuje adresu prvniho znaku ve video RAM
+
+        mov cx, 80*25   ; pocet zapisovanych znaku
+        mov al, 0       ; kod zapisovaneho znaku
+opak:
+        stosb           ; zapis znaku + atributu
+        stosb
+        inc al          ; dalsi znak/atribut
+        loop opak       ; opakujeme CX-krat
+
+        wait_key
+        exit
