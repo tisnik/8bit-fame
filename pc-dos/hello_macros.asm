e5c2808ac452b9d209ab8333570073f78fc1d1ea
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-15 09:41:31 +0200

    Link to article where this example is used

--- pc-dos/hello_macros.asm
+++ pc-dos/hello_macros.asm
@@ -1,6 +1,15 @@
 ; Program typu "Hello, world!" urceny pro DOS a prelozitelny assemblerem NASM
 ; Vyuziti maker pro zjednoduseni hlavni casti programu
 ;
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu.
+;
+; Clanek, kde je tento priklad pouzit:
+; Vývoj her a grafických dem pro oslavovanou i nenáviděnou platformu PC (první kroky)
+; https://www.root.cz/clanky/vyvoj-her-a-grafickych-dem-pro-oslavovanou-i-nenavidenou-platformu-pc-prvni-kroky/
+;
+;
 ; preklad pomoci:
 ;     nasm -f bin -o hello.com hello_macros.asm
 ;
