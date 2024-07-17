a62c2a14b9b2708d188df2199e59cf5069407a7f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-17 12:43:53 +0200

    Updated comments

--- pc-dos/hello_macros.asm
+++ pc-dos/hello_macros.asm
@@ -5,7 +5,7 @@
 ; Tento demonstracni priklad je pouzity v serialu o programovani
 ; grafickych dem a her na PC v DOSu.
 ;
-; Clanek, kde je tento priklad pouzit:
+; Clanek, kde je tento demonstracni priklad pouzit:
 ; Vývoj her a grafických dem pro oslavovanou i nenáviděnou platformu PC (první kroky)
 ; https://www.root.cz/clanky/vyvoj-her-a-grafickych-dem-pro-oslavovanou-i-nenavidenou-platformu-pc-prvni-kroky/
 ;
@@ -47,4 +47,5 @@ start:
         exit
 
         ; retezec ukonceny znakem $
+        ; (tato data jsou soucasti vysledneho souboru typu COM)
 message db "Hello, world!", 0x0d, 0x0a, "$"
