c237463e5746de3dca2f46adf28df140281c91f4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-17 12:43:56 +0200

    Updated comments

--- pc-dos/hello_wait.asm
+++ pc-dos/hello_wait.asm
@@ -5,7 +5,7 @@
 ; Tento demonstracni priklad je pouzity v serialu o programovani
 ; grafickych dem a her na PC v DOSu.
 ;
-; Clanek, kde je tento priklad pouzit:
+; Clanek, kde je tento demonstracni priklad pouzit:
 ; Vývoj her a grafických dem pro oslavovanou i nenáviděnou platformu PC (první kroky)
 ; https://www.root.cz/clanky/vyvoj-her-a-grafickych-dem-pro-oslavovanou-i-nenavidenou-platformu-pc-prvni-kroky/
 ;
@@ -35,4 +35,5 @@ start:
         int     0x21
 
         ; retezec ukonceny znakem $
+        ; (tato data jsou soucasti vysledneho souboru typu COM)
 message db "Hello, world!", 0x0d, 0x0a, "$"
