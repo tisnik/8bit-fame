88103fb13cb7e6713db03e0770807b098d7eaeca
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-23 09:44:14 +0200

    Link to article where this example is used

--- pc-dos/cga_text_mode_intensity.asm
+++ pc-dos/cga_text_mode_intensity.asm
@@ -1,6 +1,15 @@
 ; Textovy rezim karty CGA.
 ; Zmena vyznamu nejvyssiho bitu atributu.
 ;
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu.
+;
+; Clanek, kde je tento demonstracni priklad pouzit:
+; Textové režimy grafické karty CGA a řadič displeje Motorola 6845
+; https://www.root.cz/clanky/textove-rezimy-graficke-karty-cga-a-radic-displeje-motorola-6845/
+;
+;
 ; preklad pomoci:
 ;     nasm -f bin -o gfx_text.com cga_text_mode_intensity.asm
 ;
