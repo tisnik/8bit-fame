f724be3aac5a23a3b97b8d820cb825b8e52e4bcc
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-01 10:29:11 +0200

    Putpixel operation in mode 320x200x256, slow variant

---
+++ pc-dos/vga_320x200_putpixel_1.asm
@@ -0,0 +1,94 @@
+; Graficky rezim karty VGA s rozlisenim 320x200 pixelu.
+; Vykresleni barevnych usecek.
+;
+; preklad pomoci:
+;     nasm -f bin -o vga.com vga_320x200_putpixel_1.asm
+;
+; nebo pouze:
+;     nasm -o vga.com vga_320x200_putpixel_1.asm
+
+
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        mov     ah, 0x4c
+        int     0x21
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; nastaveni grafickeho rezimu
+%macro gfx_mode 1
+        mov     ah, 0
+        mov     al, %1
+        int     0x10
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        gfx_mode 0x13       ; nastaveni rezimu 320x200 se sestnacti barvami
+        mov ax, 0xa000      ; video RAM v textovem rezimu
+        mov es, ax
+        xor di, di          ; nyni ES:DI obsahuje adresu prvniho pixelu ve video RAM
+
+        mov ax, 0
+opak:
+        mov bx, ax          ; y-ová souřadnice
+
+        push ax
+        mov cl, al          ; barva
+        call putpixel       ; vykreslení pixelu
+        pop ax
+
+        push ax
+        mov cl, al          ; barva
+        add ax, 10          ; horizontalni posun useky
+        call putpixel       ; vykreslení pixelu
+        pop ax
+
+        push ax
+        mov cl, al          ; barva
+        add ax, 20          ; horizontalni posun useky
+        call putpixel       ; vykreslení pixelu
+        pop ax
+
+        inc ax              ; pusun x+=1, y+=1
+        cmp ax, 200         ; hranice obrazovky?
+        jne opak            ; ne-opakujeme
+
+        wait_key            ; cekani na klavesu
+        exit                ; navrat do DOSu
+
+; Vykresleni pixelu
+; AX - x-ova souradnice
+; BX - y-ova souradnice (staci len BL)
+; CL - barva
+putpixel:
+        mov dx, 0xa000     ; zacatek stranky video RAM
+        mov es, dx         ; nyni obsahuje ES stranku video RAM
+
+        mov di, ax         ; horizontalni posun pocitany v bajtech
+
+        mov ax, bx         ; y-ova souradnice
+        shl ax, 1          ; y*2
+        shl ax, 1          ; y*4
+        shl ax, 1          ; y*8
+        shl ax, 1          ; y*16
+        shl ax, 1          ; y*32
+        shl ax, 1          ; y*64
+        add di, ax         ; pricist cast y-oveho posunu
+        shl ax, 1          ; y*128
+        shl ax, 1          ; y*256
+        add di, ax         ; pricist zbytek y-oveho posunu
+                           ; -> y*64 + y*256 = y*320
+
+        mov [es:di], cl     ; vlastni vykresleni pixelu
+
+        ret
