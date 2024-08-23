309ef4d10f383f4d87514a85478ec0636007e25c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-23 20:03:57 +0200

    IO library

---
+++ pc-dos/io.asm
@@ -0,0 +1,22 @@
+;-----------------------------------------------------------------------------
+; Symboly, makra a subrutiny pro zjednoduseni I/O operaci
+;-----------------------------------------------------------------------------
+
+
+
+;-----------------------------------------------------------------------------
+; makra
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        mov     ah, 0x4c
+        int     0x21
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
