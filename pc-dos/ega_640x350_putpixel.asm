d518c0262c57ab289b4358388a1a5dbe5f6c772a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-05 09:36:46 +0200

    CPU and output file format specification

--- pc-dos/ega_640x350_putpixel.asm
+++ pc-dos/ega_640x350_putpixel.asm
@@ -18,6 +18,11 @@
 ; nebo pouze:
 ;     nasm -o ega.com ega_640x350_putpixel.asm
 
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
 
 ;-----------------------------------------------------------------------------
 
