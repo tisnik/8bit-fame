119c9d4925e92cd13a78d960946d6a5d017768d3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-06 09:25:04 +0200

    CPU and output file specification

--- pc-dos/ega_gfx_mode_640x200.asm
+++ pc-dos/ega_gfx_mode_640x200.asm
@@ -16,6 +16,11 @@
 ; nebo pouze:
 ;     nasm -o ega.com ega_gfx_mode_640x200.asm
 
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
 
 ;-----------------------------------------------------------------------------
 
