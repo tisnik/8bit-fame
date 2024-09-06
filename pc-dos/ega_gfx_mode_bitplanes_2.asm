bada36cd17b6b32bf17a8ee4680c0870a511de54
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-06 09:25:04 +0200

    CPU and output file specification

--- pc-dos/ega_gfx_mode_bitplanes_2.asm
+++ pc-dos/ega_gfx_mode_bitplanes_2.asm
@@ -17,6 +17,11 @@
 ; nebo pouze:
 ;     nasm -o ega.com ega_gfx_mode_bitplanes_2.asm
 
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
 
 ;-----------------------------------------------------------------------------
 
