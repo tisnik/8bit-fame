071e290aae4bc2dc38e28fdc44bc538c161d9df7
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-10 08:17:32 +0200

    CPU and output file specification

--- pc-dos/ega_standard_font.asm
+++ pc-dos/ega_standard_font.asm
@@ -17,6 +17,11 @@
 ; nebo pouze:
 ;     nasm -o ega.com ega_standard_font.asm
 
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
 
 ;-----------------------------------------------------------------------------
 
