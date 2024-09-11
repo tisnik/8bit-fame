c8019c23c722588a4c4c1c0e045994154a36fe45
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-11 10:35:35 +0200

    JMP instruction usage

---
+++ pc-dos/instruction_jump.asm
@@ -0,0 +1,24 @@
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp     cil1
+
+zpet:
+        ; tisk retezce na obrazovku
+        mov     dx, message
+        mov     ah, 9
+        int     0x21
+
+        ; ukonceni procesu a navrat do DOSu
+        mov     ah, 0x4c
+        int     0x21
+
+cil2:   jmp cil3
+cil1:   jmp cil2
+
+        ; retezec ukonceny znakem $
+        ; (tato data jsou soucasti vysledneho souboru typu COM)
+message db "Hello, world!", 0x0d, 0x0a, "$"
+
+cil3:   jmp zpet
