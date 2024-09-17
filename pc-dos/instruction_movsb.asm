f7d2ff5bba0591aea23955bdf658cd8f6dd0984b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-17 10:18:26 +0200

    MOVSB instruction

---
+++ pc-dos/instruction_movsb.asm
@@ -0,0 +1,50 @@
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        ret
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; tisk retezce na obrazovku
+%macro print 1
+        mov     dx, %1
+        mov     ah, 9
+        int     0x21
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        print message
+
+        push cs
+        pop  es   ; ES:DI obsahuje adresu prvniho znaku ve zprave
+        mov  di, message
+
+        push cs
+        pop  ds   ; DS:SI obsahuje adresu prvniho znaku ve zprave
+        mov  si, replacement
+
+        mov  cx, 12 ; pocet prepisovanych znaku
+opak:
+        movsb     ; precteni znaku a posun adresy
+                  ; zapis znaku a posun adresy
+        loop opak
+
+        print message
+
+        wait_key
+        exit
+
+        ; retezec ukonceny znakem $
+        ; (tato data jsou soucasti vysledneho souboru typu COM)
+message     db "Hello, world!", 0x0d, 0x0a, "$"
+replacement db "Zdravim svet"
