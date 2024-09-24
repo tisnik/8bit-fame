8d447bca3a413b9844a89c6b49cec0a3e6a06d32
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-24 09:17:47 +0200

    Instruction addressing

---
+++ pc-dos/instruction_addressing.asm
@@ -0,0 +1,35 @@
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        add ax, bx
+        mov ax, [bx]
+        mov ax, [si]
+        mov ax, [di]
+        mov ax, [bp]
+
+        mov ax, [bx+0x1f]
+        mov ax, [si+0x1f]
+        mov ax, [di+0x1f]
+        mov ax, [bp+0x1f]
+
+        mov ax, [bx+0xeeff]
+        mov ax, [si+0xeeff]
+        mov ax, [di+0xeeff]
+        mov ax, [bp+0xeeff]
+
+        mov ax, [bp+si]
+        mov ax, [bp+di]
+        mov ax, [bp+si+0x1f]
+        mov ax, [bp+di+0xeeff]
+        mov al, 42
+        mov ax, 42
+        inc byte [123]
+        inc word [123]
+        inc byte [10000]
+        inc word [10000]
