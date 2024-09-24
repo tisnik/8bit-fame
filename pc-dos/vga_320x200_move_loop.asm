6bd12dc4f35d12fcfef38d2b4d2da2e58e1c94f6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-24 09:17:23 +0200

    Move/transfer loop

---
+++ pc-dos/vga_320x200_move_loop.asm
@@ -0,0 +1,75 @@
+; Graficky rezim karty VGA s rozlisenim 320x200 pixelu.
+; Vykresleni rastroveho obrazku.
+;
+; preklad pomoci:
+;     nasm -f bin -o vga.com vga_320x200_move_loop.asm
+;
+; nebo pouze:
+;     nasm -o vga.com vga_320x200_move_loop.asm
+
+
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        mov     ah, 0x4c
+        int     0x21
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; nastaveni grafickeho rezimu
+%macro gfx_mode 1
+        mov     ah, 0
+        mov     al, %1
+        int     0x10
+%endmacro
+
+; paleta ve stupnich sedi
+%macro grayscale_palette 0
+        mov ax, 0x1010      ; cislo sluzby a podsluzby VGA BIOSu
+        xor bl, bl          ; index barvy
+next_dac:
+        mov ch, bl          ; prvni barvova slozka
+        shr ch, 1
+        shr ch, 1
+        mov cl, ch          ; druha barvova slozka
+        mov dh, ch          ; treti barvova slozka
+        int 0x10            ; modifikace mapovani v DAC
+        inc bl              ; zvysit index v DAC
+        jnz next_dac        ; nastavit dalsi barvu, dokud nedosahneme hodnoty 256
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        gfx_mode 0x13       ; nastaveni rezimu 320x200 s 256 barvami
+        grayscale_palette   ; nastaveni palety se stupni sedi
+
+        mov ax, cs
+        mov ds, ax
+        mov si, image       ; nyni DS:SI obsahuje adresu prvniho bajtu v obrazku
+
+        mov ax, 0xa000      ; video RAM v textovem rezimu
+        mov es, ax
+        xor di, di          ; nyni ES:DI obsahuje adresu prvniho pixelu ve video RAM
+
+        mov cx, 320*200     ; pocet zapisovanych bajtu (=pixelu)
+opak:
+        mov al, ds:[si]     ; načtení ze zdrojového bloku
+        mov es:[di], al     ; uložení do cílového bloku
+        inc si              ; zvýšení ukazatelů
+        inc di
+        loop opak           ; opakování CX-krát
+
+        wait_key            ; cekani na klavesu
+        exit                ; navrat do DOSu
+
+; pridani binarnich dat s rastrovym obrazkem
+image:
+    incbin "image_320x200.bin"
