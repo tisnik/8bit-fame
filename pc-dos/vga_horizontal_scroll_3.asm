03e552723e187516c0ecb00ee5c034de638bf27a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-09 08:27:26 +0200

    CPU type specification

--- pc-dos/vga_horizontal_scroll_3.asm
+++ pc-dos/vga_horizontal_scroll_3.asm
@@ -13,6 +13,11 @@
 
 ;-----------------------------------------------------------------------------
 
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
+;-----------------------------------------------------------------------------
+
 ; registry karty VGA
 ATTRIBUTE_CONTROLLER equ 0x3c0
 SEQUENCER_INDEX      equ 0x3c4
