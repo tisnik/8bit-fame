b354ecab0faea42a2edc9c32c0d3841115f6cbe6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-10 14:54:19 +0200

    CPU model specification

--- pc-dos/vga_320x400_bitblt_2.asm
+++ pc-dos/vga_320x400_bitblt_2.asm
@@ -10,6 +10,12 @@
 ;     nasm -o vga.com vga_320x400_bitblt_2.asm
 
 
+
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
 ;-----------------------------------------------------------------------------
 
 ; registry karty VGA
