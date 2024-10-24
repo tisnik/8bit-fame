76a176b0427453a91a33af879587a14a63099e41
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-24 08:39:03 +0200

    CPU type specification

--- pc-dos/sound_play_pitch.asm
+++ pc-dos/sound_play_pitch.asm
@@ -7,6 +7,11 @@
 ; nebo pouze:
 ;     nasm -o sound_play_pitch.com sound_play_pitch.asm
 
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
  
 ;-----------------------------------------------------------------------------
 
