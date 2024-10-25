c581415c61bcd291ce6dd503caf6a77cae3a3993
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-25 09:23:40 +0200

    CPU type specification

--- pc-dos/hello_shorter.asm
+++ pc-dos/hello_shorter.asm
@@ -16,6 +16,11 @@
 ; nebo pouze:
 ;     nasm -o hello.com hello_shorter.asm
 
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 8086        ; specifikace pouziteho instrukcniho souboru
+
  
 ;-----------------------------------------------------------------------------
 org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
