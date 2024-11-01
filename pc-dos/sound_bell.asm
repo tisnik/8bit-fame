193b852933a7e20a2bcb43da125a0b28cb1a11c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-01 08:17:05 +0100

    Added link to series of articles about assembly on IBM PC

--- pc-dos/sound_bell.asm
+++ pc-dos/sound_bell.asm
@@ -1,6 +1,11 @@
 ; "Zobrazeni" znaku Bell
 ;
 ;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
+;
 ; preklad pomoci:
 ;     nasm -f bin -o sound_bell.com sound_bell.asm
 ;
