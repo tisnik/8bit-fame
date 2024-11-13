b5716f4e8d33abf2102dedd10baa183817b6b7d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-13 09:23:43 +0100

    Added link to serie of articles about assembly on x86

--- pc-dos/vga_gfx_mode_unchained_step_1.asm
+++ pc-dos/vga_gfx_mode_unchained_step_1.asm
@@ -1,6 +1,10 @@
 ; Graficky rezim karty VGA s rozlisenim 320x200 pixelu.
 ; Vypnuti zretezeni bitovych rovin.
 ;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
 ; preklad pomoci:
 ;     nasm -f bin -o vga.com vga_gfx_mode_unchained_step_1.asm
 ;
