ace1a5ccad923bab25d69e31fe7c8489818c9b94
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-28 08:44:06 +0100

    Added link to serie of articles

--- pc-dos/instruction_print_hex.asm
+++ pc-dos/instruction_print_hex.asm
@@ -1,5 +1,9 @@
 ; Instrukcni soubor mikroprocesoru Intel 8088 a Intel 8086
 ;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
 ;-----------------------------------------------------------------------------
 
 BITS 16         ; 16bitovy vystup pro DOS
