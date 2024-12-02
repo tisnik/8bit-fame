0a12b9322058e9a309e293ddd38b7812b2ba42b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-02 08:49:58 +0100

    8, 16, and 32bit INC instruction in 32bit mode

---
+++ pc-dos/instruction_inc_32bit_B.asm
@@ -0,0 +1,37 @@
+; Instrukcni soubor mikroprocesoru Intel 80386.
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
+;-----------------------------------------------------------------------------
+
+BITS 32         ; 16bitovy vystup pro DOS
+CPU 386         ; specifikace pouziteho instrukcniho souboru
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        ret
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+	xor  al, al
+	inc  al
+
+        xor  ax, ax
+	inc  ax
+
+	xor  eax, eax
+	inc  eax
+
+        wait_key
+        exit
