0505be470d62b84f5054a3fb09b19c9d7769b03e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-03 08:19:45 +0100

    Various PUSH and POP variants

---
+++ pc-dos/instruction_push_pop_B.asm
@@ -0,0 +1,42 @@
+; Instrukcni soubor mikroprocesoru Intel 80386.
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 386         ; specifikace pouziteho instrukcniho souboru
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        ret
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        push cs
+	push ds
+	push ss
+	push es
+	push fs
+	push gs
+
+	pop  gs
+	pop  fs
+	pop  es
+	pop  ss
+	pop  ds
+        pop  cs
+
+        wait_key
+        exit
