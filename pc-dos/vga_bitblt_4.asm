e5c074572e13bd1ef05fe1f1180dc026838a2308
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-06 08:38:00 +0100

    Added link to serie of articles

--- pc-dos/vga_bitblt_4.asm
+++ pc-dos/vga_bitblt_4.asm
@@ -5,6 +5,10 @@
 ; Vyber bitovych rovin pro zapis.
 ; Vyber bitove roviny pro cteni.
 ;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
 ; preklad pomoci:
 ;     nasm -f bin -o vga.com vga_bitblt_4.asm
 ;
