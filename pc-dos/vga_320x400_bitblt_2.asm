6f5b43f677222ae0d927e826d2b7f0ea332ca79e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-06 08:38:00 +0100

    Added link to serie of articles

--- pc-dos/vga_320x400_bitblt_2.asm
+++ pc-dos/vga_320x400_bitblt_2.asm
@@ -3,6 +3,10 @@
 ; Vykresleni rastroveho obrazku postupne do vsech bitovych rovin.
 ; Bitovy prenos (dokonceny).
 ;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
 ; preklad pomoci:
 ;     nasm -f bin -o vga.com vga_320x400_bitblt_2.asm
 ;
