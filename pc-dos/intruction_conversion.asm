85604c0ed07e0b68321cfb8330fa5bed546a239b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-12-10 08:54:52 +0100

    Data conversion instructions

---
+++ pc-dos/intruction_conversion.asm
@@ -0,0 +1,20 @@
+; Instrukcni soubor mikroprocesoru Intel 80386.
+; Test instrukci pro konverzi dat.
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU 386         ; specifikace pouziteho instrukcniho souboru
+
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+	cbw
+	cwd
+	cwde
+	cdq
