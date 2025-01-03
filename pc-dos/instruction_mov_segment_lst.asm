6b91bf5b1b915c6e1294a1d28a307ed4c67bbcb9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-03 09:53:19 +0100

    Expand all tabs + reformatted comments

--- pc-dos/instruction_mov_segment_lst.asm
+++ pc-dos/instruction_mov_segment_lst.asm
@@ -25,20 +25,20 @@
     25                                  org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
     26                                  
     27                                  start:
-    28 00000000 8CD8                    	mov  ax, ds
-    29 00000002 8CC0                    	mov  ax, es
-    30 00000004 8CE0                    	mov  ax, fs
-    31 00000006 8CE8                    	mov  ax, gs
-    32 00000008 8CD0                    	mov  ax, ss
-    33 0000000A 8CC8                    	mov  ax, cs
+    28 00000000 8CD8                            mov  ax, ds
+    29 00000002 8CC0                            mov  ax, es
+    30 00000004 8CE0                            mov  ax, fs
+    31 00000006 8CE8                            mov  ax, gs
+    32 00000008 8CD0                            mov  ax, ss
+    33 0000000A 8CC8                            mov  ax, cs
     34                                  
-    35 0000000C 31C0                    	xor  ax, ax
-    36 0000000E 8ED8                    	mov  ds, ax
-    37 00000010 8EC0                    	mov  es, ax
-    38 00000012 8EE0                    	mov  fs, ax
-    39 00000014 8EE8                    	mov  gs, ax
-    40 00000016 8ED0                    	mov  ss, ax
-    41 00000018 8EC8                    	mov  cs, ax  ; toto neni dobry napad!!!
+    35 0000000C 31C0                            xor  ax, ax
+    36 0000000E 8ED8                            mov  ds, ax
+    37 00000010 8EC0                            mov  es, ax
+    38 00000012 8EE0                            mov  fs, ax
+    39 00000014 8EE8                            mov  gs, ax
+    40 00000016 8ED0                            mov  ss, ax
+    41 00000018 8EC8                            mov  cs, ax  ; toto neni dobry napad!!!
     42                                  
     43                                          wait_key
     20 0000001A 31C0                <1>  xor ax, ax
