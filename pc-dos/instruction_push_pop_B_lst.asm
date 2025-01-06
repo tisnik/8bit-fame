241f315088429ee520bc6d982a989cf45d98a802
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-06 09:24:13 +0100

    Expand all tabs + reformatted comments

--- pc-dos/instruction_push_pop_B_lst.asm
+++ pc-dos/instruction_push_pop_B_lst.asm
@@ -25,17 +25,17 @@
     25                                  
     26                                  start:
     27 00000000 0E                              push cs
-    28 00000001 1E                      	push ds
-    29 00000002 16                      	push ss
-    30 00000003 06                      	push es
-    31 00000004 0FA0                    	push fs
-    32 00000006 0FA8                    	push gs
+    28 00000001 1E                              push ds
+    29 00000002 16                              push ss
+    30 00000003 06                              push es
+    31 00000004 0FA0                            push fs
+    32 00000006 0FA8                            push gs
     33                                  
-    34 00000008 0FA9                    	pop  gs
-    35 0000000A 0FA1                    	pop  fs
-    36 0000000C 07                      	pop  es
-    37 0000000D 17                      	pop  ss
-    38 0000000E 1F                      	pop  ds
+    34 00000008 0FA9                            pop  gs
+    35 0000000A 0FA1                            pop  fs
+    36 0000000C 07                              pop  es
+    37 0000000D 17                              pop  ss
+    38 0000000E 1F                              pop  ds
     39 0000000F 0F                              pop  cs
     39          ******************       warning: instruction obsolete and removed from the target CPU [-w+obsolete-removed]
     40                                  
