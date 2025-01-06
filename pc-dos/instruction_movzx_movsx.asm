dad9650cef751f4562a7e2a41714f15ff62cfd60
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-06 09:24:13 +0100

    Expand all tabs + reformatted comments

--- pc-dos/instruction_movzx_movsx.asm
+++ pc-dos/instruction_movzx_movsx.asm
@@ -14,13 +14,13 @@ CPU 386         ; specifikace pouziteho instrukcniho souboru
 org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
 
 start:
-	movzx bx, bl
-	movzx ebx, bl
-	movzx ebx, bx
-	movzx bx, byte [start] 
-	movzx ebx, word [start] 
-	movsx bx, bl
-	movsx ebx, bl
-	movsx ebx, bx
-	movsx bx, byte [start] 
-	movsx ebx, word [start] 
+        movzx bx, bl
+        movzx ebx, bl
+        movzx ebx, bx
+        movzx bx, byte [start] 
+        movzx ebx, word [start] 
+        movsx bx, bl
+        movsx ebx, bl
+        movsx ebx, bx
+        movsx bx, byte [start] 
+        movsx ebx, word [start] 
