681f384c13c59a684b6c05a56aa6d130ee727d19
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-07 10:00:45 +0100

    Link to serie of articles

--- pc-dos/fpu_divide_by_zero.asm
+++ pc-dos/fpu_divide_by_zero.asm
@@ -1,4 +1,8 @@
 ;-----------------------------------------------------------------------------
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
 
 ;-----------------------------------------------------------------------------
 
