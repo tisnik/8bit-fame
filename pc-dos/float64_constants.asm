dacbe0aaf780b1d7e9b793274a063dbb523f38dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-15 09:38:16 +0100

    Link to article

--- pc-dos/float64_constants.asm
+++ pc-dos/float64_constants.asm
@@ -3,7 +3,11 @@
 ; Tento demonstracni priklad je pouzity v serialu o programovani
 ; grafickych dem a her na PC v DOSu:
 ; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
-
+;
+; Clanek, kde je tento demonstracni priklad pouzit:
+; Matematické koprocesory na 80×86 pro výpočty s plovoucí řádovou čárkou
+; https://www.root.cz/clanky/matematicke-koprocesory-na-80-86-pro-vypocty-s-plovouci-radovou-carkou/
+;
 ;-----------------------------------------------------------------------------
 
 org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
