49566a28ec64ac8e6b1ff08cde48273bfe5c8676
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-17 09:02:46 +0100

    FPU division operator

---
+++ pc-dos/fpu_divide.asm
@@ -0,0 +1,27 @@
+;-----------------------------------------------------------------------------
+; Zakladni aritmeticke operace provadena matematickym koprocesorem: deleni
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
+;-----------------------------------------------------------------------------
+
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main             ; skok na zacatek kodu
+
+%include "io.asm"            ; nacist symboly, makra a podprogramy
+%include "print.asm"         ; nacist symboly, makra a podprogramy
+
+main:
+        fld1                 ; nacteni FP konstanty 1.0
+        fld1                 ; nacteni FP konstanty 0.0
+        fld1                 ; nacteni FP konstanty 0.0
+        faddp                ; součet, takže na zásobníku budou konstanty 1.0 a 2.0
+        fdivp                ; vypocet podilu
+        print_float32_as_hex ; zobrazeni FP hodnoty v hexadecimalnim tvaru
+
+        wait_key             ; cekani na klavesu
+        exit                 ; navrat do DOSu
