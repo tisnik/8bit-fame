b6e91f21472bd4a9df93ffceb2c8018bed1a1945
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-20 09:07:20 +0100

    FPU detection

---
+++ pc-dos/fpu_detection.asm
@@ -0,0 +1,49 @@
+; Otestovani, jestli je nainstalovan matematicky koprocesor.
+; Kompatibilni i s cipem Intel 80286
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+CPU  286        ; specifikace pouziteho instrukcniho souboru
+
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        ret
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; tisk retezce na obrazovku
+%macro print 1
+        mov     dx, %1
+        mov     ah, 9
+        int     0x21
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100                     ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        smsw ax                    ; prenos MSW do registru AX
+        test ax, 0x4               ; test tretiho nejnizsiho bitu MSW (EM)
+
+        jz fpu_present             ; nulovy bit? -> 80287 je nainstalovan
+        print fpu_not_present_message  ; nenulovy bit? -> koprocesor chybi
+        jmp end
+fpu_present:
+        print fpu_present_message
+end:
+        wait_key                   ; cekani na stisk klavesy
+        exit                       ; navrat do DOSu
+
+; datova cast
+fpu_present_message:
+    db "FP unit is present", 0x0a, 0x0d, "$"
+
+fpu_not_present_message:
+    db "FP unit is NOT present", 0x0a, 0x0d, "$"
