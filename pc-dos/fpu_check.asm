03753c517a8113a50bac538443e6ae465134e33b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-20 09:07:14 +0100

    FPU detection

---
+++ pc-dos/fpu_check.asm
@@ -0,0 +1,68 @@
+; Otestovani, jestli je k dispozici matematický koprocesor.
+; Kompatibilni s cipem Intel 80286
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+
+;-----------------------------------------------------------------------------
+
+; ukonceni procesu a navrat do DOSu
+%macro exit 0
+        ret
+%endmacro
+
+; vyprazdneni bufferu klavesnice a cekani na klavesu
+%macro wait_key 0
+        xor     ax, ax
+        int     0x16
+%endmacro
+
+; tisk retezce na obrazovku
+%macro print 1
+        mov     dx, %1
+        mov     ah, 9
+        int     0x21
+%endmacro
+
+;-----------------------------------------------------------------------------
+org  0x100                         ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+main:
+        fninit
+        mov cx, 100                ; nechame koprocesor "vydechnout"
+
+ .wait:
+        loop .wait
+
+        fnstcw word [test_word]    ; ulozeni ridiciho slova
+        cmp word [test_word], 0x03FF  ; vychozi bitova pole ukladana cipem 8087
+        je  fpu_8087_present
+
+        cmp word [test_word], 0x037F  ; vychozi bitova pole ukladana cipem 80287 a vyssim
+        je  fpu_present
+
+        print fpu_not_present_message
+        jmp end
+fpu_8087_present:
+        print fpu_8087_present_message
+        jmp end
+fpu_present:
+        print fpu_present_message
+end:
+        wait_key                   ; cekani na stisk klavesy
+        exit                       ; navrat do DOSu
+
+; datova cast
+test_word: dw 0
+
+fpu_8087_present_message:
+    db "Math coprocessor 8087 is present", 0x0a, 0x0d, "$"
+
+fpu_present_message:
+    db "Math coprocessor >8087 is present", 0x0a, 0x0d, "$"
+
+fpu_not_present_message:
+    db "Math coprocessor is NOT present", 0x0a, 0x0d, "$"
