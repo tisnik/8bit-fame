7e0b93675d4ce1dd63b5a5e05fc4b5ad8a866aa3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-20 09:07:01 +0100

    Fixed comments

--- pc-dos/fpu_divide_r.asm
+++ pc-dos/fpu_divide_r.asm
@@ -18,8 +18,8 @@ start:
 
 main:
         fld1                 ; nacteni FP konstanty 1.0
-        fld1                 ; nacteni FP konstanty 0.0
-        fld1                 ; nacteni FP konstanty 0.0
+        fld1                 ; nacteni FP konstanty 1.0
+        fld1                 ; nacteni FP konstanty 1.0
         faddp                ; součet, takže na zásobníku budou konstanty 1.0 a 2.0
         fdivrp               ; vypocet podilu
         print_float32_as_hex ; zobrazeni FP hodnoty v hexadecimalnim tvaru
