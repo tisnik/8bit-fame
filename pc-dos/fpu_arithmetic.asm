c20b2975e53c4857e9233ee14efe09ab76b345e3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-22 08:49:54 +0100

    Added comments

--- pc-dos/fpu_arithmetic.asm
+++ pc-dos/fpu_arithmetic.asm
@@ -5,6 +5,10 @@
 ; grafickych dem a her na PC v DOSu:
 ; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
 
+; Clanek, kde je tento demonstracni priklad pouzit:
+; Matematické koprocesory na platformě 80×86 prakticky
+; https://www.root.cz/clanky/matematicke-koprocesory-na-platforme-80x86-prakticky/
+;
 ;-----------------------------------------------------------------------------
 
 org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
