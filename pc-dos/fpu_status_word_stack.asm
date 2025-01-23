89b6ecc91fceac2e17ad009f1f43afda62d9798b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-23 19:30:10 +0100

    Reformatted and expanded tabs

--- pc-dos/fpu_status_word_stack.asm
+++ pc-dos/fpu_status_word_stack.asm
@@ -16,28 +16,28 @@ start:
 
 main:
         fninit                     ; inicializace koprocesoru
-	call print_status_word     ; tisk stavového slova
+        call print_status_word     ; tisk stavového slova
 
-	fldz                       ; uložení hodnoty na zásobník
-	call print_status_word     ; tisk stavového slova
+        fldz                       ; uložení hodnoty na zásobník
+        call print_status_word     ; tisk stavového slova
 
-	fldz                       ; uložení druhé hodnoty na zásobník
-	call print_status_word     ; tisk stavového slova
+        fldz                       ; uložení druhé hodnoty na zásobník
+        call print_status_word     ; tisk stavového slova
 
-	fldz                       ; uložení třetí hodnoty na zásobník
-	call print_status_word     ; tisk stavového slova
+        fldz                       ; uložení třetí hodnoty na zásobník
+        call print_status_word     ; tisk stavového slova
 
-	faddp                      ; výpočet s odstraněním původní hodnoty
-	call print_status_word     ; tisk stavového slova
+        faddp                      ; výpočet s odstraněním původní hodnoty
+        call print_status_word     ; tisk stavového slova
 
-	faddp                      ; výpočet s odstraněním původní hodnoty
-	call print_status_word     ; tisk stavového slova
+        faddp                      ; výpočet s odstraněním původní hodnoty
+        call print_status_word     ; tisk stavového slova
 
-	faddp                      ; výpočet s odstraněním původní hodnoty
-	call print_status_word     ; tisk stavového slova
+        faddp                      ; výpočet s odstraněním původní hodnoty
+        call print_status_word     ; tisk stavového slova
 
-	faddp                      ; nyní by měl zásobník "podtéct"
-	call print_status_word     ; tisk stavového slova
+        faddp                      ; nyní by měl zásobník "podtéct"
+        call print_status_word     ; tisk stavového slova
 
         wait_key                   ; cekani na klavesu
         exit                       ; navrat do DOSu
@@ -46,9 +46,9 @@ main:
 
 print_status_word:
         fnstsw word [test_word]    ; ulozeni stavoveho slova
-	mov ax, word [test_word]
-	print_hex_16 ax            ; tisk stavoveho slova v hexadecimalnim formatu
-	ret
+        mov ax, word [test_word]
+        print_hex_16 ax            ; tisk stavoveho slova v hexadecimalnim formatu
+        ret
 
 ; datova cast
 test_word: dw 0
