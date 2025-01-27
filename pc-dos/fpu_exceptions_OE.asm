8a3e1ebbc85a8b9ff54ccce6f6797d3f7ec4bcb3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-27 10:59:26 +0100

    FPU OE (overflow) exception

---
+++ pc-dos/fpu_exceptions_OE.asm
@@ -0,0 +1,43 @@
+; Precteni stavoveho slova matematickeho koprocesoru
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+
+;-----------------------------------------------------------------------------
+
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+%include "io.asm"                  ; nacist symboly, makra a podprogramy
+%include "print.asm"               ; nacist symboly, makra a podprogramy
+
+main:
+        fninit                     ; inicializace koprocesoru
+
+	fld1                       ; uložení konstanty 0 na zásobník
+	fld1                       ; uložení konstanty 0 na zásobník
+	faddp                      ; nyní je na zásobníku uložena hodnota 2
+
+	mov cx, 20                 ; počet opakování smyčky
+smycka1:
+        fld st0                    ; duplikace hodnoty na vrcholu zásobníku
+	fmulp                      ; výpočet druhé mocniny
+	call print_status_word     ; tisk stavového slova
+	loop smycka1
+
+        wait_key                   ; cekani na klavesu
+        exit                       ; navrat do DOSu
+
+;-----------------------------------------------------------------------------
+
+print_status_word:
+        fnstsw word [test_word]    ; ulozeni stavoveho slova
+	mov ax, word [test_word]
+	and ax, 0xff               ; vymaskovat bity, které neobsahují příznaky chyb
+	print_hex_16 ax            ; tisk stavoveho slova v hexadecimalnim formatu
+	ret
+
+; datova cast
+test_word: dw 0
