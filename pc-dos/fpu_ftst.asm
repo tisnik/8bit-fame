fbd21125319b227df46f7d3b50a86b7d7e0d1481
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-01-27 11:00:11 +0100

    FTST instruction

---
+++ pc-dos/fpu_ftst.asm
@@ -0,0 +1,71 @@
+; Precteni stavoveho slova matematickeho koprocesoru
+;-----------------------------------------------------------------------------
+
+BITS 16         ; 16bitovy vystup pro DOS
+
+;-----------------------------------------------------------------------------
+
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+%include "io.asm"                  ; nacist symboly, makra a podprogramy
+%include "print.asm"               ; nacist symboly, makra a podprogramy
+
+main:
+        fninit                     ; inicializace koprocesoru
+	fldz                       ; uložení konstanty 0 na zásobník
+	ftst
+	call print_status_word     ; tisk stavového slova
+
+        fninit                     ; inicializace koprocesoru
+	fld1                       ; uložení konstanty 1 na zásobník
+	ftst
+	call print_status_word     ; tisk stavového slova
+
+        fninit                     ; inicializace koprocesoru
+	fldpi                      ; uložení konstanty Pi na zásobník
+	ftst
+	call print_status_word     ; tisk stavového slova
+
+        fninit                     ; inicializace koprocesoru
+	fld1
+	fchs                       ; změna znaménka původní konstanty 1
+	ftst
+	call print_status_word     ; tisk stavového slova
+
+        fninit                     ; inicializace koprocesoru
+	fld1
+	fldz
+	fdiv                       ; podíl 1/0
+	ftst
+	call print_status_word     ; tisk stavového slova
+
+        fninit                     ; inicializace koprocesoru
+	fldz
+	fldz
+	fdiv                       ; podíl 0/0
+	ftst
+	call print_status_word     ; tisk stavového slova
+
+        fninit                     ; inicializace koprocesoru
+	fld1
+	fchs                       ; změna znaménka
+	fsqrt                      ; odmocnina z -1
+	ftst
+	call print_status_word     ; tisk stavového slova
+
+        wait_key                   ; cekani na klavesu
+        exit                       ; navrat do DOSu
+
+;-----------------------------------------------------------------------------
+
+print_status_word:
+        fnstsw word [test_word]    ; ulozeni stavoveho slova
+	mov ax, word [test_word]
+	print_hex_16 ax            ; tisk stavoveho slova v hexadecimalnim formatu
+	ret
+
+; datova cast
+test_word: dw 0
