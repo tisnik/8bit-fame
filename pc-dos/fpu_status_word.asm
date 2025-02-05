1475698e464643ce90f78e814801cc0764fe4101
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-05 10:03:13 +0100

    Added comments

--- pc-dos/fpu_status_word.asm
+++ pc-dos/fpu_status_word.asm
@@ -1,4 +1,9 @@
+;-----------------------------------------------------------------------------
 ; Precteni stavoveho slova matematickeho koprocesoru
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
 ;
 ; Clanek, kde je tento demonstracni priklad pouzit:
 ; Matematické koprocesory na platformě 80×86 prakticky
