71f380f86f0f1b6b900a62cb5178cf68fb4753a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-17 09:26:12 +0100

    MMX arithmetic shift instruction

---
+++ pc-linux/mmx_arithmetic_right.asm
@@ -0,0 +1,53 @@
+[bits 32]
+ 
+%include "linux_macros.asm"
+
+;-----------------------------------------------------------------------------
+section .data
+
+hex_message:
+         times 8 db '?'
+	 db ' '
+	 hex_message_length equ $ - hex_message
+ 
+psraw_message:
+         db 0x0a, "psraw:", 0x0a
+	 psraw_message_length equ $ - psraw_message
+ 
+psrad_message:
+         db 0x0a, "psrad:", 0x0a
+	 psrad_message_length equ $ - psrad_message
+ 
+ 
+mmx_val db 0, 0, 1, 0xff, 0, 0, 1, 0xff
+
+;-----------------------------------------------------------------------------
+section .bss
+mmx_tmp resb 8
+
+ 
+;-----------------------------------------------------------------------------
+section .text
+        global _start                ; tento symbol ma byt dostupny i linkeru
+
+_start:
+	emms                         ; inicializace MMX
+	mov ebx, mmx_val
+	movq mm1, [ebx]              ; nacteni prvni hodnoty do registru MM1
+	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+
+        movq mm2, mm1                ; zapamatovat si hodnotu pro další použití
+
+	psraw mm1, 1                 ; aritmeticky posun doprava o jeden bit
+        print_string psraw_message, psraw_message_length
+	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+
+        movq mm1, mm2                ; obnovit hodnotu registru mm1
+	psrad mm1, 1                 ; aritmeticky posun doprava o jeden bit
+        print_string psrad_message, psrad_message_length
+	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+
+        exit                         ; ukonceni procesu
+
+
+%include "hex2string.asm"
