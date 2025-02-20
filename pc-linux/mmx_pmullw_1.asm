c86e04aec37ad5234c54703fba7d2922bb96769b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-20 16:06:55 +0100

    Reformatted

--- pc-linux/mmx_pmullw_1.asm
+++ pc-linux/mmx_pmullw_1.asm
@@ -17,8 +17,8 @@ section .data
 
 hex_message:
          times 8 db '?'
-	 db ' '
-	 hex_message_length equ $ - hex_message
+         db ' '
+         hex_message_length equ $ - hex_message
  
 mmx_val_1 dw 1, 2, 3, 4
 mmx_val_2 dw 2, 2, 2, 2
@@ -33,18 +33,18 @@ section .text
         global _start                ; tento symbol ma byt dostupny i linkeru
 
 _start:
-	emms                         ; inicializace MMX
-	mov ebx, mmx_val_1
-	movq mm1, [ebx]              ; nacteni prvni hodnoty do registru MM1
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        emms                         ; inicializace MMX
+        mov ebx, mmx_val_1
+        movq mm1, [ebx]              ; nacteni prvni hodnoty do registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
-	mov ebx, mmx_val_2
-	movq mm2, [ebx]              ; nacteni druhe hodnoty do registru MM1
-	print_mmx_reg_as_hex mm2     ; tisk hodnoty registru MM2
+        mov ebx, mmx_val_2
+        movq mm2, [ebx]              ; nacteni druhe hodnoty do registru MM1
+        print_mmx_reg_as_hex mm2     ; tisk hodnoty registru MM2
 
-	pmullw mm1, mm2              ; soucin hodnot ve vektoru slovo po slovu
+        pmullw mm1, mm2              ; soucin hodnot ve vektoru slovo po slovu
 
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
         exit                         ; ukonceni procesu
 
