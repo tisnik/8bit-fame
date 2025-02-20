7abe5751efb728cfa8f2ec43be5f5e9bac7f7a15
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-20 16:06:55 +0100

    Reformatted

--- pc-linux/mmx_cmpeq.asm
+++ pc-linux/mmx_cmpeq.asm
@@ -16,20 +16,20 @@ section .data
 
 hex_message:
          times 8 db '?'
-	 db ' '
-	 hex_message_length equ $ - hex_message
+         db ' '
+         hex_message_length equ $ - hex_message
  
 pcmpeqb_message:
          db 0x0a, "pcmpeqb:", 0x0a
-	 pcmpeqb_message_length equ $ - pcmpeqb_message
+         pcmpeqb_message_length equ $ - pcmpeqb_message
  
 pcmpeqw_message:
          db 0x0a, "pcmpeqw:", 0x0a
-	 pcmpeqw_message_length equ $ - pcmpeqw_message
+         pcmpeqw_message_length equ $ - pcmpeqw_message
  
 pcmpeqd_message:
          db 0x0a, "pcmpeqd:", 0x0a
-	 pcmpeqd_message_length equ $ - pcmpeqd_message
+         pcmpeqd_message_length equ $ - pcmpeqd_message
  
  
 mmx_val_1 db 0, 1, 2, 3, 0, 1, 2, 3
@@ -45,33 +45,33 @@ section .text
         global _start                ; tento symbol ma byt dostupny i linkeru
 
 _start:
-	emms                         ; inicializace MMX
-	mov ebx, mmx_val_1
-	movq mm1, [ebx]              ; nacteni prvni hodnoty do registru MM1
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        emms                         ; inicializace MMX
+        mov ebx, mmx_val_1
+        movq mm1, [ebx]              ; nacteni prvni hodnoty do registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
-	mov ebx, mmx_val_2
-	movq mm2, [ebx]              ; nacteni druhe hodnoty do registru MM2
-	print_mmx_reg_as_hex mm2     ; tisk hodnoty registru MM2
+        mov ebx, mmx_val_2
+        movq mm2, [ebx]              ; nacteni druhe hodnoty do registru MM2
+        print_mmx_reg_as_hex mm2     ; tisk hodnoty registru MM2
 
         movq mm3, mm1                ; zapamatovat si hodnotu pro další použití
         movq mm4, mm2                ; zapamatovat si hodnotu pro další použití
 
-	pcmpeqb mm1, mm2             ; porovnani prvku vektoru na rovnost
+        pcmpeqb mm1, mm2             ; porovnani prvku vektoru na rovnost
         print_string pcmpeqb_message, pcmpeqb_message_length
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
         movq mm1, mm3                ; obnovit hodnotu registru mm1
         movq mm2, mm4                ; obnovit hodnotu registru mm2
-	pcmpeqw mm1, mm2             ; porovnani prvku vektoru na rovnost
+        pcmpeqw mm1, mm2             ; porovnani prvku vektoru na rovnost
         print_string pcmpeqw_message, pcmpeqw_message_length
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
         movq mm1, mm3                ; obnovit hodnotu registru mm1
         movq mm2, mm4                ; obnovit hodnotu registru mm2
-	pcmpeqd mm1, mm2             ; porovnani prvku vektoru na rovnost
+        pcmpeqd mm1, mm2             ; porovnani prvku vektoru na rovnost
         print_string pcmpeqd_message, pcmpeqd_message_length
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
         exit                         ; ukonceni procesu
 
