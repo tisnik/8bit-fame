788ebbe4c14d7ee498c745d66b5e70a3ed8671ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-21 09:33:22 +0100

    Loading SSE value from unaligned memory cells

---
+++ pc-linux/sse_value_unaligned_1.asm
@@ -0,0 +1,32 @@
+[bits 32]
+ 
+%include "linux_macros.asm"
+
+;-----------------------------------------------------------------------------
+section .data
+
+hex_message:
+         times 8 db '?'
+         db ' '
+         hex_message_length equ $ - hex_message
+ 
+sse_val dd 0.0, 1.0, -1.0, 2.0
+
+;-----------------------------------------------------------------------------
+section .bss
+sse_tmp resb 16
+
+ 
+;-----------------------------------------------------------------------------
+section .text
+        global _start                ; tento symbol ma byt dostupny i linkeru
+
+_start:
+        mov ebx, sse_val
+        movups xmm0, [ebx]           ; nacteni hodnoty do registru XMM0
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
+
+        exit                         ; ukonceni procesu
+
+
+%include "hex2string.asm"
