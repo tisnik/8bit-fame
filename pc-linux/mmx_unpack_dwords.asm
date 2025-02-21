b22e329eac57df484a28d0620f878af77b5ad49e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-21 09:31:56 +0100

    Link to article where this example is used

--- pc-linux/mmx_unpack_dwords.asm
+++ pc-linux/mmx_unpack_dwords.asm
@@ -1,3 +1,12 @@
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
+; Clanek, kde je tento demonstracni priklad pouzit:
+; SIMD instrukce na 80×86: dokončení popisu MMX, instrukce 3DNow!
+; https://www.root.cz/clanky/simd-instrukce-na-80-86-dokonceni-popisu-mmx-instrukce-3dnow/
+
 [bits 32]
  
 %include "linux_macros.asm"
@@ -7,16 +16,16 @@ section .data
 
 hex_message:
          times 8 db '?'
-	 db ' '
-	 hex_message_length equ $ - hex_message
+         db ' '
+         hex_message_length equ $ - hex_message
  
 punpckldq_message:
          db 0x0a, "punpckldq:", 0x0a
-	 punpckldq_message_length equ $ - punpckldq_message
+         punpckldq_message_length equ $ - punpckldq_message
  
 punpckhdq_message:
          db 0x0a, "punpckhdq:", 0x0a
-	 punpckhdq_message_length equ $ - punpckhdq_message
+         punpckhdq_message_length equ $ - punpckhdq_message
  
  
  
@@ -33,27 +42,27 @@ section .text
         global _start                ; tento symbol ma byt dostupny i linkeru
 
 _start:
-	emms                         ; inicializace MMX
-	mov ebx, mmx_val_1
-	movq mm1, [ebx]              ; nacteni prvni hodnoty do registru MM1
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        emms                         ; inicializace MMX
+        mov ebx, mmx_val_1
+        movq mm1, [ebx]              ; nacteni prvni hodnoty do registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
-	mov ebx, mmx_val_2
-	movq mm2, [ebx]              ; nacteni druhe hodnoty do registru MM2
-	print_mmx_reg_as_hex mm2     ; tisk hodnoty registru MM2
+        mov ebx, mmx_val_2
+        movq mm2, [ebx]              ; nacteni druhe hodnoty do registru MM2
+        print_mmx_reg_as_hex mm2     ; tisk hodnoty registru MM2
 
         movq mm3, mm1                ; zapamatovat si hodnotu pro další použití
         movq mm4, mm2                ; zapamatovat si hodnotu pro další použití
 
-	punpckldq mm1, mm2           ; zkombinovani obsahu dvou vektoru
+        punpckldq mm1, mm2           ; zkombinovani obsahu dvou vektoru
         print_string punpckldq_message, punpckldq_message_length
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
         movq mm1, mm3                ; obnovit hodnotu registru mm1
         movq mm2, mm4                ; obnovit hodnotu registru mm2
-	punpckhdq mm1, mm2           ; zkombinovani obsahu dvou vektoru
+        punpckhdq mm1, mm2           ; zkombinovani obsahu dvou vektoru
         print_string punpckhdq_message, punpckhdq_message_length
-	print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
+        print_mmx_reg_as_hex mm1     ; tisk hodnoty registru MM1
 
         exit                         ; ukonceni procesu
 
