630220eb873cd636827cb4555013d8977068ac1f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-21 09:32:29 +0100

    SSE ADDPS instruction

---
+++ pc-linux/sse_addps.asm
@@ -0,0 +1,40 @@
+[bits 32]
+ 
+%include "linux_macros.asm"
+
+;-----------------------------------------------------------------------------
+section .data
+
+hex_message:
+         times 8 db '?'
+         db ' '
+         hex_message_length equ $ - hex_message
+ 
+align 16
+sse_val_1 dd 0.0, 1.0, -1.0, 2.0
+sse_val_2 dd 1.0, 1.0, 1.0, 1.0
+
+;-----------------------------------------------------------------------------
+section .bss
+sse_tmp resb 16
+
+ 
+;-----------------------------------------------------------------------------
+section .text
+        global _start                ; tento symbol ma byt dostupny i linkeru
+
+_start:
+        mov ebx, sse_val_1
+        movaps xmm0, [ebx]           ; nacteni prvni hodnoty do registru XMM0
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
+
+        mov ebx, sse_val_2
+        movaps xmm1, [ebx]           ; nacteni druhe hodnoty do registru XMM1
+        print_sse_reg_as_hex xmm1    ; tisk hodnoty registru XMM1
+
+        addps xmm0, xmm1             ; soucet vektoru
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
+        exit                         ; ukonceni procesu
+
+
+%include "hex2string.asm"
