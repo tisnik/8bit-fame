e26549325bdd7d47a11b2a3a8b6dce8e2a2cd44a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-14 14:34:43 +0100

    Dot product for an array of size 8

---
+++ gcc-simd/dot_product_8.c
@@ -0,0 +1,9 @@
+float dot_product_4(float *a, float *b) {
+    #define SIZE 8
+    int i;
+    float result = 0.0;
+    for (i=0; i<SIZE; i++) {
+        result += a[i] * b[i];
+    }
+    return result;
+}
