cbc14b42119619efc5deb3f4673e923fbc7c2f34
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-14 14:37:51 +0100

    Assembly output generated by GCC

---
+++ gcc-simd/dot_product_4_non_vect.asm
@@ -0,0 +1,28 @@
+	.file	"dot_product_4.c"
+	.text
+	.p2align 4
+	.globl	dot_product_4
+	.type	dot_product_4, @function
+dot_product_4:
+.LFB0:
+	.cfi_startproc
+	movups	(%rsi), %xmm0
+	movups	(%rdi), %xmm1
+	pxor	%xmm2, %xmm2
+	mulps	%xmm0, %xmm1
+	movaps	%xmm1, %xmm0
+	addss	%xmm2, %xmm0
+	movaps	%xmm1, %xmm2
+	shufps	$85, %xmm1, %xmm2
+	addss	%xmm2, %xmm0
+	movaps	%xmm1, %xmm2
+	unpckhps	%xmm1, %xmm2
+	shufps	$255, %xmm1, %xmm1
+	addss	%xmm2, %xmm0
+	addss	%xmm1, %xmm0
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	dot_product_4, .-dot_product_4
+	.ident	"GCC: (GNU) 14.2.1 20240912 (Red Hat 14.2.1-3)"
+	.section	.note.GNU-stack,"",@progbits
