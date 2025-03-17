4e330746aa7c33f136bbb16d8af99f0e84253e48
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-17 08:43:59 +0100

    Assembly output generated by GCC

---
+++ gcc-simd/add_arrays_restrict_size_4_vect.asm
@@ -0,0 +1,27 @@
+	.file	"add_arrays_restrict_size_4.c"
+	.intel_syntax noprefix
+# GNU C17 (GCC) version 14.2.1 20240912 (Red Hat 14.2.1-3) (x86_64-redhat-linux)
+#	compiled by GNU C version 14.2.1 20240912 (Red Hat 14.2.1-3), GMP version 6.2.1, MPFR version 4.2.1, MPC version 1.3.1, isl version none
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -msse -mfpmath=sse -masm=intel -mtune=generic -march=x86-64 -O2 -ffast-math -ftree-vectorize
+	.text
+	.p2align 4
+	.globl	add_delta
+	.type	add_delta, @function
+add_delta:
+.LFB0:
+	.cfi_startproc
+# add_arrays_restrict_size_4.c:5:         a[i] += b[i];
+	movups	xmm1, XMMWORD PTR [rdi]	# tmp109, MEM <vector(4) float> [(float *)a_11(D)]
+# add_arrays_restrict_size_4.c:5:         a[i] += b[i];
+	movups	xmm0, XMMWORD PTR [rsi]	# vect__6.9_25, MEM <vector(4) float> [(float *)b_12(D)]
+# add_arrays_restrict_size_4.c:5:         a[i] += b[i];
+	addps	xmm0, xmm1	# vect__7.10_26, tmp109
+	movups	XMMWORD PTR [rdi], xmm0	# MEM <vector(4) float> [(float *)a_11(D)], vect__7.10_26
+# add_arrays_restrict_size_4.c:7: }
+	ret	
+	.cfi_endproc
+.LFE0:
+	.size	add_delta, .-add_delta
+	.ident	"GCC: (GNU) 14.2.1 20240912 (Red Hat 14.2.1-3)"
+	.section	.note.GNU-stack,"",@progbits
