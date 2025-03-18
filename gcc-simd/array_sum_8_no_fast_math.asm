14371d29a1507604476fa3f42e312ddb84296d4a
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-18 09:28:22 +0100

    Generated code in assembly

---
+++ gcc-simd/array_sum_8_no_fast_math.asm
@@ -0,0 +1,23 @@
+	.file	"array_sum_8.c"
+	.text
+	.p2align 4
+	.globl	array_sum
+	.type	array_sum, @function
+array_sum:
+.LFB0:
+	.cfi_startproc
+	pxor	%xmm0, %xmm0
+	addss	(%rdi), %xmm0
+	addss	4(%rdi), %xmm0
+	addss	8(%rdi), %xmm0
+	addss	12(%rdi), %xmm0
+	addss	16(%rdi), %xmm0
+	addss	20(%rdi), %xmm0
+	addss	24(%rdi), %xmm0
+	addss	28(%rdi), %xmm0
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	array_sum, .-array_sum
+	.ident	"GCC: (GNU) 14.2.1 20240912 (Red Hat 14.2.1-3)"
+	.section	.note.GNU-stack,"",@progbits
