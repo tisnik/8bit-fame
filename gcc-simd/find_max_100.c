950376b5e6a5bfe78e979194b57963968b622fc7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-20 09:08:31 +0100

    Find maximum value in an array with 100 items

---
+++ gcc-simd/find_max_100.c
@@ -0,0 +1,13 @@
+#include <float.h>
+
+float find_max_100(float *a) {
+    #define SIZE 100
+    int i;
+    float max = FLT_MIN_EXP;
+    for (i=0; i<SIZE; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
