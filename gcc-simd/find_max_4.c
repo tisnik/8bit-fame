fd5e06f21cd652e5d15f1c42fe8b88dfa8de773b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-20 09:08:36 +0100

    Find maximum value in an array with 4 items

---
+++ gcc-simd/find_max_4.c
@@ -0,0 +1,13 @@
+#include <float.h>
+
+float find_max_4(float *a) {
+    #define SIZE 4
+    int i;
+    float max = FLT_MIN_EXP;
+    for (i=0; i<SIZE; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
