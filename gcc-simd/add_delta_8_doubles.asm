2d2710c1139b0870844054764d5ef4a8097fb924
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-21 14:59:00 +0100

    Assembly generated by GCC

---
+++ gcc-simd/add_delta_8_doubles.asm
@@ -0,0 +1,31 @@
+	.file	"add_delta_8_doubles.c"
+	.intel_syntax noprefix
+# GNU C17 (GCC) version 14.2.1 20240912 (Red Hat 14.2.1-3) (x86_64-redhat-linux)
+#	compiled by GNU C version 14.2.1 20240912 (Red Hat 14.2.1-3), GMP version 6.2.1, MPFR version 4.2.1, MPC version 1.3.1, isl version none
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -msse -mfpmath=sse -masm=intel -mtune=generic -march=x86-64 -O2 -ffast-math -ftree-vectorize
+	.text
+	.p2align 4
+	.globl	add_delta
+	.type	add_delta, @function
+add_delta:
+.LFB0:
+	.cfi_startproc
+	unpcklpd	xmm0, xmm0	# vect_cst__21
+	lea	rax, [rdi+64]	# _5,
+.L2:
+# add_delta_8_doubles.c:5:         a[i] += delta;
+	movupd	xmm1, XMMWORD PTR [rdi]	# vect__4.6_20, MEM <vector(2) double> [(double *)_2]
+	add	rdi, 16	# ivtmp.13,
+# add_delta_8_doubles.c:5:         a[i] += delta;
+	addpd	xmm1, xmm0	# vect__5.7_22, vect_cst__21
+	movups	XMMWORD PTR [rdi-16], xmm1	# MEM <vector(2) double> [(double *)_2], vect__5.7_22
+	cmp	rax, rdi	# _5, ivtmp.13
+	jne	.L2	#,
+# add_delta_8_doubles.c:7: }
+	ret	
+	.cfi_endproc
+.LFE0:
+	.size	add_delta, .-add_delta
+	.ident	"GCC: (GNU) 14.2.1 20240912 (Red Hat 14.2.1-3)"
+	.section	.note.GNU-stack,"",@progbits
