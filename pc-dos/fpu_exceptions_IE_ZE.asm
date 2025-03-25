e8b80a3a8d2f28181c4ac694404be0d237923aae
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-25 08:28:57 +0100

    Expand tabs in source code

--- pc-dos/fpu_exceptions_IE_ZE.asm
+++ pc-dos/fpu_exceptions_IE_ZE.asm
@@ -25,44 +25,44 @@ start:
 
 main:
         fninit                     ; inicializace koprocesoru
-	fldz                       ; uložení konstanty 0 na zásobník
-	fld1                       ; uložení konstanty 1 na zásobník
-	fdiv                       ; podíl 0/1
-	call print_status_word     ; tisk stavového slova
+        fldz                       ; uložení konstanty 0 na zásobník
+        fld1                       ; uložení konstanty 1 na zásobník
+        fdiv                       ; podíl 0/1
+        call print_status_word     ; tisk stavového slova
 
         fninit                     ; inicializace koprocesoru
-	fld1                       ; uložení konstanty 1 na zásobník
-	fld1                       ; uložení konstanty 1 na zásobník
-	fdiv                       ; podíl 1/1
-	call print_status_word     ; tisk stavového slova
+        fld1                       ; uložení konstanty 1 na zásobník
+        fld1                       ; uložení konstanty 1 na zásobník
+        fdiv                       ; podíl 1/1
+        call print_status_word     ; tisk stavového slova
 
         fninit                     ; inicializace koprocesoru
-	fld1                       ; uložení konstanty 1 na zásobník
-	fldz                       ; uložení konstanty 0 na zásobník
-	fdiv                       ; podíl 1/0
-	call print_status_word     ; tisk stavového slova
+        fld1                       ; uložení konstanty 1 na zásobník
+        fldz                       ; uložení konstanty 0 na zásobník
+        fdiv                       ; podíl 1/0
+        call print_status_word     ; tisk stavového slova
 
         fninit                     ; inicializace koprocesoru
-	fldz                       ; uložení konstanty 0 na zásobník
-	fldz                       ; uložení konstanty 0 na zásobník
-	fdiv                       ; podíl 0/0
-	call print_status_word     ; tisk stavového slova
+        fldz                       ; uložení konstanty 0 na zásobník
+        fldz                       ; uložení konstanty 0 na zásobník
+        fdiv                       ; podíl 0/0
+        call print_status_word     ; tisk stavového slova
 
         fninit                     ; inicializace koprocesoru
-	fldz
-	fsqrt                      ; odmocnina z 0
-	call print_status_word     ; tisk stavového slova
+        fldz
+        fsqrt                      ; odmocnina z 0
+        call print_status_word     ; tisk stavového slova
 
         fninit                     ; inicializace koprocesoru
-	fld1
-	fsqrt                      ; odmocnina z 1
-	call print_status_word     ; tisk stavového slova
+        fld1
+        fsqrt                      ; odmocnina z 1
+        call print_status_word     ; tisk stavového slova
 
         fninit                     ; inicializace koprocesoru
-	fld1
-	fchs                       ; změna znaménka
-	fsqrt                      ; odmocnina z -1
-	call print_status_word     ; tisk stavového slova
+        fld1
+        fchs                       ; změna znaménka
+        fsqrt                      ; odmocnina z -1
+        call print_status_word     ; tisk stavového slova
 
         wait_key                   ; cekani na klavesu
         exit                       ; navrat do DOSu
@@ -71,10 +71,10 @@ main:
 
 print_status_word:
         fnstsw word [test_word]    ; ulozeni stavoveho slova
-	mov ax, word [test_word]
-	and ax, 0xff               ; vymaskovat bity, které neobsahují příznaky chyb
-	print_hex_16 ax            ; tisk stavoveho slova v hexadecimalnim formatu
-	ret
+        mov ax, word [test_word]
+        and ax, 0xff               ; vymaskovat bity, které neobsahují příznaky chyb
+        print_hex_16 ax            ; tisk stavoveho slova v hexadecimalnim formatu
+        ret
 
 ; datova cast
 test_word: dw 0
