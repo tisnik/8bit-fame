821881654f007fcf5bc9d66e497f813bffd4a1b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-26 09:09:54 +0100

    Doubles to integers conversions

---
+++ pc-linux/sse2_cvtps2dq.asm
@@ -0,0 +1,37 @@
+[bits 32]
+ 
+%include "linux_macros.asm"
+
+;-----------------------------------------------------------------------------
+section .data
+
+hex_message:
+         times 8 db '?'
+         db ' '
+         hex_message_length equ $ - hex_message
+ 
+align 16
+sse_val_1 dd 1.0, 1.5, 2.0, 2.5
+
+;-----------------------------------------------------------------------------
+section .bss
+sse_tmp resb 16
+
+ 
+;-----------------------------------------------------------------------------
+section .text
+        global _start                ; tento symbol ma byt dostupny i linkeru
+
+_start:
+        mov ebx, sse_val_1
+        movdqu xmm0, [ebx]           ; nacteni puvodniho vektoru do registru XMM0
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
+
+        cvtps2dq xmm1, xmm0          ; konverze hodnot typu single na cela cisla
+
+        print_sse_reg_as_hex xmm1    ; tisk hodnoty registru XMM1
+
+        exit                         ; ukonceni procesu
+
+
+%include "hex2string.asm"
