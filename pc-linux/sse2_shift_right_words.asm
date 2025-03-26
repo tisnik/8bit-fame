a22d804a91703e66cd91d9977239c591377fa488
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-26 09:07:57 +0100

    Shift right words in XMM vector

---
+++ pc-linux/sse2_shift_right_words.asm
@@ -0,0 +1,50 @@
+[bits 32]
+ 
+%include "linux_macros.asm"
+
+;-----------------------------------------------------------------------------
+section .data
+
+hex_message:
+         times 8 db '?'
+         db ' '
+         hex_message_length equ $ - hex_message
+ 
+align 16
+sse_val_1 db 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+sse_val_2 db 1
+
+
+;-----------------------------------------------------------------------------
+section .bss
+sse_tmp resb 16
+
+ 
+;-----------------------------------------------------------------------------
+section .text
+        global _start                ; tento symbol ma byt dostupny i linkeru
+
+_start:
+        mov ebx, sse_val_1
+        movdqu xmm0, [ebx]           ; nacteni puvodniho vektoru do registru XMM0
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
+
+	mov esi, 1                   ; konstanta, kterou budeme pricitat
+	movd xmm1, esi               ; nacteni konstanty do druheho vektoru
+        print_sse_reg_as_hex xmm1    ; tisk hodnoty registru XMM0
+
+        mov eax, 16
+next_shift:
+        psrlw xmm0, xmm1             ; logicky posun doprava
+
+	push eax
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM1
+	pop eax
+
+        dec eax                      ; snizit pocitadlo
+	jnz next_shift               ; a opakovat smycku
+
+        exit                         ; ukonceni procesu
+
+
+%include "hex2string.asm"
