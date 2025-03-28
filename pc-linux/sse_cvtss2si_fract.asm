f85782390ae141e33a5f95394c82aaa6f9e25475
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-28 09:00:58 +0100

    Reformatted

--- pc-linux/sse_cvtss2si_fract.asm
+++ pc-linux/sse_cvtss2si_fract.asm
@@ -40,22 +40,22 @@ _start:
         movaps xmm0, [ebx]           ; nacteni prvni hodnoty do registru XMM0
         print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
 
-	cvtss2si eax, xmm0           ; konverze jednoho prvku
-	print_hex eax, 10
+        cvtss2si eax, xmm0           ; konverze jednoho prvku
+        print_hex eax, 10
 
         mov ebx, sse_val_2
         movaps xmm0, [ebx]           ; nacteni prvni hodnoty do registru XMM0
         print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
 
-	cvtss2si eax, xmm0           ; konverze jednoho prvku
-	print_hex eax, 10
+        cvtss2si eax, xmm0           ; konverze jednoho prvku
+        print_hex eax, 10
 
         mov ebx, sse_val_3
         movaps xmm0, [ebx]           ; nacteni prvni hodnoty do registru XMM0
         print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
 
-	cvtss2si eax, xmm0           ; konverze jednoho prvku
-	print_hex eax, 10
+        cvtss2si eax, xmm0           ; konverze jednoho prvku
+        print_hex eax, 10
 
         exit                         ; ukonceni procesu
 
