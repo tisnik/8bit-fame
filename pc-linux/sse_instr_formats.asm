c43a71e380d3db7d3afd0d79204d70dcf8f0950c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-28 09:00:58 +0100

    Reformatted

--- pc-linux/sse_instr_formats.asm
+++ pc-linux/sse_instr_formats.asm
@@ -14,7 +14,7 @@
 section .text
         movups xmm0, [ebx]           ; nacteni hodnoty do registru XMM0
         movaps xmm0, [ebx]           ; nacteni hodnoty do registru XMM0
-	movups xmm0, xmm1            ; prenosy mezi registry
+        movups xmm0, xmm1            ; prenosy mezi registry
 
         addps xmm0, xmm1             ; soucet prvku vektoru
         subps xmm0, xmm1             ; rozdil prvku vektoru
@@ -32,21 +32,21 @@ section .text
         maxss xmm0, xmm1             ; vyber vetsich skalaru
         maxss xmm0, xmm1             ; vyber mensich skalaru
 
-	cmpeqps xmm0, xmm1           ; porovnani prvku vektoru
-	cmpltps xmm0, xmm1           ; porovnani prvku vektoru
-	cmpleps xmm0, xmm1           ; porovnani prvku vektoru
-	cmpunordps xmm0, xmm1        ; porovnani prvku vektoru
-	cmpneqps xmm0, xmm1          ; porovnani prvku vektoru
-	cmpnltps xmm0, xmm1          ; porovnani prvku vektoru
-	cmpnleps xmm0, xmm1          ; porovnani prvku vektoru
-	cmpordps xmm0, xmm1          ; porovnani prvku vektoru
+        cmpeqps xmm0, xmm1           ; porovnani prvku vektoru
+        cmpltps xmm0, xmm1           ; porovnani prvku vektoru
+        cmpleps xmm0, xmm1           ; porovnani prvku vektoru
+        cmpunordps xmm0, xmm1        ; porovnani prvku vektoru
+        cmpneqps xmm0, xmm1          ; porovnani prvku vektoru
+        cmpnltps xmm0, xmm1          ; porovnani prvku vektoru
+        cmpnleps xmm0, xmm1          ; porovnani prvku vektoru
+        cmpordps xmm0, xmm1          ; porovnani prvku vektoru
 
-	unpckhps xmm0, xmm1           ; prolozeni prvku dvou vektoru
-	unpcklps xmm0, xmm1           ; prolozeni prvku dvou vektoru
+        unpckhps xmm0, xmm1           ; prolozeni prvku dvou vektoru
+        unpcklps xmm0, xmm1           ; prolozeni prvku dvou vektoru
         shufps xmm0, xmm1, 0b11111111 ; prolozeni prvku dvou vektoru
 
         cvtsi2ss xmm0, eax           ; konverze jednoho prvku
-	cvtss2si eax, xmm0           ; konverze jednoho prvku
+        cvtss2si eax, xmm0           ; konverze jednoho prvku
 
-	ldmxcsr [ebx]                ; načtení nové hodnoty
-	stmxcsr [ebx]                ; uložení hodnoty MXCSR
+        ldmxcsr [ebx]                ; načtení nové hodnoty
+        stmxcsr [ebx]                ; uložení hodnoty MXCSR
