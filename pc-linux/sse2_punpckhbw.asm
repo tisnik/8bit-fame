ced6264658d26a85d5cea857ef36b2423482ffbb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-04 13:09:22 +0200

    SSE2 instruction PUNPCKHBW

---
+++ pc-linux/sse2_punpckhbw.asm
@@ -0,0 +1,42 @@
+[bits 32]
+ 
+%include "linux_macros.asm"
+
+;-----------------------------------------------------------------------------
+section .data
+
+hex_message:
+         times 8 db '?'
+         db ' '
+         hex_message_length equ $ - hex_message
+ 
+align 16
+sse_val_1 db 0x01, 0x11, 0x21, 0x31, 0x41, 0x51, 0x61, 0x71, 0x81, 0x91, 0xa1, 0xb1, 0xc1, 0xd1, 0xe1, 0xf1
+sse_val_2 db 0x02, 0x12, 0x22, 0x32, 0x42, 0x52, 0x62, 0x72, 0x82, 0x92, 0xa2, 0xb2, 0xc2, 0xd2, 0xe2, 0xf2
+
+;-----------------------------------------------------------------------------
+section .bss
+sse_tmp resb 16
+
+ 
+;-----------------------------------------------------------------------------
+section .text
+        global _start                ; tento symbol ma byt dostupny i linkeru
+
+_start:
+        mov ebx, sse_val_1           ; adresa prvniho vektoru
+        movdqu xmm0, [ebx]           ; nacteni puvodniho vektoru do registru XMM0
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
+
+        mov ebx, sse_val_2           ; adresa druheho vektoru
+        movdqu xmm1, [ebx]           ; nacteni puvodniho vektoru do registru XMM1
+        print_sse_reg_as_hex xmm1    ; tisk hodnoty registru XMM0
+
+        punpckhbw xmm0, xmm1         ; provedeni operace prolozeni hodnot z obou vektoru po bajtech
+
+        print_sse_reg_as_hex xmm0    ; tisk hodnoty registru XMM0
+
+        exit                         ; ukonceni procesu
+
+
+%include "hex2string.asm"
