1b5ab48cb6321fd150a603ad83de2d892361954d
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-10 16:10:03 +0200

    Expanded tabs

--- Speccy-asm/03-symbolic-names.asm
+++ Speccy-asm/03-symbolic-names.asm
@@ -21,9 +21,9 @@ attribute_adr equ $5800
 entry_point   equ $8000
 
 
-	org entry_point
+        org entry_point
 
 start:
-	ld a,%11010110
-	ld (attribute_adr),a
-	ret
+        ld a,%11010110
+        ld (attribute_adr),a
+        ret
