9e45e30cc81bab6a5e3a160446c5a9f3d87c65d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-11 09:30:24 +0200

    Expanded tabs

--- Speccy-asm/09-loop.asm
+++ Speccy-asm/09-loop.asm
@@ -20,19 +20,19 @@
 ATTRIBUTE_ADR equ $5800
 ENTRY_POINT   equ $8000
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	ld hl, ATTRIBUTE_ADR  ; adresa pro zápis
-	xor a                 ; zapisovaná hodnota
-	ld b, a               ; počitadlo smyčky
+        ld hl, ATTRIBUTE_ADR  ; adresa pro zápis
+        xor a                 ; zapisovaná hodnota
+        ld b, a               ; počitadlo smyčky
 
 loop:
-	ld (hl),a             ; zápis hodnoty na adresu (HL)
-	inc hl                ; zvýšení adresy
-	inc a                 ; zvýšení zapisované hodnoty
-	dec b                 ; snížení hodnoty počitadla
-	jp NZ, loop           ; skok pokud se ještě nedosáhlo nuly
-	ret
+        ld (hl),a             ; zápis hodnoty na adresu (HL)
+        inc hl                ; zvýšení adresy
+        inc a                 ; zvýšení zapisované hodnoty
+        dec b                 ; snížení hodnoty počitadla
+        jp NZ, loop           ; skok pokud se ještě nedosáhlo nuly
+        ret
 
 end ENTRY_POINT
