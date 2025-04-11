bd644db197fca817146d85249069048dcaa996dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-04-11 09:30:25 +0200

    Expanded tabs

--- Speccy-asm/103-beep.asm
+++ Speccy-asm/103-beep.asm
@@ -17,17 +17,17 @@ BEEPER        equ $03B5
 
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 beep MACRO pitch, duration
-	ld   hl, pitch
-	ld   de, duration
-	call BEEPER
+        ld   hl, pitch
+        ld   de, duration
+        call BEEPER
 ENDM
 
 start:
-	beep 964, 1000                 ; přehrání noty
-	ret                            ; návrat do BASICu
+        beep 964, 1000                 ; přehrání noty
+        ret                            ; návrat do BASICu
 
 
 end ENTRY_POINT
