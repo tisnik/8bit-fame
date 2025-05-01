2c0197c39a333b5c519ac10c2192b5264b3b5847
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-01 19:09:35 +0200

    Compilation via assembly, non underlined external symbols

---
+++ compilers/bc/add4.asm
@@ -0,0 +1,58 @@
+        ifndef  ??version
+?debug  macro
+        endm
+publicdll macro name
+        public  name
+        endm
+$comm   macro   name,dist,size,count
+        comm    dist name:BYTE:count*size
+        endm
+        else
+$comm   macro   name,dist,size,count
+        comm    dist name[size]:BYTE:count
+        endm
+        endif
+        ?debug  S "add.c"
+        ?debug  C E99566A15A05666F6F2E63
+_TEXT   segment byte public 'CODE'
+_TEXT   ends
+DGROUP  group   _DATA,_BSS
+        assume  cs:_TEXT,ds:DGROUP
+_DATA   segment word public 'DATA'
+d@      label   byte
+d@w     label   word
+_DATA   ends
+_BSS    segment word public 'BSS'
+b@      label   byte
+b@w     label   word
+_BSS    ends
+_TEXT   segment byte public 'CODE'
+   ;    
+   ;    int add(int a, int b) {
+   ;    
+        assume  cs:_TEXT
+add     proc    near
+        push    bp
+        mov     bp,sp
+   ;    
+   ;        return a+b;
+   ;    
+        mov     ax,word ptr [bp+4]
+        add     ax,word ptr [bp+6]
+        jmp     short @1@50
+@1@50:
+   ;    
+   ;    }
+   ;    
+        pop     bp
+        ret     
+add     endp
+        ?debug  C E9
+_TEXT   ends
+_DATA   segment word public 'DATA'
+s@      label   byte
+_DATA   ends
+_TEXT   segment byte public 'CODE'
+_TEXT   ends
+        public  add
+        end
