3f74e58231d0aa1ef36fb5f5d7b19f4284571420
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-02 09:00:59 +0200

    Compilation into assembly by Watcom C with optimizations

---
+++ compilers/watcom/add2.asm
@@ -0,0 +1,18 @@
+Module: C:\add.c
+GROUP: 'DGROUP' CONST,CONST2,_DATA
+
+Segment: _TEXT BYTE USE16 00000003 bytes
+0000                          add_:
+0000    01 D0                     add         ax,dx 
+0002    C3                        ret         
+
+Routine Size: 3 bytes,    Routine Base: _TEXT + 0000
+
+No disassembly errors
+
+Segment: CONST WORD USE16 00000000 bytes
+
+Segment: CONST2 WORD USE16 00000000 bytes
+
+Segment: _DATA WORD USE16 00000000 bytes
+
