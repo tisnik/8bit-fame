7db8a7937d2f432b3c3e9f94fccaaa2f5fa807e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-16 09:09:41 +0200

    Add function compiled into assembly without optimizations

---
+++ compilers/djgpp/add_1.asm
@@ -0,0 +1,22 @@
+        .file   "add.c"
+        .intel_syntax noprefix
+        .section .text
+        .globl  _add
+_add:
+LFB0:
+        .cfi_startproc
+        push    ebp
+        .cfi_def_cfa_offset 8
+        .cfi_offset 5, -8
+        mov     ebp, esp
+        .cfi_def_cfa_register 5
+        mov     edx, DWORD PTR [ebp+8]
+        mov     eax, DWORD PTR [ebp+12]
+        add     eax, edx
+        pop     ebp
+        .cfi_restore 5
+        .cfi_def_cfa 4, 4
+        ret
+        .cfi_endproc
+LFE0:
+        .ident  "GCC: (GNU) 9.3.0"
