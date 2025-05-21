3e07d8f71c1810c3ac2920302dd93fe296aab1c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-21 09:08:34 +0200

    Add floating point numbers compiled into assembly with optimizations

---
+++ compilers/watcom/add_f_3.asm
@@ -0,0 +1,17 @@
+Module: c:\add_f.c
+GROUP: 'DGROUP' CONST,CONST2,_DATA
+
+Segment: _TEXT BYTE USE16 00000003 bytes
+0000                          add_:
+0000    E9 00 00                  jmp         __FSA 
+
+Routine Size: 3 bytes,    Routine Base: _TEXT + 0000
+
+No disassembly errors
+
+Segment: CONST WORD USE16 00000000 bytes
+
+Segment: CONST2 WORD USE16 00000000 bytes
+
+Segment: _DATA WORD USE16 00000000 bytes
+
