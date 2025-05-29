8000e24898a46f5583fd20a9125b247aa46fc472
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-29 08:30:53 +0200

    Reformatted

--- Speccy-asm/108-direct-speaker-border-di.asm
+++ Speccy-asm/108-direct-speaker-border-di.asm
@@ -19,25 +19,25 @@ BEEPER_PORT   equ $fe
 
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 
 start:
-	ld   A, %00000111              ; původní hodnota zapisovaná na port
-	di                             ; zakázat přerušení
+        ld   A, %00000111              ; původní hodnota zapisovaná na port
+        di                             ; zakázat přerušení
 loop:
-	xor  %00010001                 ; negace prvního a čtvrtého bitu
-	out  (BEEPER_PORT), A          ; zápis na port
-	call short_delay               ; počkat
-	jr   loop                      ; a opakovat celé znovu
+        xor  %00010001                 ; negace prvního a čtvrtého bitu
+        out  (BEEPER_PORT), A          ; zápis na port
+        call short_delay               ; počkat
+        jr   loop                      ; a opakovat celé znovu
 
 
 short_delay:
-	; zpožďovací rutina
-	; mění B (což nám nevadí)
+        ; zpožďovací rutina
+        ; mění B (což nám nevadí)
         ld   b, 98                    ; počitadlo zpožďovací smyčky
 delay_loop:
-	djnz delay_loop                ; opakovat smyčku s počitadlem v B
-	ret                            ; návrat z podprogramu
+        djnz delay_loop                ; opakovat smyčku s počitadlem v B
+        ret                            ; návrat z podprogramu
 
 end ENTRY_POINT
