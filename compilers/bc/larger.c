dfce0809cbe2e1242caa67f6df8b0a99758eb083
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-04 09:05:17 +0200

    Find larger value, written in C

---
+++ compilers/bc/larger.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef unsigned int uint;
+
+uint* larger_value(uint *x, uint *y) {
+    if (*x > *y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int main(void) {
+    uint a = 1;
+    uint b = 2;
+
+    printf("%d\n", *larger_value(&a, &b));
+    printf("%d\n", *larger_value(&b, &a));
+
+    return 0;
+}
+
