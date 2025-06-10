7f8e8f822fec21ab1e3af41898aee4abe3b7ff50
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-10 13:55:07 +0200

    Setting SVGA mode

---
+++ pc-dos/svga_mode_0.asm
@@ -0,0 +1,52 @@
+;-----------------------------------------------------------------------------
+; MODE    RESOLUTION  BITS PER PIXEL  MAXIMUM COLORS
+; 0x0100  640x400     8               256
+; 0x0101  640x480     8               256
+; 0x0102  800x600     4               16
+; 0x0103  800x600     8               256
+; 0x010D  320x200     15              32k
+; 0x010E  320x200     16              64k
+; 0x010F  320x200     24/32           16m
+; 0x0110  640x480     15              32k
+; 0x0111  640x480     16              64k
+; 0x0112  640x480     24/32           16m
+; 0x0113  800x600     15              32k
+; 0x0114  800x600     16              64k
+; 0x0115  800x600     24/32           16m
+; 0x0116  1024x768    15              32k
+; 0x0117  1024x768    16              64k
+; 0x0118  1024x768    24/32           16m
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+%include "io.asm"                  ; nacist symboly, makra a podprogramy
+%include "print.asm"               ; nacist symboly, makra a podprogramy
+
+main:
+        push ds
+        pop  es                    ; nastaveni CS=DS=ES
+
+        mov  bx, 0x103             ; cislo rezimu
+        mov  ax, 0x4f02            ; nastaveni grafickeho rezimu
+        int      0x10
+
+        cmp ax, 0x004f             ; test, zda bylo volani funkce BIOSu uspesne
+        jne     failed
+
+        jmp     finish
+
+failed:
+        print_string failed_msg
+
+finish:
+        wait_key            ; cekani na klavesu
+        exit                ; navrat do DOSu
+
+
+; datova cast
+section .data
+
+failed_msg:      db "Failed", 0x0a, 0x0d, "$"
