62387c443ca8f43a5a2ee749341f84be3dea8e38
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-17 09:35:32 +0200

    SVGA text mode - attributes, cursors etc.

---
+++ pc-dos/svga_text_mode_3.asm
@@ -0,0 +1,51 @@
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+%include "io.asm"                  ; nacist symboly, makra a podprogramy
+%include "print.asm"               ; nacist symboly, makra a podprogramy
+
+
+
+
+main:
+        push ds
+        pop  es                    ; nastaveni CS=DS=ES
+
+        mov  bx, 0x10c             ; cislo rezimu
+        mov  ax, 0x4f02            ; nastaveni grafickeho rezimu
+        int      0x10
+
+        cmp ax, 0x004f             ; test, zda bylo volani funkce BIOSu uspesne
+        jne     failed
+
+
+success:
+        mov ax, 0xb800
+        mov es, ax
+        mov di, 0       ; nyni ES:DI obsahuje adresu prvniho znaku ve video RAM
+
+        mov cx, 40*25   ; pocet zapisovanych znaku
+        mov al, 0       ; kod zapisovaneho znaku
+opak:
+        stosb           ; zapis znaku + atributu
+        stosb
+        inc al          ; dalsi znak/atribut
+        loop opak       ; opakujeme CX-krat
+        jmp     finish
+
+failed:
+        print_string failed_msg
+
+finish:
+        wait_key            ; cekani na klavesu
+        exit                ; navrat do DOSu
+
+
+; datova cast
+section .data
+
+success_msg:     db "Success", 0x0a, 0x0d, "$"
+failed_msg:      db "Failed", 0x0a, 0x0d, "$"
