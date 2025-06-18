b060d5bc2c115c397dee32084b264e6c534cdeeb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-18 10:29:46 +0200

    Image BitBLT + logical line change

---
+++ pc-dos/svga_640x480_image_6.asm
@@ -0,0 +1,129 @@
+;-----------------------------------------------------------------------------
+; MODE    RESOLUTION  BITS PER PIXEL  MAXIMUM COLORS
+; 0x0100  640x400     8               256
+; 0x0101  640x480     8               256
+; 0x0102  800x600     4               16
+; 0x0103  800x600     8               256
+; 0x010D  320x200     15              32k
+; 0x010E  320x200     16              64k
+; 0x010F  320x200     24/32           16m
+; 0x0110  640x480     15              32k
+; 0x0111  640x480     16              64k
+; 0x0112  640x480     24/32           16m
+; 0x0113  800x600     15              32k
+; 0x0114  800x600     16              64k
+; 0x0115  800x600     24/32           16m
+; 0x0116  1024x768    15              32k
+; 0x0117  1024x768    16              64k
+; 0x0118  1024x768    24/32           16m
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+%include "io.asm"                  ; nacist symboly, makra a podprogramy
+%include "print.asm"               ; nacist symboly, makra a podprogramy
+
+
+
+
+main:
+        push ds
+        pop  es                    ; nastaveni CS=DS=ES
+
+        mov  bx, 0x103             ; cislo rezimu
+        mov  ax, 0x4f02            ; nastaveni grafickeho rezimu
+        int      0x10
+
+        cmp ax, 0x004f             ; test, zda bylo volani funkce BIOSu uspesne
+        jne     failed
+
+        mov  ax, 0x4f06            ; nastaveni nebo precteni delky logickeho radku
+	mov  bl, 00                ; operace nastaveni
+	mov  cx, 800               ; delka radku
+
+        cmp ax, 0x004f             ; test, zda bylo volani funkce BIOSu uspesne
+        jne     failed
+
+
+success:
+        call grayscale_palette     ; nastaveni palety se stupni sedi
+
+        mov ax, cs
+        mov ds, ax
+        mov si, image              ; nyni DS:SI obsahuje adresu prvniho bajtu v obrazku
+
+	mov  dx, 0                 ; cislo pametoveho banku video pameti
+	call set_video_window      ; nastaveni pametoveho banku
+
+	xcenter equ 800/2-320/2
+
+        mov di, xcenter            ; offset na zacatek video RAM
+        mov al, 82                 ; pocet prenesenych radku
+	call bitblt                ; zavolat subrutinu pro blokovy prenos
+
+	mov  dx, 1                 ; cislo pametoveho banku video pameti
+	call set_video_window      ; nastaveni pametoveho banku
+	mov di, xcenter+82*800-65536       ; offset na zacatek video RAM
+        mov al, 82                 ; pocet prenesenych radku
+	call bitblt                ; zavolat subrutinu pro blokovy prenos
+
+	mov  dx, 2                 ; cislo pametoveho banku video pameti
+	call set_video_window      ; nastaveni pametoveho banku
+	mov di, xcenter+2*82*800-65536*2   ; offset na zacatek video RAM
+        mov al, 200-82*2           ; pocet prenesenych radku
+	call bitblt                ; zavolat subrutinu pro blokovy prenos
+
+        jmp     finish
+
+failed:
+        print_string failed_msg
+
+finish:
+        wait_key            ; cekani na klavesu
+        exit                ; navrat do DOSu
+
+bitblt:
+        push 0xa000                ; video RAM v textovem rezimu
+        pop  es
+next_line:
+        mov cx, 320/2              ; pocet zapisovanych bajtu (=pixelu)
+        rep movsw                  ; prenos jednoho obrazoveho radku
+	add di, 800-320            ; preskocit zbytek obrazoveho radku
+	dec al                     ; snizit pocitadlo radku
+	jnz next_line
+	ret                        ; navrat ze subrutiny
+
+set_video_window:
+        ; ocekava se, ze DL je nastaven korektne!
+	mov ax, 0x4f05             ; nastaveni okna
+	mov bx, 0x0000             ; okno A
+	int     0x10               ; volani VBE
+	ret                        ; navrat ze subrutiny
+
+; paleta ve stupnich sedi
+grayscale_palette:
+        mov ax, 0x1010             ; cislo sluzby a podsluzby VGA BIOSu
+        xor bl, bl                 ; index barvy
+next_dac:
+        mov ch, bl                 ; prvni barvova slozka
+        shr ch, 1
+        shr ch, 1
+        mov cl, ch                 ; druha barvova slozka
+        mov dh, ch                 ; treti barvova slozka
+        int 0x10                   ; modifikace mapovani v DAC
+        inc bl                     ; zvysit index v DAC
+        jnz next_dac               ; nastavit dalsi barvu, dokud nedosahneme hodnoty 256
+	ret                        ; navrat ze subrutiny
+
+
+; datova cast
+section .data
+
+success_msg:     db "Success", 0x0a, 0x0d, "$"
+failed_msg:      db "Failed", 0x0a, 0x0d, "$"
+
+; pridani binarnich dat s rastrovym obrazkem
+image:
+    incbin "image_320x200.bin"
