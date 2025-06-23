76065e1e70c419a8d8e1c3dc0a653cf75ae9ec0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-23 13:00:48 +0200

    Reformatted

--- Speccy-asm/12-loop.asm
+++ Speccy-asm/12-loop.asm
@@ -20,20 +20,20 @@
 ATTRIBUTE_ADR equ $5800
 ENTRY_POINT   equ $8000
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	ld ix, ATTRIBUTE_ADR  ; adresa pro zápis
-	xor a                 ; zapisovaná hodnota
-	ld b, a               ; počitadlo smyčky
+        ld ix, ATTRIBUTE_ADR  ; adresa pro zápis
+        xor a                 ; zapisovaná hodnota
+        ld b, a               ; počitadlo smyčky
 
 loop:
-	ld (ix),a             ; zápis hodnoty na adresu (IX)
-	inc ix                ; zvýšení adresy
-	inc a                 ; zvýšení zapisované hodnoty
-	djnz loop             ; kombinace dec b + jp NZ, loop
-	                      ; snížení hodnoty počitadla
-	                      ; skok pokud se ještě nedosáhlo nuly
-	ret
+        ld (ix),a             ; zápis hodnoty na adresu (IX)
+        inc ix                ; zvýšení adresy
+        inc a                 ; zvýšení zapisované hodnoty
+        djnz loop             ; kombinace dec b + jp NZ, loop
+                              ; snížení hodnoty počitadla
+                              ; skok pokud se ještě nedosáhlo nuly
+        ret
 
 end ENTRY_POINT
