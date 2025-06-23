039760728a614e3f53b3566bbab9a6c4372d1946
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-23 13:00:48 +0200

    Reformatted

--- Speccy-asm/119-noise-envelope.asm
+++ Speccy-asm/119-noise-envelope.asm
@@ -21,40 +21,40 @@ AY_DATA_REGISTER   equ $bffd
 org ENTRY_POINT
 
 start:
-	ld      d, 5                   ; výška šumu
-	ld      e, 100
-	call    ay_write               ; zápis hodnoty do AY
+        ld      d, 5                   ; výška šumu
+        ld      e, 100
+        call    ay_write               ; zápis hodnoty do AY
 
-	ld      d, 8                   ; hlasitost kanálu A
-	ld      e, 16                  ; hlasitost ovládat pomocí obálky
-	call    ay_write               ; zápis hodnoty do AY
+        ld      d, 8                   ; hlasitost kanálu A
+        ld      e, 16                  ; hlasitost ovládat pomocí obálky
+        call    ay_write               ; zápis hodnoty do AY
 
-	ld	d, $b                  ; frekvence generátoru obálky (spodní bajt)
-	ld      e, 0
-	call    ay_write               ; zápis hodnoty do AY
+        ld      d, $b                  ; frekvence generátoru obálky (spodní bajt)
+        ld      e, 0
+        call    ay_write               ; zápis hodnoty do AY
 
-	ld	d, $c                  ; frekvence generátoru obálky (horní bajt)
-	ld      e, 1
-	call    ay_write               ; zápis hodnoty do AY
+        ld      d, $c                  ; frekvence generátoru obálky (horní bajt)
+        ld      e, 1
+        call    ay_write               ; zápis hodnoty do AY
 
-	ld	d, $d                  ; nastavení tvaru obálky (bitové pole)
-	ld      e, 12
-	call    ay_write               ; zápis hodnoty do AY
+        ld      d, $d                  ; nastavení tvaru obálky (bitové pole)
+        ld      e, 12
+        call    ay_write               ; zápis hodnoty do AY
 
-	ld      d, 7                   ; povolení výstupu z kanálu A
-	ld      e, %11110111           ; bitová maska
-	call    ay_write               ; zápis hodnoty do AY
+        ld      d, 7                   ; povolení výstupu z kanálu A
+        ld      e, %11110111           ; bitová maska
+        call    ay_write               ; zápis hodnoty do AY
 
 loop:
-	jr      loop                   ; nechceme návrat do BASICu
+        jr      loop                   ; nechceme návrat do BASICu
 
 
 ay_write:
-	ld      bc, AY_SELECT_REGISTER ; zápis do výběrového registru AY
-	out     (c), d                 ; index vybraného registru
-	ld      bc,AY_DATA_REGISTER    ; zápis do datového registru AY
-	out     (c), e                 ; zapisovaná hodnota
-	ret                            ; návrat ze subrutiny
+        ld      bc, AY_SELECT_REGISTER ; zápis do výběrového registru AY
+        out     (c), d                 ; index vybraného registru
+        ld      bc,AY_DATA_REGISTER    ; zápis do datového registru AY
+        out     (c), e                 ; zapisovaná hodnota
+        ret                            ; návrat ze subrutiny
 
 
 end ENTRY_POINT
