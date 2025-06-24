41720f4fb990d56787cad16a6ca7804ab1f9fccc
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-24 11:19:43 +0200

    Expanded tabs, reformatted

--- Speccy-asm/33-fill-in-vram-no-ret.asm
+++ Speccy-asm/33-fill-in-vram-no-ret.asm
@@ -23,20 +23,20 @@ ENTRY_POINT   equ $8000
 PIXELS        equ 256*192
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	ld hl, SCREEN_ADR     ; adresa pro zápis
-	ld a, PIXELS/8/256    ; počet opakování bloku s 256 zápisy
-	ld b, 0               ; počitadlo vnitřní smyčky
+        ld hl, SCREEN_ADR     ; adresa pro zápis
+        ld a, PIXELS/8/256    ; počet opakování bloku s 256 zápisy
+        ld b, 0               ; počitadlo vnitřní smyčky
 
 loop:
-	ld (hl), 0xff         ; zápis hodnoty na adresu (HL)
-	inc hl                ; zvýšení adresy
-	djnz loop             ; vnitřní smyčka: blok s 256 zápisy
-	dec a                 ; počitadlo vnější smyčky
-	jp NZ, loop           ; skok pokud se ještě nedosáhlo nuly
+        ld (hl), 0xff         ; zápis hodnoty na adresu (HL)
+        inc hl                ; zvýšení adresy
+        djnz loop             ; vnitřní smyčka: blok s 256 zápisy
+        dec a                 ; počitadlo vnější smyčky
+        jp NZ, loop           ; skok pokud se ještě nedosáhlo nuly
 finish:
-	jr finish             ; nevrátíme se do systému
+        jr finish             ; nevrátíme se do systému
 
 end ENTRY_POINT
