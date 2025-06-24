b39550e0a5bb3a7562fc89ed14227c40c042d998
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-24 11:19:42 +0200

    Expanded tabs, reformatted

--- Speccy-asm/18-cls.asm
+++ Speccy-asm/18-cls.asm
@@ -20,10 +20,10 @@
 ENTRY_POINT   equ $8000
 ROM_CLS       equ $0DAF
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	call ROM_CLS    ; smazání obrazovky a otevření kanálu číslo 2 (screen)
-	ret             ; návrat z programu do BASICu
+        call ROM_CLS    ; smazání obrazovky a otevření kanálu číslo 2 (screen)
+        ret             ; návrat z programu do BASICu
 
 end ENTRY_POINT
