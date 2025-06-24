1165229bdcfda0661550c911fb27cd2b38c6ab53
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-24 11:19:42 +0200

    Expanded tabs, reformatted

--- Speccy-asm/21-print-char.asm
+++ Speccy-asm/21-print-char.asm
@@ -20,13 +20,13 @@
 ENTRY_POINT      equ $8000
 ROM_OPEN_CHANNEL equ $1601
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
         ld   A,2              ; číslo kanálu
-	call ROM_OPEN_CHANNEL ; otevření kanálu číslo 2 (screen)
-	ld   A, 42            ; kód znaku '*' pro tisk
-	rst  0x10             ; zavolání rutiny v ROM
-	ret                   ; návrat z programu do BASICu
+        call ROM_OPEN_CHANNEL ; otevření kanálu číslo 2 (screen)
+        ld   A, 42            ; kód znaku '*' pro tisk
+        rst  0x10             ; zavolání rutiny v ROM
+        ret                   ; návrat z programu do BASICu
 
 end ENTRY_POINT
