9730cb073d052d024e1ec3f3a17213824772ae6b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-24 11:19:43 +0200

    Expanded tabs, reformatted

--- Speccy-asm/39-fill-block-optimized.asm
+++ Speccy-asm/39-fill-block-optimized.asm
@@ -25,17 +25,17 @@ PIXELS          equ 256*192
 PATTERN         equ %10101010
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	ld hl, SCREEN_ADR     ; adresa pro zápis
-	ld b, 8               ; počitadlo zapsaných bajtů
+        ld hl, SCREEN_ADR     ; adresa pro zápis
+        ld b, 8               ; počitadlo zapsaných bajtů
 
 loop:
-	ld (hl), PATTERN      ; zápis hodnoty na adresu (HL)
-	inc h                 ; posun na definici dalšího obrazového řádku
-	djnz loop             ; vnitřní smyčka: blok s osmi zápisy
+        ld (hl), PATTERN      ; zápis hodnoty na adresu (HL)
+        inc h                 ; posun na definici dalšího obrazového řádku
+        djnz loop             ; vnitřní smyčka: blok s osmi zápisy
 finish:
-	ret                   ; ukončit program
+        ret                   ; ukončit program
 
 end ENTRY_POINT
