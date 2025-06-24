b1b0a5e4a0be4b9b53d86ffbe3876baa3cc0222b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-24 11:19:43 +0200

    Expanded tabs, reformatted

--- Speccy-asm/31-attributes.asm
+++ Speccy-asm/31-attributes.asm
@@ -23,27 +23,27 @@ ROM_PRINT        equ $203C
 ATTR_T           equ 23695
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	ld   A,2              ; číslo kanálu
-	call ROM_OPEN_CHANNEL ; otevření kanálu číslo 2 (screen)
+        ld   A,2              ; číslo kanálu
+        call ROM_OPEN_CHANNEL ; otevření kanálu číslo 2 (screen)
 
-	ld   B, 64            ; barva tisku
-	ld   HL, ATTR_T       ; adresa systémové proměnné ATTR_T
+        ld   B, 64            ; barva tisku
+        ld   HL, ATTR_T       ; adresa systémové proměnné ATTR_T
 
 loop:
-	ld   (HL), B          ; změna barvy tisku
-	push BC               ; uchovat BC
-	ld   DE, TEXT         ; adresa prvního znaku v řetězci
-	ld   BC, TEXT_LENGTH  ; délka textu
-	call ROM_PRINT        ; volání subrutiny v ROM
-	pop  BC               ; obnovit BC
-	djnz loop             ; tisk další barvou
-	ret                   ; ukončit program
+        ld   (HL), B          ; změna barvy tisku
+        push BC               ; uchovat BC
+        ld   DE, TEXT         ; adresa prvního znaku v řetězci
+        ld   BC, TEXT_LENGTH  ; délka textu
+        call ROM_PRINT        ; volání subrutiny v ROM
+        pop  BC               ; obnovit BC
+        djnz loop             ; tisk další barvou
+        ret                   ; ukončit program
 
 ; řetězec
-TEXT:	db "Hello, speccy!"
+TEXT:   db "Hello, speccy!"
 
 TEXT_LENGTH: equ $ - TEXT
 
