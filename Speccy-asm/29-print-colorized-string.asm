052b93ed35e39a50d07aa29fdb03f2f30ec3e29c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-24 11:19:43 +0200

    Expanded tabs, reformatted

--- Speccy-asm/29-print-colorized-string.asm
+++ Speccy-asm/29-print-colorized-string.asm
@@ -36,24 +36,24 @@ YELLOW_COLOR     equ %110
 WHITE_COLOR      equ %111
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	ld   A,2              ; číslo kanálu
-	call ROM_OPEN_CHANNEL ; otevření kanálu číslo 2 (screen)
+        ld   A,2              ; číslo kanálu
+        call ROM_OPEN_CHANNEL ; otevření kanálu číslo 2 (screen)
 
-	ld   HL, TEXT         ; adresa prvního znaku v řetězci
+        ld   HL, TEXT         ; adresa prvního znaku v řetězci
 
 LOOP:
-	ld   A, (HL)          ; načíst kód znaku z řetězce
-	and  a                ; test na kód znak s kódem 0
-	ret  Z                ; ukončit program na konci řetězce
+        ld   A, (HL)          ; načíst kód znaku z řetězce
+        and  a                ; test na kód znak s kódem 0
+        ret  Z                ; ukončit program na konci řetězce
 
-	rst  0x10             ; zavolání rutiny v ROM
-	inc  HL               ; přechod na další znak
-	jr   LOOP
+        rst  0x10             ; zavolání rutiny v ROM
+        inc  HL               ; přechod na další znak
+        jr   LOOP
 
 ; nulou ukončený řetězec
-TEXT:	db PAPER, RED_COLOR, "Hello", INK, WHITE_COLOR, "Speccy", FLASH, 1, "!", 0
+TEXT:   db PAPER, RED_COLOR, "Hello", INK, WHITE_COLOR, "Speccy", FLASH, 1, "!", 0
 
 end ENTRY_POINT
