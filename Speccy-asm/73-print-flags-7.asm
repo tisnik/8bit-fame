919b4b07ce8edae156a755e304d6b045ff14ad25
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-25 09:40:51 +0200

    Expanded tabs, reformatted

--- Speccy-asm/73-print-flags-7.asm
+++ Speccy-asm/73-print-flags-7.asm
@@ -22,20 +22,20 @@ ROM_CLS       equ $0DAF
 OUT_NUM_1     equ $1A1B
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	call ROM_CLS    ; smazání obrazovky a otevření kanálu číslo 2 (screen)
+        call ROM_CLS    ; smazání obrazovky a otevření kanálu číslo 2 (screen)
 
-	ld   A, 128     ; první sčítanec
-	add  A, 128     ; druhý sčítance + výsledek operace
+        ld   A, 128     ; první sčítanec
+        add  A, 128     ; druhý sčítance + výsledek operace
 
-	push AF         ; uložíme pár AF na zásobník
-	pop  BC         ; obnovíme původní obsah AF ze zásobníku, ovšem nyní do BC
-	ld   B, 0       ; vymažeme B (což byl obsah A), ponecháme jen C (což byl obsah F)
+        push AF         ; uložíme pár AF na zásobník
+        pop  BC         ; obnovíme původní obsah AF ze zásobníku, ovšem nyní do BC
+        ld   B, 0       ; vymažeme B (což byl obsah A), ponecháme jen C (což byl obsah F)
 
-	call OUT_NUM_1  ; zavolání rutiny pro výpis celého čísla
+        call OUT_NUM_1  ; zavolání rutiny pro výpis celého čísla
 
-	ret             ; návrat z programu do BASICu
+        ret             ; návrat z programu do BASICu
 
 end ENTRY_POINT
