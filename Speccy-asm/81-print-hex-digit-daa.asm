7161fab05afe72fedf70be501a0d427be461f31f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-25 09:40:51 +0200

    Expanded tabs, reformatted

--- Speccy-asm/81-print-hex-digit-daa.asm
+++ Speccy-asm/81-print-hex-digit-daa.asm
@@ -22,43 +22,43 @@ ROM_CLS       equ $0DAF
 OUT_NUM_1     equ $1A1B
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	call ROM_CLS        ; smazání obrazovky a otevření kanálu číslo 2 (screen)
+        call ROM_CLS        ; smazání obrazovky a otevření kanálu číslo 2 (screen)
 
-	xor  A              ; hodnota cifry, která se má vytisknout
-	ld   B, 16          ; počitadlo smyčky
-loop:	                    ; vytisknout hexa cifru s přechodem na nový řádek
-	call print_hex_digit_nl
-	inc  A              ; zvýšit hodnotu tištěné cifry
-	djnz loop           ; opakování smyčky se snížením hodnoty počitadla
+        xor  A              ; hodnota cifry, která se má vytisknout
+        ld   B, 16          ; počitadlo smyčky
+loop:                       ; vytisknout hexa cifru s přechodem na nový řádek
+        call print_hex_digit_nl
+        inc  A              ; zvýšit hodnotu tištěné cifry
+        djnz loop           ; opakování smyčky se snížením hodnoty počitadla
 
-	ret                 ; návrat z programu do BASICu
+        ret                 ; návrat z programu do BASICu
 
 print_hex_digit_nl:
-	push AF             ; uschovat A pro pozdější využití
-	or   $f0            ; nastavit horní čtyři bity + příznaky
-	                    ; nyní je v A jedna z hodnot:
-			    ;    0xf0 0xf1 0xf2 ... 0xf9 0xfa 0xfb 0xfc 0xfd 0xfe 0xff
-	daa                 ; desítková korekce pro původní hodnoty A-F
-	                    ; +  0x60 0x60 0x60 ... 0x60 0x66 0x66 0x66 0x66 0x66 0x66
-	                    ; nyní je v A jedna z hodnot:
-			    ;    0x50 0x51 0x52 ... 0x59 0x60 0x61 0x62 0x63 0x64 0x65
-	add A, $a0          ; přičtení konstanty
-	                    ; nyní je v A jedna z hodnot:
-			    ;    0xf0 0xf1 0xf2 ... 0xf9 0x00 0x01 0x02 0x03 0x04 0x05
-			    ; C    0    0    0        0    1    1    1    1    1    1
-	adc A, $40          ; přičtení konstanty a navíc i příznaku carry
-			    ;    0x30 0x31 0x31 ... 0x39 0x41 0x42 0x43 0x44 0x45 0x46
-			    ; >   '0'  '1'  '2'      '9'  'A'  'B'  'C'  'D'  'E'  'F'
-	rst  0x10           ; zavolání rutiny v ROM pro tisk jednoho znaku
+        push AF             ; uschovat A pro pozdější využití
+        or   $f0            ; nastavit horní čtyři bity + příznaky
+                            ; nyní je v A jedna z hodnot:
+                            ;    0xf0 0xf1 0xf2 ... 0xf9 0xfa 0xfb 0xfc 0xfd 0xfe 0xff
+        daa                 ; desítková korekce pro původní hodnoty A-F
+                            ; +  0x60 0x60 0x60 ... 0x60 0x66 0x66 0x66 0x66 0x66 0x66
+                            ; nyní je v A jedna z hodnot:
+                            ;    0x50 0x51 0x52 ... 0x59 0x60 0x61 0x62 0x63 0x64 0x65
+        add A, $a0          ; přičtení konstanty
+                            ; nyní je v A jedna z hodnot:
+                            ;    0xf0 0xf1 0xf2 ... 0xf9 0x00 0x01 0x02 0x03 0x04 0x05
+                            ; C    0    0    0        0    1    1    1    1    1    1
+        adc A, $40          ; přičtení konstanty a navíc i příznaku carry
+                            ;    0x30 0x31 0x31 ... 0x39 0x41 0x42 0x43 0x44 0x45 0x46
+                            ; >   '0'  '1'  '2'      '9'  'A'  'B'  'C'  'D'  'E'  'F'
+        rst  0x10           ; zavolání rutiny v ROM pro tisk jednoho znaku
 
 new_line:
-	ld   A, 0x0d        ; kód znaku pro odřádkování
-	rst  0x10           ; zavolání rutiny v ROM
-	pop  AF             ; obnovit A
-	ret                 ; návrat ze subrutiny
+        ld   A, 0x0d        ; kód znaku pro odřádkování
+        rst  0x10           ; zavolání rutiny v ROM
+        pop  AF             ; obnovit A
+        ret                 ; návrat ze subrutiny
 
 
 end ENTRY_POINT
