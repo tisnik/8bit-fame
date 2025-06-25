ca4c741ea45ca7ec33fce6519ea0dff715fc12c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-25 09:40:51 +0200

    Expanded tabs, reformatted

--- Speccy-asm/74-print-hex-number.asm
+++ Speccy-asm/74-print-hex-number.asm
@@ -22,74 +22,74 @@ ROM_CLS       equ $0DAF
 OUT_NUM_1     equ $1A1B
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	call ROM_CLS        ; smazání obrazovky a otevření kanálu číslo 2 (screen)
+        call ROM_CLS        ; smazání obrazovky a otevření kanálu číslo 2 (screen)
 
-	ld   A, 0x00        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0x00        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ld   A, 0x01        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0x01        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ld   A, 0x09        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0x09        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ld   A, 0x99        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0x99        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ld   A, 0x0a        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0x0a        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ld   A, 0xa0        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0xa0        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ld   A, 0xba        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0xba        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ld   A, 0xff        ; vytisknout hexa hodnotu s přechodem na nový řádek
-	call print_hex_number
-	call new_line
+        ld   A, 0xff        ; vytisknout hexa hodnotu s přechodem na nový řádek
+        call print_hex_number
+        call new_line
 
-	ret                 ; návrat z programu do BASICu
+        ret                 ; návrat z programu do BASICu
 
 print_hex_number:
-	push AF             ; uschovat A pro pozdější využití
-	rrca                ; rotace o čtyři bity doprava
-	rrca
-	rrca
-	rrca
-	and  $0f            ; zamaskovat horní čtyři bity
-	call print_hex_digit; vytisknout hexa číslici
+        push AF             ; uschovat A pro pozdější využití
+        rrca                ; rotace o čtyři bity doprava
+        rrca
+        rrca
+        rrca
+        and  $0f            ; zamaskovat horní čtyři bity
+        call print_hex_digit; vytisknout hexa číslici
 
-	pop  AF             ; obnovit A
-	and  $0f            ; zamaskovat horní čtyři bity
-	call print_hex_digit; vytisknout hexa číslici
-	ret                 ; a návrat z podprogramu
+        pop  AF             ; obnovit A
+        and  $0f            ; zamaskovat horní čtyři bity
+        call print_hex_digit; vytisknout hexa číslici
+        ret                 ; a návrat z podprogramu
 
 print_hex_digit:
-	cp   0x0a           ; test, jestli je číslice menší než 10
-	jr c, print_0_to_9  ; ok, hodnota je menší než 10, budeme tedy tisknout desítkovou číslici
-	add  A, 65-10       ; ASCII kód znaku 'A', ovšem začínáme od desítky, ne od nuly
-	rst  0x10           ; zavolání rutiny v ROM pro tisk jednoho znaku
-	ret                 ; návrat ze subrutiny
+        cp   0x0a           ; test, jestli je číslice menší než 10
+        jr c, print_0_to_9  ; ok, hodnota je menší než 10, budeme tedy tisknout desítkovou číslici
+        add  A, 65-10       ; ASCII kód znaku 'A', ovšem začínáme od desítky, ne od nuly
+        rst  0x10           ; zavolání rutiny v ROM pro tisk jednoho znaku
+        ret                 ; návrat ze subrutiny
 print_0_to_9:
-	add  A, 48          ; ASCII kód znaku '0'
-	rst  0x10           ; zavolání rutiny v ROM pro tisk jednoho znaku
-	ret                 ; návrat ze subrutiny
+        add  A, 48          ; ASCII kód znaku '0'
+        rst  0x10           ; zavolání rutiny v ROM pro tisk jednoho znaku
+        ret                 ; návrat ze subrutiny
 
 new_line:
-	ld   A, 0x0d        ; kód znaku pro odřádkování
-	rst  0x10           ; zavolání rutiny v ROM
-	ret                 ; návrat ze subrutiny
+        ld   A, 0x0d        ; kód znaku pro odřádkování
+        rst  0x10           ; zavolání rutiny v ROM
+        ret                 ; návrat ze subrutiny
 
 
 end ENTRY_POINT
