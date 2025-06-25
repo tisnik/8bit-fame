7df95135845c04c514262eb933ce4557b1b4e3ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-25 09:40:51 +0200

    Expanded tabs, reformatted

--- Speccy-asm/65-more-numbers-A.asm
+++ Speccy-asm/65-more-numbers-A.asm
@@ -22,19 +22,19 @@ ROM_CLS       equ $0DAF
 OUT_NUM_1     equ $1A1B
 
 
-	org ENTRY_POINT
+        org ENTRY_POINT
 
 start:
-	call ROM_CLS    ; smazání obrazovky a otevření kanálu číslo 2 (screen)
-	ld   BC, 1      ; numerická hodnota, kterou chceme vytisknout
+        call ROM_CLS    ; smazání obrazovky a otevření kanálu číslo 2 (screen)
+        ld   BC, 1      ; numerická hodnota, kterou chceme vytisknout
 next_number:
-	push BC         ; uchovat počitadlo
-	call OUT_NUM_1  ; zavolání rutiny pro výpis celého čísla
-	pop  BC         ; obnovit počitadlo
-	inc  C          ; a zvýšit o jedničku
-	ld   A, C
-	cp   11         ; kontrola na konec smyčky
-	jr   nz, next_number
-	ret             ; návrat z programu do BASICu
+        push BC         ; uchovat počitadlo
+        call OUT_NUM_1  ; zavolání rutiny pro výpis celého čísla
+        pop  BC         ; obnovit počitadlo
+        inc  C          ; a zvýšit o jedničku
+        ld   A, C
+        cp   11         ; kontrola na konec smyčky
+        jr   nz, next_number
+        ret             ; návrat z programu do BASICu
 
 end ENTRY_POINT
