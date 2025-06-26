040fa8066bba40f19312e19e9fa52b8c9b85c96e
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-26 08:14:20 +0200

    Add in FX format

---
+++ pc-dos/fx_add_1.asm
@@ -0,0 +1,42 @@
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+%include "io.asm"                  ; nacist symboly, makra a podprogramy
+%include "print.asm"               ; nacist symboly, makra a podprogramy
+
+P       equ     65536              ; poloha desetinne tecky v FX-pointu
+
+main:
+        mov  eax, 1 * P            ; konstanta v FX-pointu: 1.0
+        print_hex eax
+
+        mov  eax, 1 * P            ; konstanta v FX-pointu: 1.0
+        mov  ebx, 2 * P            ; konstanta v FX-pointu: 2.0
+        add  eax, ebx              ; scitani v FX-pointu
+        print_hex eax
+
+        mov  eax, 1 * P / 2        ; konstanta v FX-pointu: 0.5
+        print_hex eax
+
+        mov  eax, 3 * P / 2        ; konstanta v FX-pointu: 1.5
+        print_hex eax
+
+        mov  eax, 1 * P / 2        ; konstanta v FX-pointu: 0.5
+        mov  ebx, 3 * P / 2        ; konstanta v FX-pointu: 1.5
+        add  eax, ebx              ; scitani v FX-pointu
+        print_hex eax
+
+finish:
+        wait_key                   ; cekani na klavesu
+        exit                       ; navrat do DOSu
+
+
+; datova cast
+section .data
+
+section .bss
+
+
