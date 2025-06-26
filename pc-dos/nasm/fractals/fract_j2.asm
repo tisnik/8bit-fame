062f52bb97fe0f041ea605c23c8078a39fbb481c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-26 20:59:22 +0200

    Reformatted

--- pc-dos/nasm/fractals/fract_j2.asm
+++ pc-dos/nasm/fractals/fract_j2.asm
@@ -39,23 +39,23 @@ MIN equ -2*P
 ;-----------------------------------------------------------------------------
 section .data
 
-CX_     dd -100000                      ;imaginarni konstanta pro iteraci
+CX_     dd -100000                      ; imaginarni konstanta pro iteraci
 CY_     dd -80000
-PCX     dd 6000                         ;posun realne casti konstanty
+PCX     dd 6000                         ; posun realne casti konstanty
 
 
 
 ;-----------------------------------------------------------------------------
 section .bss
 
-X       resb  1                         ;pozice ve fraktalu
+X       resb  1                         ; pozice ve fraktalu
 Y       resb  1
-ZX1     resd  1                         ;komplexni cisla
-ZY1     resd  1                         ;komplexni cisla
-ZX2     resd  1                         ;komplexni cisla
-ZY2     resd  1                         ;komplexni cisla
-ZX3     resd  1                         ;komplexni cisla
-ZY3     resd  1                         ;komplexni cisla
+ZX1     resd  1                         ; komplexni cisla
+ZY1     resd  1                         ; komplexni cisla
+ZX2     resd  1                         ; komplexni cisla
+ZY2     resd  1                         ; komplexni cisla
+ZX3     resd  1                         ; komplexni cisla
+ZY3     resd  1                         ; komplexni cisla
 
 
 
@@ -64,94 +64,94 @@ org     0x100
 section .text
 
 START:
-        mov     AX,13h                  ;graphics 320x200x256
+        mov     AX,13h                  ; graphics 320x200x256
         int     10h
         mov     CL,8
-        push    0A000h                  ;do ES adresu video RAM
+        push    0A000h                  ; do ES adresu video RAM
         pop     ES
 
 OPAK:
-        mov     DI,11520+96             ;aby se fraktal vykreslil
+        mov     DI,11520+96             ; aby se fraktal vykreslil
         mov     BP,52608-320+95
-        mov     EAX, dword [CX_]        ;doprostred obrazovky
-        add     EAX, dword [PCX]        ;posun konstanty
+        mov     EAX, dword [CX_]        ; doprostred obrazovky
+        add     EAX, dword [PCX]        ; posun konstanty
         mov     dword [CX_],EAX
-        cmp     EAX,120000              ;kontrola CX_
-        jg      short UP                ;CX_ osciluje mezi -100000 a 100000
+        cmp     EAX,120000              ; kontrola CX_
+        jg      short UP                ; CX_ osciluje mezi -100000 a 100000
         cmp     EAX,-120000
         jl      short UP
         jmp     short PP
-UP:     neg     dword [PCX]             ;posun na opacnou stranu
-        add     dword [CY_],6000        ;posun CY_
-        cmp     dword [CY_],120000      ;kontrola CY_
+UP:     neg     dword [PCX]             ; posun na opacnou stranu
+        add     dword [CY_],6000        ; posun CY_
+        cmp     dword [CY_],120000      ; kontrola CY_
         jl      short PP
         mov     dword [CY_],-80000
 PP:
         mov     dword [ZX1],MIN
-        mov     byte [X],64             ;fraktal bude velikosti 128x128
+        mov     byte [X],64             ; fraktal bude velikosti 128x128
 FORX:
         mov     dword [ZY1],MIN
         mov     byte [Y],128
 FORY:
-        mov     CH,16                   ;pocet iteraci
+        mov     CH,16                   ; pocet iteraci
         mov     EAX, dword [ZX1]
-        mov     dword [ZX2],EAX         ;ZX2:=ZX1
+        mov     dword [ZX2],EAX         ; ZX2:=ZX1
         mov     ESI, dword [ZY1]
 
 REP_:
-        mov     EAX,[ZX2]
-        sar     EAX,CL                  ;ZX3:=ZX2^2
+        mov     EAX, [ZX2]
+        sar     EAX,CL                  ; ZX3:=ZX2^2
         imul    EAX
         mov     [ZX3],EAX
 
         mov     EAX,ESI
-        sar     EAX,CL                  ;ZY3:=ZY2^2
+        sar     EAX,CL                  ; ZY3:=ZY2^2
         imul    EAX
         mov     [ZY3],EAX
 
-        mov     EAX,[ZX2]
-        sar     EAX,CL                  ;ZX2 div 256
-        sar     ESI,7                   ;ZY2 div 256 * 2
+        mov     EAX, [ZX2]
+        sar     EAX,CL                  ; ZX2 div 256
+        sar     ESI,7                   ; ZY2 div 256 * 2
         imul    ESI
 
-        add     EAX,[CY_]               ;ZY2:=ZX2*ZY2+CY_
+        add     EAX, [CY_]              ; ZY2:=ZX2*ZY2+CY_
         mov     ESI,EAX
 
-        mov     EAX,[ZX3]
-        sub     EAX,[ZY3]
-        add     EAX,[CX_]
-        mov     [ZX2],EAX               ;ZX2:=ZX3^2-ZY3^2+CX_
+        mov     EAX, [ZX3]
+        sub     EAX, [ZY3]
+        add     EAX, [CX_]
+        mov     [ZX2],EAX               ; ZX2:=ZX3^2-ZY3^2+CX_
 
-        dec     CH                      ;pocitadlo iteraci
+        dec     CH                      ; pocitadlo iteraci
         jz      short POKRAC
-        mov     EAX,[ZX3]
-        add     EAX,[ZY3]
-        cmp     EAX,4*P                 ;kontrola bailout
+        mov     EAX, [ZX3]
+        add     EAX, [ZY3]
+        cmp     EAX,4*P                 ; kontrola bailout
         jc      short REP_
 
 POKRAC:
-        mov     AL,CH                   ;pocet iteraci
-        add     AL,16                   ;uprava barvy
-        stosb                           ;vykreslit prvni bod
-        mov     [ES:BP],AL              ;vykreslit druhy bod
+        mov     AL,CH                   ; pocet iteraci
+        add     AL,16                   ; uprava barvy
+        stosb                           ; vykreslit prvni bod
+        mov     [ES:BP],AL              ; vykreslit druhy bod
         dec     BP
-        add     dword [ZY1],K           ;ZY1:=ZY1+K
-        dec     byte [Y]                ;Y:=Y-1
+        add     dword [ZY1],K           ; ZY1:=ZY1+K
+        dec     byte [Y]                ; Y:=Y-1
         jnz     short FORY
-        add     DI,320-128              ;dalsi radek
+        add     DI,320-128              ; dalsi radek
         sub     BP,320-128
-        add     dword [ZX1],K           ;ZX1:=ZX1+K
-        dec     byte [X]                ;X:=X-1
+        add     dword [ZX1],K           ; ZX1:=ZX1+K
+        dec     byte [X]                ; X:=X-1
         jnz     FORX
 
-        mov     AH,01h                  ;je klavesa v bufferu ?
+        mov     AH,01h                  ; je klavesa v bufferu ?
         int     16h
-        jz      OPAK                    ;opakovat do stisku klavesy
-        xor     AH,AH                   ;cteni klavesy ->vymazani bufferu
+        jz      OPAK                    ; opakovat do stisku klavesy
+        xor     AH,AH                   ; cteni klavesy ->vymazani bufferu
         int     16h                     ; klavesnice
-        mov     AX,3                    ;nastaveni textoveho rezimu
+        mov     AX,3                    ; nastaveni textoveho rezimu
         int     10h
-        retn                            ;konec
+        retn                            ; konec
 
 ; finito
 
