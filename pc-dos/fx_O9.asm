52bcc0f4daeddcaccc0618fa0cd8edee0a80ac4c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-04 08:02:16 +0200

    Added link to article where this example is used

--- pc-dos/fx_O9.asm
+++ pc-dos/fx_O9.asm
@@ -1,3 +1,15 @@
+;-----------------------------------------------------------------------------
+;
+; Tento demonstracni priklad je pouzity v serialu o programovani
+; grafickych dem a her na PC v DOSu:
+; https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+;
+; Clanek, kde je tento demonstracni priklad pouzit:
+; Výpočty v systému pevné řádové čárky na platformě IBM PC
+; https://www.root.cz/clanky/vypocty-v-systemu-pevne-radove-carky-na-platforme-ibm-pc/
+;
+;-----------------------------------------------------------------------------
+
 fx_add(unsigned int, unsigned int):
         lea     eax, [rdi+rsi]
         ret
