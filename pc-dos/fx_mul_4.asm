b97bda091e558e2d2bac20944b5638c4215a9cc1
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-09 09:04:38 +0200

    FX multiply for large values

---
+++ pc-dos/fx_mul_4.asm
@@ -0,0 +1,39 @@
+;-----------------------------------------------------------------------------
+org  0x100        ; zacatek kodu pro programy typu COM (vzdy se zacina na 256)
+
+start:
+        jmp main                   ; skok na zacatek kodu
+
+%include "io.asm"                  ; nacist symboly, makra a podprogramy
+%include "print.asm"               ; nacist symboly, makra a podprogramy
+
+P       equ     65536              ; poloha desetinne tecky v FX-pointu
+
+x       equ     2 * P              ; konstanty, ktere se budou nasobit
+y       equ     P / 256
+
+main:
+        mov  eax, x
+        print_hex eax              ; vytisknout hodnotu prvni konstanty
+
+        mov  eax, y
+        print_hex eax              ; vytisknout hodnotu druhe konstanty
+
+        mov  eax, x
+        mov  ebx, y
+        shr  eax, 8                ; posun jeste pred nasobenim (ztrata presnosti)
+        shr  ebx, 8                ; posun jeste pred nasobenim (ztrata presnosti)
+        mul  ebx                   ; nasobeni v FX-pointu do EDX:EAX
+        print_hex eax
+
+finish:
+        wait_key                   ; cekani na klavesu
+        exit                       ; navrat do DOSu
+
+
+; datova cast
+section .data
+
+section .bss
+
+
