c22d4ad12fbba595f7da7e1f925582b9121887cc
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-14 08:25:45 +0200

    FX operations implemented in C

--- pc-dos/fx.c
+++ pc-dos/fx.c
@@ -1,3 +1,15 @@
+/*
+ *
+ * Tento demonstracni priklad je pouzity v serialu o programovani
+ * grafickych dem a her na PC v DOSu:
+ * https://www.root.cz/serialy/vyvoj-her-a-grafickych-dem-pro-platformu-pc/
+ *
+ * Clanek, kde je tento demonstracni priklad pouzit:
+ * Výpočty v systému pevné řádové čárky na platformě IBM PC (2. část)
+ * https://www.root.cz/clanky/vypocty-v-systemu-pevne-radove-carky-na-platforme-ibm-pc-2-cast/
+ *
+*/
+
 #include <stdint.h>
 
 uint32_t fx_add(uint32_t x, uint32_t y) {
