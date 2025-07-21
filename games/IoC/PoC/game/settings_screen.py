8e26fcafd5880d2d5ed6097eab21421db6c2e79f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-21 08:03:09 +0200

    Fixed imports

--- games/IoC/PoC/game/settings_screen.py
+++ games/IoC/PoC/game/settings_screen.py
@@ -15,11 +15,10 @@
 """Settings screen displayed in the game and selected from main menu."""
 
 import pygame
-
 from abstract_menu_screen import AbstractMenuScreen
 from colors import Colors
-from resources import Resources
 from ghost import Ghost
+from resources import Resources
 
 
 class SettingsScreen(AbstractMenuScreen):
