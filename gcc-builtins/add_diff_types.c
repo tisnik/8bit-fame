36bc31ea06700f98e5a7301cb9f6b00e4cb28e53
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-28 08:33:59 +0200

    Add builtin with different value types - source in C (GCC)

---
+++ gcc-builtins/add_diff_types.c
@@ -0,0 +1,50 @@
+#include <stdbool.h>
+
+bool add_overflow_ccc(unsigned char x, unsigned char y) {
+    unsigned char z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
+bool add_overflow_cci(unsigned char x, unsigned char y) {
+    unsigned int z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
+bool add_overflow_cic(unsigned char x, unsigned int y) {
+    unsigned char z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
+bool add_overflow_cii(unsigned char x, unsigned int y) {
+    unsigned int z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
+bool add_overflow_icc(unsigned char x, unsigned char y) {
+    unsigned char z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
+bool add_overflow_ici(unsigned int x, unsigned char y) {
+    unsigned int z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
+bool add_overflow_iic(unsigned int x, unsigned int y) {
+    unsigned char z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
+bool add_overflow_iii(unsigned int x, unsigned int y) {
+    unsigned int z;
+    bool overflow = __builtin_add_overflow(x, y, &z);
+    return overflow;
+}
+
