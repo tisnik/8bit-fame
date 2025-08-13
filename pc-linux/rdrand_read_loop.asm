807f80a1cbe20645385aaa4fa80706edf08d8dfc
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-13 11:47:26 +0200

    Reformatted and expanded tabs

--- pc-linux/rdrand_read_loop.asm
+++ pc-linux/rdrand_read_loop.asm
@@ -7,7 +7,7 @@ section .data
 
 hex_message:
          times 8 db '?'
-	 db 0x0a
+         db 0x0a
          hex_message_length equ $ - hex_message
 
 ;-----------------------------------------------------------------------------
@@ -19,17 +19,17 @@ section .text
         global _start                ; tento symbol ma byt dostupny i linkeru
 
 _start:
-	mov     ecx, 10              ; pocitadlo smycky
+        mov     ecx, 10              ; pocitadlo smycky
 rand:
-	push    ecx                  ; uchovat hodnotu pocitadla
+        push    ecx                  ; uchovat hodnotu pocitadla
 
-	rdrand  edx                  ; ziskat pseudonahodnou hodnotu
+        rdrand  edx                  ; ziskat pseudonahodnou hodnotu
         mov     ebx, hex_message     ; buffer, ktery se zaplni hexa cislicemi
         call    hex2string           ; zavolani prislusne subrutiny
         print_string   hex_message, hex_message_length    ; tisk hexadecimalni hodnoty
 
-	pop     ecx                  ; obnovit hodnotu pocitadla
-	loop    rand                 ; opakovat smycku
+        pop     ecx                  ; obnovit hodnotu pocitadla
+        loop    rand                 ; opakovat smycku
 
         exit                         ; ukonceni procesu
 
