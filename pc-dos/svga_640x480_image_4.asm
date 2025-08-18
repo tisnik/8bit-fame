bdf2308371f410c2034cdb3ee9efd879ecb8e599
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-18 08:39:00 +0200

    Reformatted and expanded tabs

--- pc-dos/svga_640x480_image_4.asm
+++ pc-dos/svga_640x480_image_4.asm
@@ -46,24 +46,24 @@ success:
         mov ds, ax
         mov si, image              ; nyni DS:SI obsahuje adresu prvniho bajtu v obrazku
 
-	mov  dx, 0                 ; cislo pametoveho banku video pameti
-	call set_video_window      ; nastaveni pametoveho banku
+        mov  dx, 0                 ; cislo pametoveho banku video pameti
+        call set_video_window      ; nastaveni pametoveho banku
 
-	xor di, di                 ; offset na zacatek video RAM
+        xor di, di                 ; offset na zacatek video RAM
         mov al, 82                 ; pocet prenesenych radku
-	call bitblt                ; zavolat subrutinu pro blokovy prenos
+        call bitblt                ; zavolat subrutinu pro blokovy prenos
 
-	mov  dx, 1                 ; cislo pametoveho banku video pameti
-	call set_video_window      ; nastaveni pametoveho banku
-	mov di, 82*800-65536       ; offset na zacatek video RAM
+        mov  dx, 1                 ; cislo pametoveho banku video pameti
+        call set_video_window      ; nastaveni pametoveho banku
+        mov di, 82*800-65536       ; offset na zacatek video RAM
         mov al, 82                 ; pocet prenesenych radku
-	call bitblt                ; zavolat subrutinu pro blokovy prenos
+        call bitblt                ; zavolat subrutinu pro blokovy prenos
 
-	mov  dx, 2                 ; cislo pametoveho banku video pameti
-	call set_video_window      ; nastaveni pametoveho banku
-	mov di, 2*82*800-65536*2   ; offset na zacatek video RAM
+        mov  dx, 2                 ; cislo pametoveho banku video pameti
+        call set_video_window      ; nastaveni pametoveho banku
+        mov di, 2*82*800-65536*2   ; offset na zacatek video RAM
         mov al, 200-82*2           ; pocet prenesenych radku
-	call bitblt                ; zavolat subrutinu pro blokovy prenos
+        call bitblt                ; zavolat subrutinu pro blokovy prenos
 
         jmp     finish
 
@@ -80,17 +80,17 @@ bitblt:
 next_line:
         mov cx, 320/2              ; pocet zapisovanych bajtu (=pixelu)
         rep movsw                  ; prenos jednoho obrazoveho radku
-	add di, 800-320            ; preskocit zbytek obrazoveho radku
-	dec al                     ; snizit pocitadlo radku
-	jnz next_line
-	ret                        ; navrat ze subrutiny
+        add di, 800-320            ; preskocit zbytek obrazoveho radku
+        dec al                     ; snizit pocitadlo radku
+        jnz next_line
+        ret                        ; navrat ze subrutiny
 
 set_video_window:
         ; ocekava se, ze DL je nastaven korektne!
-	mov ax, 0x4f05             ; nastaveni okna
-	mov bx, 0x0000             ; okno A
-	int     0x10               ; volani VBE
-	ret                        ; navrat ze subrutiny
+        mov ax, 0x4f05             ; nastaveni okna
+        mov bx, 0x0000             ; okno A
+        int     0x10               ; volani VBE
+        ret                        ; navrat ze subrutiny
 
 ; paleta ve stupnich sedi
 grayscale_palette:
@@ -105,7 +105,7 @@ next_dac:
         int 0x10                   ; modifikace mapovani v DAC
         inc bl                     ; zvysit index v DAC
         jnz next_dac               ; nastavit dalsi barvu, dokud nedosahneme hodnoty 256
-	ret                        ; navrat ze subrutiny
+        ret                        ; navrat ze subrutiny
 
 
 ; datova cast
