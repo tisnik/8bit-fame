51f8ddc6264e97a4c9096e8446c79802a9e105c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-26 09:50:37 +0200

    Fix: peform rotate left operation, not rotate right

--- gcc-builtins/rotate_left_x86_09.asm
+++ gcc-builtins/rotate_left_x86_09.asm
@@ -1,23 +1,23 @@
 rotate_left_8bit:
         mov     eax, edi
         mov     ecx, esi
-        ror     al, cl
+        rol     al, cl
         ret
 
 rotate_left_16bit:
         mov     eax, edi
         mov     ecx, esi
-        ror     ax, cl
+        rol     ax, cl
         ret
 
 rotate_left_32bit:
         mov     eax, edi
         mov     ecx, esi
-        ror     eax, cl
+        rol     eax, cl
         ret
 
 rotate_left_64bit:
         mov     rax, rdi
         mov     ecx, esi
-        ror     rax, cl
+        rol     rax, cl
         ret
