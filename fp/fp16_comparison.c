405d0168dfb8fefc283c95993d766c90804b5900
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-08 08:14:48 +0200

    FP16 comparison instructions, original C code

---
+++ fp/fp16_comparison.c
@@ -0,0 +1,23 @@
+bool fp16_eq(_Float16 x, _Float16 y) {
+    return x == y;
+}
+
+bool fp16_ne(_Float16 x, _Float16 y) {
+    return x != y;
+}
+
+bool fp16_gt(_Float16 x, _Float16 y) {
+    return x > y;
+}
+
+bool fp16_ge(_Float16 x, _Float16 y) {
+    return x >= y;
+}
+
+bool fp16_lt(_Float16 x, _Float16 y) {
+    return x < y;
+}
+
+bool fp16_le(_Float16 x, _Float16 y) {
+    return x <= y;
+}
