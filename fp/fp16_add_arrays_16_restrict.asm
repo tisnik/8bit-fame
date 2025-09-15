f99053aeae8c8c9b879ab33495d7ac550013e116
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-15 09:20:03 +0200

    Adding two arrays of half float items, assembly output

---
+++ fp/fp16_add_arrays_16_restrict.asm
@@ -0,0 +1,7 @@
+add_arrays:
+        ldp     q29, q31, [x0]
+        ldp     q28, q30, [x1]
+        fadd    v28.8h, v29.8h, v28.8h
+        fadd    v30.8h, v31.8h, v30.8h
+        stp     q28, q30, [x0]
+        ret
