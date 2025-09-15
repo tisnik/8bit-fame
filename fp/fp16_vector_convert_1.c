b6f8e2a7400e6618384d63b348573160337ec223
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-15 09:20:04 +0200

    Coversion of all vector items: half float->float, original C source

---
+++ fp/fp16_vector_convert_1.c
@@ -0,0 +1,7 @@
+typedef _Float16 float16x8 __attribute__((vector_size(16)));
+typedef float float32x8 __attribute__((vector_size(32)));
+
+float32x8 to_float32x8(float16x8 x)
+{
+    return __builtin_convertvector(x, float32x8);
+}
