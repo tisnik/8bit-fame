4f4fbb59907093ca56e4da6af72b738777ea8ae3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-15 09:20:03 +0200

    Per-item arithmetic operations over vectors, assembly output

---
+++ fp/fp16_vector_arith_1.asm
@@ -0,0 +1,15 @@
+add:
+        fadd    v0.8h, v0.8h, v1.8h
+        ret
+
+sub:
+        fsub    v0.8h, v0.8h, v1.8h
+        ret
+
+mul:
+        fmul    v0.8h, v0.8h, v1.8h
+        ret
+
+div:
+        fdiv    v0.8h, v0.8h, v1.8h
+        ret
