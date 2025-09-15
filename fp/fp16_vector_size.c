696fe45445c1322bd4ec85df0918734d1c23fbc7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-15 09:20:04 +0200

    Retrieving vector size

---
+++ fp/fp16_vector_size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+typedef _Float16 float16x8 __attribute__((vector_size(16)));
+
+int main(void)
+{
+    printf("scalar: %ld bytes\n", sizeof(_Float16));
+    printf("vector: %ld bytes\n", sizeof(float16x8));
+ 
+    return 0;
+}
