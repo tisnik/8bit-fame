d80e5ce38f02b8d81a95017c2d61279f799251a5
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-24 10:46:03 +0200

    Dot product implemented in C

---
+++ fp/fp16_dot_product.c
@@ -0,0 +1,10 @@
+typedef _Float16 float16x8 __attribute__((vector_size(16)));
+
+_Float16 dot_product(float16x8 a, float16x8 b) {
+    int i;
+    _Float16 result = 0.0;
+    for (i=0; i<sizeof(float16x8)/sizeof(_Float16); i++) {
+        result += a[i] * b[i];
+    }
+    return result;
+}
