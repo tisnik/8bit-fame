1cf2b642f57e32e4991cc88b812ad397fd122a65
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-01 09:42:19 +0100

    Fibonacci computation implemented in C

---
+++ LLVM_IR/fibonacci.c
@@ -0,0 +1,14 @@
+long fibonacciIter(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    long result = 0;
+    long n1 = 0;
+    long n2 = 1;
+    for(n--; n > 0; n--) {
+        result = n1 + n2;
+        n1 = n2;
+        n2 = result;
+    }
+    return result;
+}
