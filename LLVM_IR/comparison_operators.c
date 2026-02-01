564c467543c786140318a81eb8ce78975272a2a7
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-01 09:42:19 +0100

    Comparison operators in C

---
+++ LLVM_IR/comparison_operators.c
@@ -0,0 +1,17 @@
+#define EQ(type) type eq_##type(type x, type y) {return x==y;}
+#define NE(type) type ne_##type(type x, type y) {return x!=y;}
+#define LT(type) type lt_##type(type x, type y) {return x<y;}
+#define LE(type) type le_##type(type x, type y) {return x<=y;}
+#define GT(type) type gt_##type(type x, type y) {return x>y;}
+#define GE(type) type ge_##type(type x, type y) {return x>=y;}
+
+#define ALL(type) \
+    EQ(type) \
+    NE(type) \
+    LT(type) \
+    LE(type) \
+    GT(type) \
+    GE(type)
+
+ALL(int)
+ALL(unsigned)
