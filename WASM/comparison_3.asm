b6d9b524bf48b54eea86ab5dd0296eefb5cac9da
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Compiled into WebAssembly

---
+++ WASM/comparison_3.asm
@@ -0,0 +1,34 @@
+comparison_3.wasm:	file format wasm 0x1
+
+Code Disassembly:
+
+000048 func[0] <eq>:
+ 000049: 20 00                      | local.get 0
+ 00004b: 20 01                      | local.get 1
+ 00004d: 51                         | i64.eq
+ 00004e: 0b                         | end
+000050 func[1] <ne>:
+ 000051: 20 00                      | local.get 0
+ 000053: 20 01                      | local.get 1
+ 000055: 52                         | i64.ne
+ 000056: 0b                         | end
+000058 func[2] <lt>:
+ 000059: 20 00                      | local.get 0
+ 00005b: 20 01                      | local.get 1
+ 00005d: 53                         | i64.lt_s
+ 00005e: 0b                         | end
+000060 func[3] <le>:
+ 000061: 20 00                      | local.get 0
+ 000063: 20 01                      | local.get 1
+ 000065: 57                         | i64.le_s
+ 000066: 0b                         | end
+000068 func[4] <gt>:
+ 000069: 20 00                      | local.get 0
+ 00006b: 20 01                      | local.get 1
+ 00006d: 55                         | i64.gt_s
+ 00006e: 0b                         | end
+000070 func[5] <ge>:
+ 000071: 20 00                      | local.get 0
+ 000073: 20 01                      | local.get 1
+ 000075: 59                         | i64.ge_s
+ 000076: 0b                         | end
