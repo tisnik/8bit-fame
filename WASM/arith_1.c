8b9167b9a57cb9a0b8ad9496e0465f820d9fff21
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Basic arithmetic operations in C for integers

---
+++ WASM/arith_1.c
@@ -0,0 +1,39 @@
+int iadd(int a, int b) {
+    return a + b;
+}
+
+int isub(int a, int b) {
+    return a - b;
+}
+
+int imul(int a, int b) {
+    return a * b;
+}
+
+int idiv(int a, int b) {
+    return a / b;
+}
+
+int imod(int a, int b) {
+    return a % b;
+}
+
+unsigned int uadd(unsigned int a, unsigned int b) {
+    return a + b;
+}
+
+unsigned int usub(unsigned int a, unsigned int b) {
+    return a - b;
+}
+
+unsigned int umul(unsigned int a, unsigned int b) {
+    return a * b;
+}
+
+unsigned int udiv(unsigned int a, unsigned int b) {
+    return a / b;
+}
+
+unsigned int umod(unsigned int a, unsigned int b) {
+    return a % b;
+}
