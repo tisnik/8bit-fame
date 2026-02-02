5e3981e011161e5ed5921c0cefcd573b4b1621d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Comparison operations for doubles

---
+++ WASM/comparison_6.c
@@ -0,0 +1,24 @@
+int eq(double a, double b) {
+    return a == b;
+}
+
+int ne(double a, double b) {
+    return a != b;
+}
+
+int lt(double a, double b) {
+    return a < b;
+}
+
+int le(double a, double b) {
+    return a <= b;
+}
+
+int gt(double a, double b) {
+    return a > b;
+}
+
+int ge(double a, double b) {
+    return a >= b;
+}
+
