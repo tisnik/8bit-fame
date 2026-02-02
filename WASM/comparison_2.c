2fbc0b0999eb0cf61e9821fb399064c3959387cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Comparison operations for unsigned integers

---
+++ WASM/comparison_2.c
@@ -0,0 +1,24 @@
+unsigned int eq(unsigned int a, unsigned int b) {
+    return a == b;
+}
+
+unsigned int ne(unsigned int a, unsigned int b) {
+    return a != b;
+}
+
+unsigned int lt(unsigned int a, unsigned int b) {
+    return a < b;
+}
+
+unsigned int le(unsigned int a, unsigned int b) {
+    return a <= b;
+}
+
+unsigned int gt(unsigned int a, unsigned int b) {
+    return a > b;
+}
+
+unsigned int ge(unsigned int a, unsigned int b) {
+    return a >= b;
+}
+
