18af2b830a9c0ab40ff656c0477c38f23bc5f948
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Basic arithmetic operations in C for longs

---
+++ WASM/arith_2.c
@@ -0,0 +1,39 @@
+long long iadd(long long a, long long b) {
+    return a + b;
+}
+
+long long isub(long long a, long long b) {
+    return a - b;
+}
+
+long long imul(long long a, long long b) {
+    return a * b;
+}
+
+long long idiv(long long a, long long b) {
+    return a / b;
+}
+
+long long imod(long long a, long long b) {
+    return a % b;
+}
+
+unsigned long long uadd(unsigned long long a, unsigned long long b) {
+    return a + b;
+}
+
+unsigned long long usub(unsigned long long a, unsigned long long b) {
+    return a - b;
+}
+
+unsigned long long umul(unsigned long long a, unsigned long long b) {
+    return a * b;
+}
+
+unsigned long long udiv(unsigned long long a, unsigned long long b) {
+    return a / b;
+}
+
+unsigned long long umod(unsigned long long a, unsigned long long b) {
+    return a % b;
+}
