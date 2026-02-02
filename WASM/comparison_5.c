af41d2a45305bb940c77302de850e2eb1d06a31a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Comparison operations for floats

---
+++ WASM/comparison_5.c
@@ -0,0 +1,24 @@
+int eq(float a, float b) {
+    return a == b;
+}
+
+int ne(float a, float b) {
+    return a != b;
+}
+
+int lt(float a, float b) {
+    return a < b;
+}
+
+int le(float a, float b) {
+    return a <= b;
+}
+
+int gt(float a, float b) {
+    return a > b;
+}
+
+int ge(float a, float b) {
+    return a >= b;
+}
+
