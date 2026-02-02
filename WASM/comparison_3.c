dafc51691ce7dc5ebd8cd3333b4daf2797a6cf22
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Comparison operations for longs

---
+++ WASM/comparison_3.c
@@ -0,0 +1,24 @@
+int eq(long long a, long long b) {
+    return a == b;
+}
+
+int ne(long long a, long long b) {
+    return a != b;
+}
+
+int lt(long long a, long long b) {
+    return a < b;
+}
+
+int le(long long a, long long b) {
+    return a <= b;
+}
+
+int gt(long long a, long long b) {
+    return a > b;
+}
+
+int ge(long long a, long long b) {
+    return a >= b;
+}
+
