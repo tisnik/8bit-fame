a690d065514132224f2e24ae2454fa842560d674
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Comparison operations for long longs

---
+++ WASM/comparison_4.c
@@ -0,0 +1,24 @@
+int eq(unsigned long long a, unsigned long long b) {
+    return a == b;
+}
+
+int ne(unsigned long long a, unsigned long long b) {
+    return a != b;
+}
+
+int lt(unsigned long long a, unsigned long long b) {
+    return a < b;
+}
+
+int le(unsigned long long a, unsigned long long b) {
+    return a <= b;
+}
+
+int gt(unsigned long long a, unsigned long long b) {
+    return a > b;
+}
+
+int ge(unsigned long long a, unsigned long long b) {
+    return a >= b;
+}
+
