9de903f2c19902c6c8c63aeca1e201c92c9b66d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Basic arithmetic operations in C for floats

---
+++ WASM/arith_3.c
@@ -0,0 +1,26 @@
+float fadd(float a, float b) {
+    return a + b;
+}
+
+float fsub(float a, float b) {
+    return a - b;
+}
+
+float fmul(float a, float b) {
+    return a * b;
+}
+
+float fdiv(float a, float b) {
+    return a / b;
+}
+
+float sqrtf(float x);
+float fabsf(float x);
+
+float fsqrt(float a) {
+    return sqrtf(a);
+}
+
+float fabs(float a) {
+    return fabsf(a);
+}
