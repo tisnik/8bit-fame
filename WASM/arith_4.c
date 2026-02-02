e48c9df4dc04f60d0b38b3aa9ef1793b56685e9f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-02 08:29:09 +0100

    Basic arithmetic operations in C for doubles

---
+++ WASM/arith_4.c
@@ -0,0 +1,26 @@
+double fadd(double a, double b) {
+    return a + b;
+}
+
+double fsub(double a, double b) {
+    return a - b;
+}
+
+double fmul(double a, double b) {
+    return a * b;
+}
+
+double fdiv(double a, double b) {
+    return a / b;
+}
+
+double sqrt(double x);
+double fabs(double x);
+
+double fsqrt(double a) {
+    return sqrt(a);
+}
+
+double fabs_(double a) {
+    return fabs(a);
+}
