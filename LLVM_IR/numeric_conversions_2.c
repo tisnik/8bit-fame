769436225340322611da173ed8704eba2f82547a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-03 08:28:33 +0100

    Numeric conversions: signed integers

---
+++ LLVM_IR/numeric_conversions_2.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+
+#define CONVERT(type1, type2) type2 convert_##type1##_to_##type2(type1 x) {return (type2)x;}
+
+CONVERT(int8_t, int8_t)
+CONVERT(int8_t, int16_t)
+CONVERT(int8_t, int32_t)
+CONVERT(int8_t, int64_t)
+
+CONVERT(int16_t, int8_t)
+CONVERT(int16_t, int16_t)
+CONVERT(int16_t, int32_t)
+CONVERT(int16_t, int64_t)
+
+CONVERT(int32_t, int8_t)
+CONVERT(int32_t, int16_t)
+CONVERT(int32_t, int32_t)
+CONVERT(int32_t, int64_t)
+
+CONVERT(int64_t, int8_t)
+CONVERT(int64_t, int16_t)
+CONVERT(int64_t, int32_t)
+CONVERT(int64_t, int64_t)
