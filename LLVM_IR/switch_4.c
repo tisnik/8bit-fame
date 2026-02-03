adc5121bdc10462c93a864a0147ce9c03efe5e20
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-03 08:28:33 +0100

    Switch statement in C sources, example #4

---
+++ LLVM_IR/switch_4.c
@@ -0,0 +1,15 @@
+int numeric_value(int x) {
+    switch (x) {
+        case 0:
+            return 10;
+        case 1:
+            return 6;
+        case 2:
+            return 3;
+        case 3:
+            return -1;
+        default:
+            return -2;
+    }
+}
+
