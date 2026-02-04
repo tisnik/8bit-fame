c549ef1fccbe4997a7130ee6886e88390d2aa24e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-04 09:00:38 +0100

    Conversions from int to char, short etc.

---
+++ WASM/conversions_2.c
@@ -0,0 +1,19 @@
+char char_from_int(int x) {
+    return x;
+}
+
+short short_from_int(int x) {
+    return x;
+}
+
+char char_from_long(long long x) {
+    return x;
+}
+
+short short_from_long(long long x) {
+    return x;
+}
+
+int int_from_long(long long x) {
+    return x;
+}
