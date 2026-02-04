4cf33ce5031b8d26ec07a4ed01ee5aeb66a038b2
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-04 09:00:27 +0100

    Arithmetic operators for floating point values, C source

---
+++ LLVM_IR/arith_operators_float.c
@@ -0,0 +1,17 @@
+#define NEG(type) type neg_##type(type x) {return -x;}
+#define ADD(type) type add_##type(type x, type y) {return x+y;}
+#define SUB(type) type sub_##type(type x, type y) {return x-y;}
+#define MUL(type) type mul_##type(type x, type y) {return x*y;}
+#define DIV(type) type div_##type(type x, type y) {return x/y;}
+#define SHL(type) type shl_##type(type x, type y) {return x<<y;}
+#define SHR(type) type shr_##type(type x, type y) {return x>>y;}
+
+#define ALL(type) \
+    NEG(type) \
+    ADD(type) \
+    SUB(type) \
+    MUL(type) \
+    DIV(type)
+
+ALL(float)
+ALL(double)
