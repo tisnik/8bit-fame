a4f267565971b329eaaf21e6a02f42a880dad381
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-04 09:00:38 +0100

    Compilation into WebAssembly

---
+++ WASM/conversions_1.asm
@@ -0,0 +1,13 @@
+
+conversions_1.wasm:	file format wasm 0x1
+
+Code Disassembly:
+
+000043 func[0] <from_int>:
+ 000044: 20 00                      | local.get 0
+ 000046: ac                         | i64.extend_i32_s
+ 000047: 0b                         | end
+000049 func[1] <from_uint>:
+ 00004a: 20 00                      | local.get 0
+ 00004c: ad                         | i64.extend_i32_u
+ 00004d: 0b                         | end
