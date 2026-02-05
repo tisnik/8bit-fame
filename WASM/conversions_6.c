1c4d4a4a88ede03aaa83beccd4ed45602ab56ada
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-05 08:48:22 +0100

    Numeric conversion operation, 6th variant

---
+++ WASM/conversions_6.c
@@ -0,0 +1,15 @@
+int int_from_float(float x) {
+    return x;
+}
+
+long long long_from_float(float x) {
+    return x;
+}
+
+int int_from_double(double x) {
+    return x;
+}
+
+long long long_from_double(double x) {
+    return x;
+}
