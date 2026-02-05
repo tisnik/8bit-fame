cacf59607d1b2a4542d8cec3d89f2e1120a6d1c0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-05 08:48:22 +0100

    Numeric conversion operation, 5th variant

---
+++ WASM/conversions_5.c
@@ -0,0 +1,31 @@
+float float_from_int(int x) {
+    return x;
+}
+
+double double_from_int(int x) {
+    return x;
+}
+
+float float_from_long(long long x) {
+    return x;
+}
+
+double double_from_long(long long x) {
+    return x;
+}
+
+float float_from_unsigned_int(unsigned int x) {
+    return x;
+}
+
+double double_from_unsigned_int(unsigned int x) {
+    return x;
+}
+
+float float_from_unsigned_long(unsigned long long x) {
+    return x;
+}
+
+double double_from_unsigned_long(unsigned long long x) {
+    return x;
+}
