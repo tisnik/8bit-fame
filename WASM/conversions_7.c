b8d727cc6d87b91e8bbe1beeb00fd45ff195f2cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-05 08:48:22 +0100

    Numeric conversion operation, 7th variant

---
+++ WASM/conversions_7.c
@@ -0,0 +1,16 @@
+unsigned int unsigned_int_from_float(float x) {
+    return x;
+}
+
+unsigned long long unsigned_long_from_float(float x) {
+    return x;
+}
+
+unsigned int unsigned_int_from_double(double x) {
+    return x;
+}
+
+unsigned long long unsigned_long_from_double(double x) {
+    return x;
+}
+
