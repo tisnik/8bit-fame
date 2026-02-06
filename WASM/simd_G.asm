085d470cb24b6414fb9e554bea443b7fa36b18e7
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-06 09:28:54 +0100

    Compilation into WebAssembly

---
+++ WASM/simd_G.asm
@@ -0,0 +1,365 @@
+
+simd_G.wasm:	file format wasm 0x1
+
+Code Disassembly:
+
+00007e func[0] <eq_i8x16>:
+ 00007f: 20 00                      | local.get 0
+ 000081: 20 01                      | local.get 1
+ 000083: fd 23                      | i8x16.eq
+ 000085: 0b                         | end
+000087 func[1] <lt_i8x16>:
+ 000088: 20 00                      | local.get 0
+ 00008a: 20 01                      | local.get 1
+ 00008c: fd 25                      | i8x16.lt_s
+ 00008e: 0b                         | end
+000090 func[2] <le_i8x16>:
+ 000091: 20 00                      | local.get 0
+ 000093: 20 01                      | local.get 1
+ 000095: fd 29                      | i8x16.le_s
+ 000097: 0b                         | end
+000099 func[3] <gt_i8x16>:
+ 00009a: 20 00                      | local.get 0
+ 00009c: 20 01                      | local.get 1
+ 00009e: fd 27                      | i8x16.gt_s
+ 0000a0: 0b                         | end
+0000a2 func[4] <ge_i8x16>:
+ 0000a3: 20 00                      | local.get 0
+ 0000a5: 20 01                      | local.get 1
+ 0000a7: fd 2b                      | i8x16.ge_s
+ 0000a9: 0b                         | end
+0000ab func[5] <ne_i8x16>:
+ 0000ac: 20 00                      | local.get 0
+ 0000ae: 20 01                      | local.get 1
+ 0000b0: fd 24                      | i8x16.ne
+ 0000b2: 0b                         | end
+0000b4 func[6] <eq_u8x16>:
+ 0000b5: 20 00                      | local.get 0
+ 0000b7: 20 01                      | local.get 1
+ 0000b9: fd 23                      | i8x16.eq
+ 0000bb: 0b                         | end
+0000bd func[7] <lt_u8x16>:
+ 0000be: 20 00                      | local.get 0
+ 0000c0: 20 01                      | local.get 1
+ 0000c2: fd 26                      | i8x16.lt_u
+ 0000c4: 0b                         | end
+0000c6 func[8] <le_u8x16>:
+ 0000c7: 20 00                      | local.get 0
+ 0000c9: 20 01                      | local.get 1
+ 0000cb: fd 2a                      | i8x16.le_u
+ 0000cd: 0b                         | end
+0000cf func[9] <gt_u8x16>:
+ 0000d0: 20 00                      | local.get 0
+ 0000d2: 20 01                      | local.get 1
+ 0000d4: fd 28                      | i8x16.gt_u
+ 0000d6: 0b                         | end
+0000d8 func[10] <ge_u8x16>:
+ 0000d9: 20 00                      | local.get 0
+ 0000db: 20 01                      | local.get 1
+ 0000dd: fd 2c                      | i8x16.ge_u
+ 0000df: 0b                         | end
+0000e1 func[11] <ne_u8x16>:
+ 0000e2: 20 00                      | local.get 0
+ 0000e4: 20 01                      | local.get 1
+ 0000e6: fd 24                      | i8x16.ne
+ 0000e8: 0b                         | end
+0000ea func[12] <eq_i16x8>:
+ 0000eb: 20 00                      | local.get 0
+ 0000ed: 20 01                      | local.get 1
+ 0000ef: fd 2d                      | i16x8.eq
+ 0000f1: 0b                         | end
+0000f3 func[13] <lt_i16x8>:
+ 0000f4: 20 00                      | local.get 0
+ 0000f6: 20 01                      | local.get 1
+ 0000f8: fd 2f                      | i16x8.lt_s
+ 0000fa: 0b                         | end
+0000fc func[14] <le_i16x8>:
+ 0000fd: 20 00                      | local.get 0
+ 0000ff: 20 01                      | local.get 1
+ 000101: fd 33                      | i16x8.le_s
+ 000103: 0b                         | end
+000105 func[15] <gt_i16x8>:
+ 000106: 20 00                      | local.get 0
+ 000108: 20 01                      | local.get 1
+ 00010a: fd 31                      | i16x8.gt_s
+ 00010c: 0b                         | end
+00010e func[16] <ge_i16x8>:
+ 00010f: 20 00                      | local.get 0
+ 000111: 20 01                      | local.get 1
+ 000113: fd 35                      | i16x8.ge_s
+ 000115: 0b                         | end
+000117 func[17] <ne_i16x8>:
+ 000118: 20 00                      | local.get 0
+ 00011a: 20 01                      | local.get 1
+ 00011c: fd 2e                      | i16x8.ne
+ 00011e: 0b                         | end
+000120 func[18] <eq_u16x8>:
+ 000121: 20 00                      | local.get 0
+ 000123: 20 01                      | local.get 1
+ 000125: fd 2d                      | i16x8.eq
+ 000127: 0b                         | end
+000129 func[19] <lt_u16x8>:
+ 00012a: 20 00                      | local.get 0
+ 00012c: 20 01                      | local.get 1
+ 00012e: fd 30                      | i16x8.lt_u
+ 000130: 0b                         | end
+000132 func[20] <le_u16x8>:
+ 000133: 20 00                      | local.get 0
+ 000135: 20 01                      | local.get 1
+ 000137: fd 34                      | i16x8.le_u
+ 000139: 0b                         | end
+00013b func[21] <gt_u16x8>:
+ 00013c: 20 00                      | local.get 0
+ 00013e: 20 01                      | local.get 1
+ 000140: fd 32                      | i16x8.gt_u
+ 000142: 0b                         | end
+000144 func[22] <ge_u16x8>:
+ 000145: 20 00                      | local.get 0
+ 000147: 20 01                      | local.get 1
+ 000149: fd 36                      | i16x8.ge_u
+ 00014b: 0b                         | end
+00014d func[23] <ne_u16x8>:
+ 00014e: 20 00                      | local.get 0
+ 000150: 20 01                      | local.get 1
+ 000152: fd 2e                      | i16x8.ne
+ 000154: 0b                         | end
+000156 func[24] <eq_i32x4>:
+ 000157: 20 00                      | local.get 0
+ 000159: 20 01                      | local.get 1
+ 00015b: fd 37                      | i32x4.eq
+ 00015d: 0b                         | end
+00015f func[25] <lt_i32x4>:
+ 000160: 20 00                      | local.get 0
+ 000162: 20 01                      | local.get 1
+ 000164: fd 39                      | i32x4.lt_s
+ 000166: 0b                         | end
+000168 func[26] <le_i32x4>:
+ 000169: 20 00                      | local.get 0
+ 00016b: 20 01                      | local.get 1
+ 00016d: fd 3d                      | i32x4.le_s
+ 00016f: 0b                         | end
+000171 func[27] <gt_i32x4>:
+ 000172: 20 00                      | local.get 0
+ 000174: 20 01                      | local.get 1
+ 000176: fd 3b                      | i32x4.gt_s
+ 000178: 0b                         | end
+00017a func[28] <ge_i32x4>:
+ 00017b: 20 00                      | local.get 0
+ 00017d: 20 01                      | local.get 1
+ 00017f: fd 3f                      | i32x4.ge_s
+ 000181: 0b                         | end
+000183 func[29] <ne_i32x4>:
+ 000184: 20 00                      | local.get 0
+ 000186: 20 01                      | local.get 1
+ 000188: fd 38                      | i32x4.ne
+ 00018a: 0b                         | end
+00018c func[30] <eq_u32x4>:
+ 00018d: 20 00                      | local.get 0
+ 00018f: 20 01                      | local.get 1
+ 000191: fd 37                      | i32x4.eq
+ 000193: 0b                         | end
+000195 func[31] <lt_u32x4>:
+ 000196: 20 00                      | local.get 0
+ 000198: 20 01                      | local.get 1
+ 00019a: fd 3a                      | i32x4.lt_u
+ 00019c: 0b                         | end
+00019e func[32] <le_u32x4>:
+ 00019f: 20 00                      | local.get 0
+ 0001a1: 20 01                      | local.get 1
+ 0001a3: fd 3e                      | i32x4.le_u
+ 0001a5: 0b                         | end
+0001a7 func[33] <gt_u32x4>:
+ 0001a8: 20 00                      | local.get 0
+ 0001aa: 20 01                      | local.get 1
+ 0001ac: fd 3c                      | i32x4.gt_u
+ 0001ae: 0b                         | end
+0001b0 func[34] <ge_u32x4>:
+ 0001b1: 20 00                      | local.get 0
+ 0001b3: 20 01                      | local.get 1
+ 0001b5: fd 40                      | i32x4.ge_u
+ 0001b7: 0b                         | end
+0001b9 func[35] <ne_u32x4>:
+ 0001ba: 20 00                      | local.get 0
+ 0001bc: 20 01                      | local.get 1
+ 0001be: fd 38                      | i32x4.ne
+ 0001c0: 0b                         | end
+0001c2 func[36] <eq_i64x2>:
+ 0001c3: 20 00                      | local.get 0
+ 0001c5: 20 01                      | local.get 1
+ 0001c7: fd d6 01                   | i64x2.eq
+ 0001ca: 0b                         | end
+0001cc func[37] <lt_i64x2>:
+ 0001cd: 20 00                      | local.get 0
+ 0001cf: 20 01                      | local.get 1
+ 0001d1: fd d8 01                   | i64x2.lt_s
+ 0001d4: 0b                         | end
+0001d6 func[38] <le_i64x2>:
+ 0001d7: 20 00                      | local.get 0
+ 0001d9: 20 01                      | local.get 1
+ 0001db: fd da 01                   | i64x2.le_s
+ 0001de: 0b                         | end
+0001e0 func[39] <gt_i64x2>:
+ 0001e1: 20 00                      | local.get 0
+ 0001e3: 20 01                      | local.get 1
+ 0001e5: fd d9 01                   | i64x2.gt_s
+ 0001e8: 0b                         | end
+0001ea func[40] <ge_i64x2>:
+ 0001eb: 20 00                      | local.get 0
+ 0001ed: 20 01                      | local.get 1
+ 0001ef: fd db 01                   | i64x2.ge_s
+ 0001f2: 0b                         | end
+0001f4 func[41] <ne_i64x2>:
+ 0001f5: 20 00                      | local.get 0
+ 0001f7: 20 01                      | local.get 1
+ 0001f9: fd d7 01                   | i64x2.ne
+ 0001fc: 0b                         | end
+0001fe func[42] <eq_u64x2>:
+ 0001ff: 20 00                      | local.get 0
+ 000201: 20 01                      | local.get 1
+ 000203: fd d6 01                   | i64x2.eq
+ 000206: 0b                         | end
+000208 func[43] <lt_u64x2>:
+ 000209: 42 7f                      | i64.const 18446744073709551615
+ 00020b: 42 00                      | i64.const 0
+ 00020d: 20 00                      | local.get 0
+ 00020f: fd 1d 00                   | i64x2.extract_lane 0
+ 000212: 20 01                      | local.get 1
+ 000214: fd 1d 00                   | i64x2.extract_lane 0
+ 000217: 54                         | i64.lt_u
+ 000218: 1b                         | select
+ 000219: fd 12                      | i64x2.splat
+ 00021b: 42 7f                      | i64.const 18446744073709551615
+ 00021d: 42 00                      | i64.const 0
+ 00021f: 20 00                      | local.get 0
+ 000221: fd 1d 01                   | i64x2.extract_lane 1
+ 000224: 20 01                      | local.get 1
+ 000226: fd 1d 01                   | i64x2.extract_lane 1
+ 000229: 54                         | i64.lt_u
+ 00022a: 1b                         | select
+ 00022b: fd 1e 01                   | i64x2.replace_lane 1
+ 00022e: 0b                         | end
+000230 func[44] <le_u64x2>:
+ 000231: 42 7f                      | i64.const 18446744073709551615
+ 000233: 42 00                      | i64.const 0
+ 000235: 20 00                      | local.get 0
+ 000237: fd 1d 00                   | i64x2.extract_lane 0
+ 00023a: 20 01                      | local.get 1
+ 00023c: fd 1d 00                   | i64x2.extract_lane 0
+ 00023f: 58                         | i64.le_u
+ 000240: 1b                         | select
+ 000241: fd 12                      | i64x2.splat
+ 000243: 42 7f                      | i64.const 18446744073709551615
+ 000245: 42 00                      | i64.const 0
+ 000247: 20 00                      | local.get 0
+ 000249: fd 1d 01                   | i64x2.extract_lane 1
+ 00024c: 20 01                      | local.get 1
+ 00024e: fd 1d 01                   | i64x2.extract_lane 1
+ 000251: 58                         | i64.le_u
+ 000252: 1b                         | select
+ 000253: fd 1e 01                   | i64x2.replace_lane 1
+ 000256: 0b                         | end
+000258 func[45] <gt_u64x2>:
+ 000259: 42 7f                      | i64.const 18446744073709551615
+ 00025b: 42 00                      | i64.const 0
+ 00025d: 20 00                      | local.get 0
+ 00025f: fd 1d 00                   | i64x2.extract_lane 0
+ 000262: 20 01                      | local.get 1
+ 000264: fd 1d 00                   | i64x2.extract_lane 0
+ 000267: 56                         | i64.gt_u
+ 000268: 1b                         | select
+ 000269: fd 12                      | i64x2.splat
+ 00026b: 42 7f                      | i64.const 18446744073709551615
+ 00026d: 42 00                      | i64.const 0
+ 00026f: 20 00                      | local.get 0
+ 000271: fd 1d 01                   | i64x2.extract_lane 1
+ 000274: 20 01                      | local.get 1
+ 000276: fd 1d 01                   | i64x2.extract_lane 1
+ 000279: 56                         | i64.gt_u
+ 00027a: 1b                         | select
+ 00027b: fd 1e 01                   | i64x2.replace_lane 1
+ 00027e: 0b                         | end
+000280 func[46] <ge_u64x2>:
+ 000281: 42 7f                      | i64.const 18446744073709551615
+ 000283: 42 00                      | i64.const 0
+ 000285: 20 00                      | local.get 0
+ 000287: fd 1d 00                   | i64x2.extract_lane 0
+ 00028a: 20 01                      | local.get 1
+ 00028c: fd 1d 00                   | i64x2.extract_lane 0
+ 00028f: 5a                         | i64.ge_u
+ 000290: 1b                         | select
+ 000291: fd 12                      | i64x2.splat
+ 000293: 42 7f                      | i64.const 18446744073709551615
+ 000295: 42 00                      | i64.const 0
+ 000297: 20 00                      | local.get 0
+ 000299: fd 1d 01                   | i64x2.extract_lane 1
+ 00029c: 20 01                      | local.get 1
+ 00029e: fd 1d 01                   | i64x2.extract_lane 1
+ 0002a1: 5a                         | i64.ge_u
+ 0002a2: 1b                         | select
+ 0002a3: fd 1e 01                   | i64x2.replace_lane 1
+ 0002a6: 0b                         | end
+0002a8 func[47] <ne_u64x2>:
+ 0002a9: 20 00                      | local.get 0
+ 0002ab: 20 01                      | local.get 1
+ 0002ad: fd d7 01                   | i64x2.ne
+ 0002b0: 0b                         | end
+0002b2 func[48] <eq_f32x4>:
+ 0002b3: 20 00                      | local.get 0
+ 0002b5: 20 01                      | local.get 1
+ 0002b7: fd 41                      | f32x4.eq
+ 0002b9: 0b                         | end
+0002bb func[49] <lt_f32x4>:
+ 0002bc: 20 00                      | local.get 0
+ 0002be: 20 01                      | local.get 1
+ 0002c0: fd 43                      | f32x4.lt
+ 0002c2: 0b                         | end
+0002c4 func[50] <le_f32x4>:
+ 0002c5: 20 00                      | local.get 0
+ 0002c7: 20 01                      | local.get 1
+ 0002c9: fd 45                      | f32x4.le
+ 0002cb: 0b                         | end
+0002cd func[51] <gt_f32x4>:
+ 0002ce: 20 00                      | local.get 0
+ 0002d0: 20 01                      | local.get 1
+ 0002d2: fd 44                      | f32x4.gt
+ 0002d4: 0b                         | end
+0002d6 func[52] <ge_f32x4>:
+ 0002d7: 20 00                      | local.get 0
+ 0002d9: 20 01                      | local.get 1
+ 0002db: fd 46                      | f32x4.ge
+ 0002dd: 0b                         | end
+0002df func[53] <ne_f32x4>:
+ 0002e0: 20 00                      | local.get 0
+ 0002e2: 20 01                      | local.get 1
+ 0002e4: fd 42                      | f32x4.ne
+ 0002e6: 0b                         | end
+0002e8 func[54] <eq_f64x2>:
+ 0002e9: 20 00                      | local.get 0
+ 0002eb: 20 01                      | local.get 1
+ 0002ed: fd 47                      | f64x2.eq
+ 0002ef: 0b                         | end
+0002f1 func[55] <lt_f64x2>:
+ 0002f2: 20 00                      | local.get 0
+ 0002f4: 20 01                      | local.get 1
+ 0002f6: fd 49                      | f64x2.lt
+ 0002f8: 0b                         | end
+0002fa func[56] <le_f64x2>:
+ 0002fb: 20 00                      | local.get 0
+ 0002fd: 20 01                      | local.get 1
+ 0002ff: fd 4b                      | f64x2.le
+ 000301: 0b                         | end
+000303 func[57] <gt_f64x2>:
+ 000304: 20 00                      | local.get 0
+ 000306: 20 01                      | local.get 1
+ 000308: fd 4a                      | f64x2.gt
+ 00030a: 0b                         | end
+00030c func[58] <ge_f64x2>:
+ 00030d: 20 00                      | local.get 0
+ 00030f: 20 01                      | local.get 1
+ 000311: fd 4c                      | f64x2.ge
+ 000313: 0b                         | end
+000315 func[59] <ne_f64x2>:
+ 000316: 20 00                      | local.get 0
+ 000318: 20 01                      | local.get 1
+ 00031a: fd 48                      | f64x2.ne
+ 00031c: 0b                         | end
