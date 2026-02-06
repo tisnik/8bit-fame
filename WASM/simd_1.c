6ed5be2d7ae389fdbf195093351470882fb91d71
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-06 09:28:54 +0100

    SIMD operations: vector size

---
+++ WASM/simd_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+typedef signed char i8x16 __attribute__((vector_size(16)));
+ 
+int main(void)
+{
+    printf("scalar signed byte:   %ld byte(s)\n", sizeof(signed char));
+    printf("vector signed bytes:  %ld byte(s)\n", sizeof(i8x16));
+    return 0;
+}
