a2a869c12e60b5c2191718dc3a3eb794924d37b4
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-06 09:28:54 +0100

    Compilation into WebAssembly

---
+++ WASM/simd_5_A.asm
@@ -0,0 +1,109 @@
+
+simd_3.wasm:	file format wasm 0x1
+
+Code Disassembly:
+
+000062 func[0] <add>:
+ 000063: 20 00                      | local.get 0
+ 000065: 20 19                      | local.get 25
+ 000067: 20 09                      | local.get 9
+ 000069: 6a                         | i32.add
+ 00006a: 3a 00 08                   | i32.store8 0 8
+ 00006d: 20 00                      | local.get 0
+ 00006f: 20 15                      | local.get 21
+ 000071: 20 05                      | local.get 5
+ 000073: 6a                         | i32.add
+ 000074: 3a 00 04                   | i32.store8 0 4
+ 000077: 20 00                      | local.get 0
+ 000079: 20 13                      | local.get 19
+ 00007b: 20 03                      | local.get 3
+ 00007d: 6a                         | i32.add
+ 00007e: 3a 00 02                   | i32.store8 0 2
+ 000081: 20 00                      | local.get 0
+ 000083: 20 12                      | local.get 18
+ 000085: 20 02                      | local.get 2
+ 000087: 6a                         | i32.add
+ 000088: 3a 00 01                   | i32.store8 0 1
+ 00008b: 20 00                      | local.get 0
+ 00008d: 20 11                      | local.get 17
+ 00008f: 20 01                      | local.get 1
+ 000091: 6a                         | i32.add
+ 000092: 3a 00 00                   | i32.store8 0 0
+ 000095: 20 00                      | local.get 0
+ 000097: 41 0f                      | i32.const 15
+ 000099: 6a                         | i32.add
+ 00009a: 20 20                      | local.get 32
+ 00009c: 20 10                      | local.get 16
+ 00009e: 6a                         | i32.add
+ 00009f: 3a 00 00                   | i32.store8 0 0
+ 0000a2: 20 00                      | local.get 0
+ 0000a4: 41 0e                      | i32.const 14
+ 0000a6: 6a                         | i32.add
+ 0000a7: 20 1f                      | local.get 31
+ 0000a9: 20 0f                      | local.get 15
+ 0000ab: 6a                         | i32.add
+ 0000ac: 3a 00 00                   | i32.store8 0 0
+ 0000af: 20 00                      | local.get 0
+ 0000b1: 41 0d                      | i32.const 13
+ 0000b3: 6a                         | i32.add
+ 0000b4: 20 1e                      | local.get 30
+ 0000b6: 20 0e                      | local.get 14
+ 0000b8: 6a                         | i32.add
+ 0000b9: 3a 00 00                   | i32.store8 0 0
+ 0000bc: 20 00                      | local.get 0
+ 0000be: 41 0c                      | i32.const 12
+ 0000c0: 6a                         | i32.add
+ 0000c1: 20 1d                      | local.get 29
+ 0000c3: 20 0d                      | local.get 13
+ 0000c5: 6a                         | i32.add
+ 0000c6: 3a 00 00                   | i32.store8 0 0
+ 0000c9: 20 00                      | local.get 0
+ 0000cb: 41 0b                      | i32.const 11
+ 0000cd: 6a                         | i32.add
+ 0000ce: 20 1c                      | local.get 28
+ 0000d0: 20 0c                      | local.get 12
+ 0000d2: 6a                         | i32.add
+ 0000d3: 3a 00 00                   | i32.store8 0 0
+ 0000d6: 20 00                      | local.get 0
+ 0000d8: 41 0a                      | i32.const 10
+ 0000da: 6a                         | i32.add
+ 0000db: 20 1b                      | local.get 27
+ 0000dd: 20 0b                      | local.get 11
+ 0000df: 6a                         | i32.add
+ 0000e0: 3a 00 00                   | i32.store8 0 0
+ 0000e3: 20 00                      | local.get 0
+ 0000e5: 41 09                      | i32.const 9
+ 0000e7: 6a                         | i32.add
+ 0000e8: 20 1a                      | local.get 26
+ 0000ea: 20 0a                      | local.get 10
+ 0000ec: 6a                         | i32.add
+ 0000ed: 3a 00 00                   | i32.store8 0 0
+ 0000f0: 20 00                      | local.get 0
+ 0000f2: 41 07                      | i32.const 7
+ 0000f4: 6a                         | i32.add
+ 0000f5: 20 18                      | local.get 24
+ 0000f7: 20 08                      | local.get 8
+ 0000f9: 6a                         | i32.add
+ 0000fa: 3a 00 00                   | i32.store8 0 0
+ 0000fd: 20 00                      | local.get 0
+ 0000ff: 41 06                      | i32.const 6
+ 000101: 6a                         | i32.add
+ 000102: 20 17                      | local.get 23
+ 000104: 20 07                      | local.get 7
+ 000106: 6a                         | i32.add
+ 000107: 3a 00 00                   | i32.store8 0 0
+ 00010a: 20 00                      | local.get 0
+ 00010c: 41 05                      | i32.const 5
+ 00010e: 6a                         | i32.add
+ 00010f: 20 16                      | local.get 22
+ 000111: 20 06                      | local.get 6
+ 000113: 6a                         | i32.add
+ 000114: 3a 00 00                   | i32.store8 0 0
+ 000117: 20 00                      | local.get 0
+ 000119: 41 03                      | i32.const 3
+ 00011b: 6a                         | i32.add
+ 00011c: 20 14                      | local.get 20
+ 00011e: 20 04                      | local.get 4
+ 000120: 6a                         | i32.add
+ 000121: 3a 00 00                   | i32.store8 0 0
+ 000124: 0b                         | end
