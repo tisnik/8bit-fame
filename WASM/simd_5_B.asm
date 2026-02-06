9de8c92821baaf147c646b0e4df4b356cfaa6326
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-06 09:28:54 +0100

    Compilation into WebAssembly

---
+++ WASM/simd_5_B.asm
@@ -0,0 +1,10 @@
+
+simd_3.wasm:	file format wasm 0x1
+
+Code Disassembly:
+
+000043 func[0] <add>:
+ 000044: 20 01                      | local.get 1
+ 000046: 20 00                      | local.get 0
+ 000048: fd 6e                      | i8x16.add
+ 00004a: 0b                         | end
