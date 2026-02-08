a0c44889f1c303e6a9a7ba7b5f83d2494dc0982f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-08 09:47:40 +0100

    Switch statement written in C

---
+++ WASM/switch_1.c
@@ -0,0 +1,22 @@
+void foo(void);
+void bar(void);
+void baz(void);
+void bzz(void);
+
+void numeric_value(int x) {
+    switch (x) {
+        case 0:
+            foo();
+            break;
+        case 1:
+            bar();
+            break;
+        case 2:
+            baz();
+            break;
+        case 3:
+            bzz();
+            break;
+    }
+}
+
