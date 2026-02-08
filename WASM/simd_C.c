d4ab443eaa413ed94dfd9405490f7f7418b0b6eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-08 09:47:40 +0100

    SIMD per-items multiplication written in C

---
+++ WASM/simd_C.c
@@ -0,0 +1,26 @@
+typedef signed   char           i8x16 __attribute__((vector_size(16)));
+typedef unsigned char           u8x16 __attribute__((vector_size(16)));
+typedef signed   short int      i16x8 __attribute__((vector_size(16)));
+typedef unsigned short int      u16x8 __attribute__((vector_size(16)));
+typedef signed   int            i32x4 __attribute__((vector_size(16)));
+typedef unsigned int            u32x4 __attribute__((vector_size(16)));
+typedef   signed long long int  i64x2 __attribute__((vector_size(16)));
+typedef unsigned long long int  u64x2 __attribute__((vector_size(16)));
+typedef float                   f32x4 __attribute__((vector_size(16)));
+typedef double                  f64x2 __attribute__((vector_size(16)));
+ 
+#define MUL(type) type mul_##type(type x, type y) {return (type)(x*y);}
+
+#define ALL(type) \
+    MUL(type)
+
+ALL(i8x16)
+ALL(u8x16)
+ALL(i16x8)
+ALL(u16x8)
+ALL(i32x4)
+ALL(u32x4)
+ALL(i64x2)
+ALL(u64x2)
+ALL(f32x4)
+ALL(f64x2)
