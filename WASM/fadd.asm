5681433172cfebb1b97ef8834923f2a82d94f3d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-08 09:47:39 +0100

    Floating point add compiled into WebAssembly

---
+++ WASM/fadd.asm
@@ -0,0 +1,9 @@
+fadd.wasm:	file format wasm 0x1
+
+Code Disassembly:
+
+000043 func[0] <fadd>:
+ 000044: 20 00                      | local.get 0
+ 000046: 20 01                      | local.get 1
+ 000048: 92                         | f32.add
+ 000049: 0b                         | end
