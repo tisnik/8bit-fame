925f03ed33552e1636e19e5b7ce1a9fe4ee081e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-09 09:14:03 +0100

    Function call chain written in C

---
+++ LLVM_IR/stack.c
@@ -0,0 +1,24 @@
+int return_constant() {
+    return 42;
+}
+
+int x(int);
+
+int call_x(int a) {
+    return x(a);
+}
+
+int first(int a, int b) {
+    return a;
+}
+
+int second(int a, int b) {
+    return b;
+}
+
+int try_return(int a) {
+    if (a<0) {
+        return 42;
+    }
+    return 9999;
+}
