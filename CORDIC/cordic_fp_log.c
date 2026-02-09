61726402ccdc0c60ffc87d00f5bb2744409f475b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-09 09:16:31 +0100

    Reformatted

--- CORDIC/cordic_fp_log.c
+++ CORDIC/cordic_fp_log.c
@@ -15,48 +15,47 @@
 
 // ln(1+2*(-i))
 double tabp[MAXITER] =
-{ 
-    0.40546510810816,
-    0.22314355131421,
-    0.11778303565638,
-    0.06062462181643,
-    0.03077165866675,
-    0.01550418653597,
-    0.00778214044205,
-    0.00389864041566,
-    0.00195122013126,
-    0.00097608597306,
+    {
+        0.40546510810816,
+        0.22314355131421,
+        0.11778303565638,
+        0.06062462181643,
+        0.03077165866675,
+        0.01550418653597,
+        0.00778214044205,
+        0.00389864041566,
+        0.00195122013126,
+        0.00097608597306,
 };
 
 // ln(1-2*(-i))
 double tabm[MAXITER] =
-{
-    -0.69314718055995,
-    -0.28768207245178,
-    -0.13353139262452,
-    -0.06453852113757,
-    -0.03174869831458,
-    -0.01574835696814,
-    -0.00784317746103,
-    -0.00391389932114,
-    -0.00195503483580,
-    -0.00097703964783,
+    {
+        -0.69314718055995,
+        -0.28768207245178,
+        -0.13353139262452,
+        -0.06453852113757,
+        -0.03174869831458,
+        -0.01574835696814,
+        -0.00784317746103,
+        -0.00391389932114,
+        -0.00195503483580,
+        -0.00097703964783,
 };
 
 // výpočet logaritmu algoritmem CORDIC
-double log_cordic(double a)
-{
+double log_cordic(double a) {
     const double three_eigth = 0.375;
-    int sk, expo;
-    double sum = tabm[0];
-    double x = 2.0 * frexpf (a, &expo);
-    double ex2 = 1.0; // dvojková mocnina
-    int k;
+    int          sk, expo;
+    double       sum = tabm[0];
+    double       x = 2.0 * frexpf(a, &expo);
+    double       ex2 = 1.0; // dvojková mocnina
+    int          k;
 
     for (k = 0; k < MAXITER; k++) {
         sk = 0;
         // zjistit směr rotace
-        if ((x - 1.0) <  (-three_eigth * ex2)) {
+        if ((x - 1.0) < (-three_eigth * ex2)) {
             sk = +1;
         }
         if ((x - 1.0) >= (+three_eigth * ex2)) {
@@ -65,21 +64,20 @@ double log_cordic(double a)
         ex2 /= 2.0;
         if (sk == 1) { // přímá rotace
             x = x + x * ex2;
-            sum = sum - tabp [k];
-        } 
+            sum = sum - tabp[k];
+        }
         if (sk == -1) { // opačná rotace
             x = x - x * ex2;
-            sum = sum - tabm [k];
+            sum = sum - tabm[k];
         }
     }
     return expo * K + sum; // přepočet logaritmu
 }
 
-
-int main (void) {
+int main(void) {
     double a = M_E - 2.0; // "pěkná" počáteční hodnota
 
-    for (; a<=2.0*M_E; a+=0.1) {
+    for (; a <= 2.0 * M_E; a += 0.1) {
         double log_value = log_cordic(a);
         double log_correct = log(a);
         double log_error = fabs(log_correct - log_value);
