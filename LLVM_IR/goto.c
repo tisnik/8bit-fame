a665487ca3de2c3507bd6802b7697839bec34b67
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-09 09:14:03 +0100

    Goto statement, written in C

---
+++ LLVM_IR/goto.c
@@ -0,0 +1,16 @@
+void positive_case(void);
+void negative_case(void);
+void finish(void);
+
+void bar(int i) {
+    if (i>0) {
+        goto positive;
+    }
+    positive_case();
+    goto end;
+positive:
+    negative_case();
+end:
+    finish();
+}
+
