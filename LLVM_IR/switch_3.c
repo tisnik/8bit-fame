7ef5c05abc995b99028a0392213d2ab52bcdb0fd
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-10 09:34:08 +0100

    Reformatted

--- LLVM_IR/switch_3.c
+++ LLVM_IR/switch_3.c
@@ -1,14 +1,13 @@
 int numeric_value(int x) {
-    switch (x) {
-        case 0:
-            return 10;
-        case 1:
-            return 6;
-        case 2:
-            return 3;
-        case 3:
-            return -1;
-    }
-    return -2;
+  switch (x) {
+  case 0:
+    return 10;
+  case 1:
+    return 6;
+  case 2:
+    return 3;
+  case 3:
+    return -1;
+  }
+  return -2;
 }
-
