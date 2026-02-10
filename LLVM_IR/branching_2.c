9b835adcc43e75720a6a5a13f7759bfa7580c63e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-10 09:34:08 +0100

    Reformatted

--- LLVM_IR/branching_2.c
+++ LLVM_IR/branching_2.c
@@ -1,9 +1,8 @@
 unsigned int gcd(unsigned int u, unsigned int v) {
-    while (v) {
-        int t = u;
-        u = v;
-        v = t % v;
-    }
-    return u;
+  while (v) {
+    int t = u;
+    u = v;
+    v = t % v;
+  }
+  return u;
 }
-
