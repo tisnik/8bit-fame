2c8b30e9483523e860bd70355ccf48a5900c11dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-10 09:34:08 +0100

    Reformatted

--- LLVM_IR/switch_2.c
+++ LLVM_IR/switch_2.c
@@ -1,18 +1,17 @@
 unsigned int numeric_value(unsigned int x, unsigned int y) {
-    switch (x) {
-        case 42:
-            y++;
-            break;
-        case 100:
-            y+=2;
-            break;
-        case 6502:
-            y-=2;
-            break;
-        case 8080:
-            y--;
-            break;
-    }
-    return y;
+  switch (x) {
+  case 42:
+    y++;
+    break;
+  case 100:
+    y += 2;
+    break;
+  case 6502:
+    y -= 2;
+    break;
+  case 8080:
+    y--;
+    break;
+  }
+  return y;
 }
-
