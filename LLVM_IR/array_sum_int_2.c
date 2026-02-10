55fe2dd15e973152922358ca2cdb4fdb28c189e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-10 09:34:08 +0100

    Reformatted

--- LLVM_IR/array_sum_int_2.c
+++ LLVM_IR/array_sum_int_2.c
@@ -1,10 +1,10 @@
 unsigned int sum(unsigned int *array, int size) {
-    unsigned int s = 0;
-    unsigned int *p = array;
-    int i;
+  unsigned int s = 0;
+  unsigned int *p = array;
+  int i;
 
-    for (i=0; i<size; i++) {
-        s += *p++;
-    }
-    return s;
+  for (i = 0; i < size; i++) {
+    s += *p++;
+  }
+  return s;
 }
