492723db6f6862ec36956d3f615894cc7f450fb0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-11 09:12:27 +0100

    Reformatted

--- LLVM_IR/arith_operators_float.c
+++ LLVM_IR/arith_operators_float.c
@@ -1,20 +1,20 @@
-#define NEG(type)                                                              \
-  type neg_##type(type x) { return -x; }
-#define ADD(type)                                                              \
-  type add_##type(type x, type y) { return x + y; }
-#define SUB(type)                                                              \
-  type sub_##type(type x, type y) { return x - y; }
-#define MUL(type)                                                              \
-  type mul_##type(type x, type y) { return x * y; }
-#define DIV(type)                                                              \
-  type div_##type(type x, type y) { return x / y; }
+#define NEG(type) \
+    type neg_##type(type x) { return -x; }
+#define ADD(type) \
+    type add_##type(type x, type y) { return x + y; }
+#define SUB(type) \
+    type sub_##type(type x, type y) { return x - y; }
+#define MUL(type) \
+    type mul_##type(type x, type y) { return x * y; }
+#define DIV(type) \
+    type div_##type(type x, type y) { return x / y; }
 
-#define ALL(type)                                                              \
-  NEG(type)                                                                    \
-  ADD(type)                                                                    \
-  SUB(type)                                                                    \
-  MUL(type)                                                                    \
-  DIV(type)
+#define ALL(type) \
+    NEG(type)     \
+    ADD(type)     \
+    SUB(type)     \
+    MUL(type)     \
+    DIV(type)
 
 ALL(float)
 ALL(double)
