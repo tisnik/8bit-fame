7c2fd8141671a87dca0b2a5ba553e3d7974fb8b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-12 08:55:15 +0100

    Reformatted

--- gcc-simd/add_arrays_size_17.c
+++ gcc-simd/add_arrays_size_17.c
@@ -1,7 +1,7 @@
 void add_delta(float *a, float *b) {
-    #define SIZE 17
+#define SIZE 17
     int i;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         a[i] += b[i];
     }
 }
