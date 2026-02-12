6350cfd272480be4c1e2178fe5526177aae4860d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-12 08:55:15 +0100

    Reformatted

--- gcc-simd/add_delta_4_doubles.c
+++ gcc-simd/add_delta_4_doubles.c
@@ -1,7 +1,7 @@
 void add_delta(double *a, double delta) {
-    #define SIZE 4
+#define SIZE 4
     int i;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         a[i] += delta;
     }
 }
