11e3252906d5dba7f6ed39b9aa70f250642912a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-12 08:55:15 +0100

    Reformatted

--- gcc-simd/add_arrays_size_16_backward.c
+++ gcc-simd/add_arrays_size_16_backward.c
@@ -1,7 +1,7 @@
 void add_arrays(float *a, float *b) {
-    #define SIZE 16
+#define SIZE 16
     int i;
-    for (i=SIZE-1; i>=0; i--) {
+    for (i = SIZE - 1; i >= 0; i--) {
         a[i] += b[i];
     }
 }
