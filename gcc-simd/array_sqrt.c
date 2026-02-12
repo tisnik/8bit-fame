bc822df7aa90eacedba8abf61565c5ee60e7650d
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-12 08:55:16 +0100

    Reformatted

--- gcc-simd/array_sqrt.c
+++ gcc-simd/array_sqrt.c
@@ -1,9 +1,9 @@
 #include <math.h>
 
 void array_sqrt(float *a) {
-    #define SIZE 24
+#define SIZE 24
     int i;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         a[i] = sqrt(a[i]);
     }
 }
