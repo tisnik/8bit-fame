f8d9b5d58469a9f4cf5ec9f26b7ccb22f5156087
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-12 08:55:15 +0100

    Reformatted

--- gcc-simd/add_arrays_32_bytes.c
+++ gcc-simd/add_arrays_32_bytes.c
@@ -1,7 +1,7 @@
 void add_arrays(unsigned char *a, unsigned char *b) {
-    #define SIZE 32
+#define SIZE 32
     int i;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         a[i] += b[i];
     }
 }
