27815dbe948de073b6c8dee41b1a30e1d2ffc259
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-12 08:55:16 +0100

    Reformatted

--- gcc-simd/add_delta_64_bytes.c
+++ gcc-simd/add_delta_64_bytes.c
@@ -1,7 +1,7 @@
 void add_delta(unsigned char *a, unsigned char delta) {
-    #define SIZE 64
+#define SIZE 64
     int i;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         a[i] += delta;
     }
 }
