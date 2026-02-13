56e797ddc8795108dff81b7d77ad353c5ec41d2b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-13 09:53:53 +0100

    Reformatted

--- WASM/simd_4.c
+++ WASM/simd_4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 typedef signed char i8x16 __attribute__((vector_size(16)));
- 
+
 i8x16 add(i8x16 x, i8x16 y) {
-    return x+y;
+    return x + y;
 }
 
 void print_vector(i8x16 *v) {
     int i;
 
     printf("[");
-    for (i=0; i<16; i++) {
+    for (i = 0; i < 16; i++) {
         printf(" %2d", (*v)[i]);
     }
     printf("]\n");
@@ -19,7 +19,7 @@ void print_vector(i8x16 *v) {
 int main(void) {
     i8x16 v1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     i8x16 v2 = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
-    i8x16 v3 = v1+v2;
+    i8x16 v3 = v1 + v2;
 
     print_vector(&v1);
     print_vector(&v2);
