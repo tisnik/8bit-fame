a31a8f5c886fd36efeb46328e916ed7f3ec82b2c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-13 09:53:53 +0100

    Reformatted

--- WASM/loop_3.c
+++ WASM/loop_3.c
@@ -1,9 +1,8 @@
 int loop(int start) {
     int i;
-    int s=start;
-    for (i=0; i<10; i++) {
-        s*=42;
+    int s = start;
+    for (i = 0; i < 10; i++) {
+        s *= 42;
     }
     return s;
 }
-
