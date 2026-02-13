110d53b7477745efa22ebd8a57107887f03ebf01
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-13 09:53:54 +0100

    Reformatted

--- WASM/simd_8.c
+++ WASM/simd_8.c
@@ -1,15 +1,15 @@
 typedef float  f32x4 __attribute__((vector_size(16)));
 typedef double f64x2 __attribute__((vector_size(16)));
 typedef __fp16 f16x8 __attribute__((vector_size(16)));
- 
+
 f16x8 add_f16x8(f16x8 x, f16x8 y) {
-    return x+y;
+    return x + y;
 }
- 
+
 f32x4 add_f32x4(f32x4 x, f32x4 y) {
-    return x+y;
+    return x + y;
 }
- 
+
 f64x2 add_f64x2(f64x2 x, f64x2 y) {
-    return x+y;
+    return x + y;
 }
