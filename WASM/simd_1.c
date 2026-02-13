7a05838786ac2a06209bab6a920d4658b46ca084
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-13 09:53:53 +0100

    Reformatted

--- WASM/simd_1.c
+++ WASM/simd_1.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
- 
+
 typedef signed char i8x16 __attribute__((vector_size(16)));
- 
-int main(void)
-{
+
+int main(void) {
     printf("scalar signed byte:   %ld byte(s)\n", sizeof(signed char));
     printf("vector signed bytes:  %ld byte(s)\n", sizeof(i8x16));
     return 0;
