3a1468d6e3b5a8b7748953b5849cb058c0d77ace
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-13 09:53:53 +0100

    Reformatted

--- WASM/simd_2.c
+++ WASM/simd_2.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
- 
-typedef signed   char      i8x16 __attribute__((vector_size(16)));
+
+typedef signed char        i8x16 __attribute__((vector_size(16)));
 typedef unsigned char      u8x16 __attribute__((vector_size(16)));
-typedef signed   short int i16x8 __attribute__((vector_size(16)));
+typedef signed short int   i16x8 __attribute__((vector_size(16)));
 typedef unsigned short int u16x8 __attribute__((vector_size(16)));
-typedef signed   int       i32x4 __attribute__((vector_size(16)));
+typedef signed int         i32x4 __attribute__((vector_size(16)));
 typedef unsigned int       u32x4 __attribute__((vector_size(16)));
-typedef   signed long int  i64x2 __attribute__((vector_size(16)));
+typedef signed long int    i64x2 __attribute__((vector_size(16)));
 typedef unsigned long int  u64x2 __attribute__((vector_size(16)));
- 
-int main(void)
-{
+
+int main(void) {
     printf("signed char:    %ld bytes\n", sizeof(signed char));
     printf("unsigned char:  %ld bytes\n", sizeof(unsigned char));
     printf("signed short:   %ld bytes\n", sizeof(signed short int));
@@ -19,7 +18,7 @@ int main(void)
     printf("unsigned int:   %ld bytes\n", sizeof(unsigned int));
     printf("signed long:    %ld bytes\n", sizeof(signed long int));
     printf("unsigned long:  %ld bytes\n", sizeof(unsigned long int));
- 
+
     printf("\n");
 
     printf("vector signed char:    %ld bytes\n", sizeof(i8x16));
@@ -30,6 +29,6 @@ int main(void)
     printf("vector unsigned int:   %ld bytes\n", sizeof(u32x4));
     printf("vector signed long:    %ld bytes\n", sizeof(i64x2));
     printf("vector unsigned long:  %ld bytes\n", sizeof(u64x2));
- 
+
     return 0;
 }
