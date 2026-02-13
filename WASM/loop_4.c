136840b65c223a4244389ab6be7de4f9fe131c5a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-13 09:53:53 +0100

    Reformatted

--- WASM/loop_4.c
+++ WASM/loop_4.c
@@ -2,10 +2,9 @@ void print(int);
 
 void nested_loops(void) {
     int x, y;
-    for (y=1; y<=10; y++) {
-        for (x=1; x<=10; x++) {
-            print(x*y);
+    for (y = 1; y <= 10; y++) {
+        for (x = 1; x <= 10; x++) {
+            print(x * y);
         }
     }
 }
-
