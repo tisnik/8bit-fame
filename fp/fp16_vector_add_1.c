806c4e3e1472fbf7db2c04f70ee069075db0f81e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_vector_add_1.c
+++ fp/fp16_vector_add_1.c
@@ -1,6 +1,5 @@
 typedef _Float16 float16x8 __attribute__((vector_size(16)));
 
-float16x8 add(float16x8 x, float16x8 y)
-{
-    return x+y;
+float16x8 add(float16x8 x, float16x8 y) {
+    return x + y;
 }
