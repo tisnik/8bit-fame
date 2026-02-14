ae67c0f3a4dcd2470c70094b975264e096b9114e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_arith.c
+++ fp/fp16_arith.c
@@ -1,15 +1,15 @@
 _Float16 fp16_add(_Float16 x, _Float16 y) {
-    return x+y;
+    return x + y;
 }
 
 _Float16 fp16_sub(_Float16 x, _Float16 y) {
-    return x-y;
+    return x - y;
 }
 
 _Float16 fp16_mul(_Float16 x, _Float16 y) {
-    return x*y;
+    return x * y;
 }
 
 _Float16 fp16_div(_Float16 x, _Float16 y) {
-    return x/y;
+    return x / y;
 }
