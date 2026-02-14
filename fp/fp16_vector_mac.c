85674df99c080b72664814e2dd23f90303605a89
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_vector_mac.c
+++ fp/fp16_vector_mac.c
@@ -1,6 +1,5 @@
 typedef _Float16 float16x8 __attribute__((vector_size(16)));
 
-float16x8 mac(float16x8 a, float16x8 b, float16x8 c)
-{
-    return a+b*c;
+float16x8 mac(float16x8 a, float16x8 b, float16x8 c) {
+    return a + b * c;
 }
