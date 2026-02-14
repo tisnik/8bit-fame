589f46d497cd322c56cd2a7ef5a74904ad9adb56
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_vector_convert_1.c
+++ fp/fp16_vector_convert_1.c
@@ -1,7 +1,6 @@
 typedef _Float16 float16x8 __attribute__((vector_size(16)));
-typedef float float32x8 __attribute__((vector_size(32)));
+typedef float    float32x8 __attribute__((vector_size(32)));
 
-float32x8 to_float32x8(float16x8 x)
-{
+float32x8 to_float32x8(float16x8 x) {
     return __builtin_convertvector(x, float32x8);
 }
