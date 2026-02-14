b0b4dedb0723e89ae1c441b758cc16824170ce7c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_vector_incorrect_size_1.c
+++ fp/fp16_vector_incorrect_size_1.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
- 
+
 typedef _Float16 float16x8 __attribute__((vector_size(17)));
 
-int main(void)
-{
+int main(void) {
     printf("scalar: %ld bytes\n", sizeof(_Float16));
     printf("vector: %ld bytes\n", sizeof(float16x8));
- 
+
     return 0;
 }
