494ce9e5b94160ed31b4e06a49c56023d5d09e00
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_vector_sum.c
+++ fp/fp16_vector_sum.c
@@ -1,19 +1,14 @@
 typedef _Float16 float16x8 __attribute__((vector_size(16)));
 
-
-_Float16 sum1(float16x8 x)
-{
+_Float16 sum1(float16x8 x) {
     _Float16 sum = 0.0;
-    int i;
-    for (i=0; i<8; i++) {
+    int      i;
+    for (i = 0; i < 8; i++) {
         sum += x[i];
     }
     return sum;
 }
 
-_Float16 sum2(float16x8 x)
-{
+_Float16 sum2(float16x8 x) {
     return x[0] + x[1] + x[2] + x[3] + x[4] + x[5] + x[6] + x[7];
 }
-
-
