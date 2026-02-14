15654d8a5c8ee9499b67a3f5da6adbf44e3181ba
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_vector_delta.c
+++ fp/fp16_vector_delta.c
@@ -1,6 +1,5 @@
 typedef _Float16 float16x8 __attribute__((vector_size(16)));
 
-float16x8 add_delta(float16x8 x, _Float16 delta)
-{
-    return x+delta;
+float16x8 add_delta(float16x8 x, _Float16 delta) {
+    return x + delta;
 }
