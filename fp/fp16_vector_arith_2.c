e7b8eeb078b68f663f04db6e69f048b152835ded
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-14 09:32:33 +0100

    Reformatted

--- fp/fp16_vector_arith_2.c
+++ fp/fp16_vector_arith_2.c
@@ -1,22 +1,17 @@
 typedef _Float16 float16x64 __attribute__((vector_size(128)));
 
-
-float16x64 add(float16x64 x, float16x64 y)
-{
-    return x+y;
+float16x64 add(float16x64 x, float16x64 y) {
+    return x + y;
 }
 
-float16x64 sub(float16x64 x, float16x64 y)
-{
-    return x-y;
+float16x64 sub(float16x64 x, float16x64 y) {
+    return x - y;
 }
 
-float16x64 mul(float16x64 x, float16x64 y)
-{
-    return x*y;
+float16x64 mul(float16x64 x, float16x64 y) {
+    return x * y;
 }
 
-float16x64 div(float16x64 x, float16x64 y)
-{
-    return x/y;
+float16x64 div(float16x64 x, float16x64 y) {
+    return x / y;
 }
