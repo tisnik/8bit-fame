467d323167e3d39e0e348af28a79af2ce368d822
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-15 09:47:36 +0100

    Reformatted

--- gcc-builtins/rand_gen.c
+++ gcc-builtins/rand_gen.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include <stdint.h>
-
+#include <stdio.h>
 
 uint32_t xorshift32(void) {
-    const uint32_t start_state = 0xACE1u;
+    const uint32_t  start_state = 0xACE1u;
     static uint32_t state = start_state;
 
     uint32_t x = state;
     x ^= x << 13;
     x ^= x >> 17;
-    x ^= x <<  5;
+    x ^= x << 5;
     return state = x;
 }
 
@@ -17,7 +16,7 @@ int main(void) {
     FILE *fout;
 
     uint32_t random_value;
-    int i;
+    int      i;
 
     fout = fopen("random.bin", "wb");
     if (fout == NULL) {
@@ -25,7 +24,7 @@ int main(void) {
         return 1;
     }
 
-    for (i=0; i<1000000; i++) {
+    for (i = 0; i < 1000000; i++) {
         random_value = xorshift32();
         fwrite(&random_value, sizeof(uint32_t), 1, fout);
         if (ferror(fout)) {
@@ -38,4 +37,3 @@ int main(void) {
 
     return 0;
 }
-
