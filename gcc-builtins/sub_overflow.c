5baf0edc4896c60ba8227acfeee669e42a46a142
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-15 09:47:36 +0100

    Reformatted

--- gcc-builtins/sub_overflow.c
+++ gcc-builtins/sub_overflow.c
@@ -2,48 +2,48 @@
 
 bool sub_overflow_signed_char(signed char x, signed char y) {
     signed char z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool        overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
 
 bool sub_overflow_unsigned_char(unsigned char x, unsigned char y) {
     unsigned char z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool          overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
 
 bool sub_overflow_signed_short(signed short x, signed short y) {
     signed short z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool         overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
 
 bool sub_overflow_unsigned_short(unsigned short x, unsigned short y) {
     unsigned short z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool           overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
 
 bool sub_overflow_signed_int(signed int x, signed int y) {
     signed int z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool       overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
 
 bool sub_overflow_unsigned_int(unsigned int x, unsigned int y) {
     unsigned int z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool         overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
 
 bool sub_overflow_signed_long(signed long x, signed long y) {
     signed long z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool        overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
 
 bool sub_overflow_unsigned_long(unsigned long x, unsigned long y) {
     unsigned long z;
-    bool overflow = __builtin_sub_overflow(x, y, &z);
+    bool          overflow = __builtin_sub_overflow(x, y, &z);
     return overflow;
 }
