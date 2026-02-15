6c45a41a66e6067a5ddffab116e5d702a5bfcd98
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-15 09:47:36 +0100

    Reformatted

--- gcc-builtins/rdrand_read.c
+++ gcc-builtins/rdrand_read.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 int main(void) {
-    int success;
+    int      success;
     uint32_t random_value;
-    int i;
+    int      i;
 
-    for (i=0; i<10; i++) {
+    for (i = 0; i < 10; i++) {
         success = __builtin_ia32_rdrand32_step(&random_value);
         printf("%d %x\n", success, random_value);
     }
     return 0;
 }
-
