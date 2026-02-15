d1535f64659681a3ba605b291cdcf018e9982922
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-15 09:50:09 +0100

    Add and vector sizes

---
+++ LLVM_IR/simd_add_vector_sizes.c
@@ -0,0 +1,30 @@
+typedef signed char i8x4 __attribute__((vector_size(4)));
+typedef signed char i8x8 __attribute__((vector_size(8)));
+typedef signed char i8x16 __attribute__((vector_size(16)));
+typedef signed char i8x32 __attribute__((vector_size(32)));
+typedef signed char i8x64 __attribute__((vector_size(64)));
+typedef signed char i8x128 __attribute__((vector_size(128)));
+
+i8x4 add_i8x4(i8x4 x, i8x4 y) {
+    return x + y;
+}
+
+i8x8 add_i8x8(i8x8 x, i8x8 y) {
+    return x + y;
+}
+
+i8x16 add_i8x16(i8x16 x, i8x16 y) {
+    return x + y;
+}
+
+i8x32 add_i8x32(i8x32 x, i8x32 y) {
+    return x + y;
+}
+
+i8x64 add_i8x64(i8x64 x, i8x64 y) {
+    return x + y;
+}
+
+i8x128 add_i8x128(i8x128 x, i8x128 y) {
+    return x + y;
+}
