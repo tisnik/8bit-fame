699140a6ad5e1204d2a841478525ad2c43cec773
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-16 09:42:21 +0100

    Reformatted

--- gcc-simd/find_max_100.c
+++ gcc-simd/find_max_100.c
@@ -1,10 +1,10 @@
 #include <float.h>
 
 float find_max_100(float *a) {
-    #define SIZE 100
-    int i;
+#define SIZE 100
+    int   i;
     float max = FLT_MIN_EXP;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         if (a[i] > max) {
             max = a[i];
         }
