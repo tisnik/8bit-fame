10165c5f9cdb22420fd637a66ae8dfff7339e7c3
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-16 09:42:06 +0100

    Reformatted

--- WASM/simd_G.c
+++ WASM/simd_G.c
@@ -1,28 +1,34 @@
-typedef signed   char           i8x16 __attribute__((vector_size(16)));
-typedef unsigned char           u8x16 __attribute__((vector_size(16)));
-typedef signed   short int      i16x8 __attribute__((vector_size(16)));
-typedef unsigned short int      u16x8 __attribute__((vector_size(16)));
-typedef signed   int            i32x4 __attribute__((vector_size(16)));
-typedef unsigned int            u32x4 __attribute__((vector_size(16)));
-typedef   signed long long int  i64x2 __attribute__((vector_size(16)));
-typedef unsigned long long int  u64x2 __attribute__((vector_size(16)));
-typedef float                   f32x4 __attribute__((vector_size(16)));
-typedef double                  f64x2 __attribute__((vector_size(16)));
- 
-#define EQ(type) type eq_##type(type x, type y) {return x==y;}
-#define LT(type) type lt_##type(type x, type y) {return x<y;}
-#define LE(type) type le_##type(type x, type y) {return x<=y;}
-#define GT(type) type gt_##type(type x, type y) {return x>y;}
-#define GE(type) type ge_##type(type x, type y) {return x>=y;}
-#define NE(type) type ne_##type(type x, type y) {return x!=y;}
+typedef signed char i8x16 __attribute__((vector_size(16)));
+typedef unsigned char u8x16 __attribute__((vector_size(16)));
+typedef signed short int i16x8 __attribute__((vector_size(16)));
+typedef unsigned short int u16x8 __attribute__((vector_size(16)));
+typedef signed int i32x4 __attribute__((vector_size(16)));
+typedef unsigned int u32x4 __attribute__((vector_size(16)));
+typedef signed long long int i64x2 __attribute__((vector_size(16)));
+typedef unsigned long long int u64x2 __attribute__((vector_size(16)));
+typedef float f32x4 __attribute__((vector_size(16)));
+typedef double f64x2 __attribute__((vector_size(16)));
 
-#define ALL(type) \
-    EQ(type) \
-    LT(type) \
-    LE(type) \
-    GT(type) \
-    GE(type) \
-    NE(type)
+#define EQ(type)                                                               \
+  type eq_##type(type x, type y) { return x == y; }
+#define LT(type)                                                               \
+  type lt_##type(type x, type y) { return x < y; }
+#define LE(type)                                                               \
+  type le_##type(type x, type y) { return x <= y; }
+#define GT(type)                                                               \
+  type gt_##type(type x, type y) { return x > y; }
+#define GE(type)                                                               \
+  type ge_##type(type x, type y) { return x >= y; }
+#define NE(type)                                                               \
+  type ne_##type(type x, type y) { return x != y; }
+
+#define ALL(type)                                                              \
+  EQ(type)                                                                     \
+  LT(type)                                                                     \
+  LE(type)                                                                     \
+  GT(type)                                                                     \
+  GE(type)                                                                     \
+  NE(type)
 
 ALL(i8x16)
 ALL(u8x16)
