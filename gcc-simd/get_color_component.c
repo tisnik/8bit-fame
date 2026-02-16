31663671618792750f31da029967f5a765eb9ab4
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-16 09:42:21 +0100

    Reformatted

--- gcc-simd/get_color_component.c
+++ gcc-simd/get_color_component.c
@@ -1,24 +1,23 @@
 void get_red_component(float *restrict red, float *restrict rgb) {
-    #define SIZE 16
+#define SIZE 16
     int i;
-    for (i=0; i<SIZE/3; i++) {
-        red[i] = rgb[3*i];
+    for (i = 0; i < SIZE / 3; i++) {
+        red[i] = rgb[3 * i];
     }
 }
 
 void get_green_component(float *restrict green, float *restrict rgb) {
-    #define SIZE 16
+#define SIZE 16
     int i;
-    for (i=0; i<SIZE/3; i++) {
-        green[i] = rgb[3*i+1];
+    for (i = 0; i < SIZE / 3; i++) {
+        green[i] = rgb[3 * i + 1];
     }
 }
 
 void get_blue_component(float *restrict blue, float *restrict rgb) {
-    #define SIZE 16
+#define SIZE 16
     int i;
-    for (i=0; i<SIZE/3; i++) {
-        blue[i] = rgb[3*i+2];
+    for (i = 0; i < SIZE / 3; i++) {
+        blue[i] = rgb[3 * i + 2];
     }
 }
-
