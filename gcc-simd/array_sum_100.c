d995839ca17fdf07df5a960c1c10e3b963b1c2b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-16 09:42:21 +0100

    Reformatted

--- gcc-simd/array_sum_100.c
+++ gcc-simd/array_sum_100.c
@@ -1,8 +1,8 @@
 float array_sum(float *a) {
-    #define SIZE 100
-    int i;
+#define SIZE 100
+    int   i;
     float result = 0.0;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         result += a[i];
     }
     return result;
