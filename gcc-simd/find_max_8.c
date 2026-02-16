9fc1c2a90341cafa3724b5f815f5d2d335396d2e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-16 09:42:21 +0100

    Reformatted

--- gcc-simd/find_max_8.c
+++ gcc-simd/find_max_8.c
@@ -1,10 +1,10 @@
 #include <float.h>
 
 float find_max_8(float *a) {
-    #define SIZE 8
-    int i;
+#define SIZE 8
+    int   i;
     float max = FLT_MIN_EXP;
-    for (i=0; i<SIZE; i++) {
+    for (i = 0; i < SIZE; i++) {
         if (a[i] > max) {
             max = a[i];
         }
