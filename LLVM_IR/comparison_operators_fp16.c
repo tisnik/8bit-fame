d0e6c2af935f54190d8ec7df20a80b76860450c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-17 08:42:28 +0100

    Comparisons for half-float values

---
+++ LLVM_IR/comparison_operators_fp16.c
@@ -0,0 +1,22 @@
+#define EQ(type) \
+    int eq_##type(type x, type y) { return x == y; }
+#define NE(type) \
+    int ne_##type(type x, type y) { return x != y; }
+#define LT(type) \
+    int lt_##type(type x, type y) { return x < y; }
+#define LE(type) \
+    int le_##type(type x, type y) { return x <= y; }
+#define GT(type) \
+    int gt_##type(type x, type y) { return x > y; }
+#define GE(type) \
+    int ge_##type(type x, type y) { return x >= y; }
+
+#define ALL(type) \
+    EQ(type)      \
+    NE(type)      \
+    LT(type)      \
+    LE(type)      \
+    GT(type)      \
+    GE(type)
+
+ALL(_Float16)
