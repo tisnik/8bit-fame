b34ebe7cb6f642d636c79a8daedc2b4f2ee0f60f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-17 08:47:06 +0100

    Reformatted

--- compilers/gcc/memset.c
+++ compilers/gcc/memset.c
@@ -1,8 +1,9 @@
 typedef unsigned size_t;
 
-void * memset(void *dest, register int val, register size_t len) {
-    register unsigned char *ptr = (unsigned char*)dest;
-    while (len-- > 0)
+void *memset(void *dest, register int val, register size_t len) {
+    register unsigned char *ptr = (unsigned char *)dest;
+    while (len-- > 0) {
         *ptr++ = val;
+    }
     return dest;
 }
