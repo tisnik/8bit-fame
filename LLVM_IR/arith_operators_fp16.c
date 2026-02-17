3f88b4dcab94c23ce0ea36937999d8cdfae44717
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-02-17 08:42:28 +0100

    Arithmetic operations for half-float values

---
+++ LLVM_IR/arith_operators_fp16.c
@@ -0,0 +1,19 @@
+#define NEG(type) \
+    type neg_##type(type x) { return -x; }
+#define ADD(type) \
+    type add_##type(type x, type y) { return x + y; }
+#define SUB(type) \
+    type sub_##type(type x, type y) { return x - y; }
+#define MUL(type) \
+    type mul_##type(type x, type y) { return x * y; }
+#define DIV(type) \
+    type div_##type(type x, type y) { return x / y; }
+
+#define ALL(type) \
+    NEG(type)     \
+    ADD(type)     \
+    SUB(type)     \
+    MUL(type)     \
+    DIV(type)
+
+ALL(_Float16)
