6e2da8778a18e78aa29779e44e62ab9273d6f8ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-02 09:37:43 +0100

    Changing background color: 2nd variant

---
+++ Atari800-ca65/background_color_2.asm
@@ -0,0 +1,24 @@
+.include "atari.inc"
+
+.CODE
+
+.proc main
+        lda #0                  ; kod barvy
+        sta COLOR2              ; ulozit do registru COLOR2
+loop:   jmp loop
+end:
+.endproc
+                
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   main::end - 1           ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
