090c13038efd066fac7f24dca32afda2c94b5ad9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-03 08:29:03 +0100

    Dot product, written in C

---
+++ LLVM_IR/simd_dot_product_2.c
@@ -0,0 +1,30 @@
+typedef float  f32x4 __attribute__((vector_size(16)));
+typedef double f64x2 __attribute__((vector_size(16)));
+typedef __fp16 f16x8 __attribute__((vector_size(16)));
+
+float dot_fp16(f16x8 x , f16x8 y) {
+    int      i;
+    __fp16 result = 0.0;
+    for (i = 0; i < sizeof(f16x8) / sizeof(__fp16); i++) {
+        result += x[i] * y[i];
+    }
+    return result;
+}
+
+float dot_float(f32x4 x, f32x4 y) {
+    int      i;
+    float result = 0.0;
+    for (i = 0; i < sizeof(f32x4) / sizeof(float); i++) {
+        result += x[i] * y[i];
+    }
+    return result;
+}
+
+double dot_double(f64x2 x, f64x2 y) {
+    int      i;
+    double result = 0.0;
+    for (i = 0; i < sizeof(f64x2) / sizeof(double); i++) {
+        result += x[i] * y[i];
+    }
+    return result;
+}
