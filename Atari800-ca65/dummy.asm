b8318bcb9f6fe8097b85d1652e9cc1288cbdfb97
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-04 08:37:44 +0100

    First example: endless loop

---
+++ Atari800-ca65/dummy.asm
@@ -0,0 +1,20 @@
+.CODE
+
+.proc main
+loop:   jmp loop
+end:
+.endproc
+                
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   main::end - 1           ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   $02E0                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   $02E1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
