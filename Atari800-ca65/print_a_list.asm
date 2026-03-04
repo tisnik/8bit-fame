6863eb12927fa5790086007d9f160daab6bf2dd1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-04 08:37:44 +0100

    Recompiled

--- Atari800-ca65/print_a_list.asm
+++ Atari800-ca65/print_a_list.asm
@@ -1,4 +1,4 @@
-ca65 V2.18 - Ubuntu 2.18-1
+ca65 V2.18 - Fedora 2.19-12.fc42
 Main file   : print_a.asm
 Current file: print_a.asm
 
@@ -1447,13 +1447,15 @@ Current file: print_a.asm
 000009r 1               
 000009r 1               
 000009r 1               .segment "EXEHDR"
-000000r 1  FF FF        .word   $ffff
-000002r 1  rr rr        .word   main
-000004r 1  rr rr        .word   main::end - 1
+000000r 1  FF FF        .word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+000002r 1  rr rr        .word   main                    ; zacatek kodoveho segmentu
+000004r 1  rr rr        .word   main::end - 1           ; konec kodoveho segmentu
 000006r 1               
 000006r 1               
-000006r 1               .segment "AUTOSTRT"
-000000r 1  E0 02        .word   RUNAD                   ; definováno v atari.h
+000006r 1               .segment "AUTOSTRT"             ; segment s pocatecni adresou
+000000r 1  E0 02        .word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
 000002r 1  E1 02        .word   RUNAD+1
-000004r 1  rr rr        .word   main
-000004r 1               
+000004r 1  rr rr        .word   main                    ; adresa vstupniho bodu do programu
+000006r 1               
+000006r 1               ; finito
+000006r 1               
