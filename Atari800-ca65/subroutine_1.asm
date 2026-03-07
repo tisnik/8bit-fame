228f295398eb6f250fde0a569e43a5f53665824b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-07 09:45:54 +0100

    Subroutine call w/o arguments, assembly source

---
+++ Atari800-ca65/subroutine_1.asm
@@ -0,0 +1,30 @@
+.include "atari.inc"
+
+.CODE
+
+.proc main
+        jsr print_char
+loop:   jmp loop
+.endproc
+
+.proc print_char
+        lda #33                 ; kod znaku, ktery se bude tisknout
+        ldy #0                  ; vynulovat registr Y
+        sta (88), y             ; tisk znaku na první místo na obrazovce
+        rts
+.endproc
+
+end:                            ; potrebujeme znat adresu konce kodoveho segmentu
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                 ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
