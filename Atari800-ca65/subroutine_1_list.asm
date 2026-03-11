bc3a1677d3322895940ddbb081b74cea13857c1e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-11 08:20:24 +0100

    Listing generated by assembler

--- Atari800-ca65/subroutine_1_list.asm
+++ Atari800-ca65/subroutine_1_list.asm
@@ -1436,10 +1436,12 @@ Current file: subroutine_1.asm
 000000r 1               
 000000r 1               .CODE
 000000r 1               
+000000r 1               
 000000r 1               .proc main
 000000r 1  20 rr rr             jsr print_char
 000003r 1  4C rr rr     loop:   jmp loop
 000006r 1               .endproc
+000006r 1               
 000006r 1               
 000006r 1               .proc print_char
 000006r 1  A9 21                lda #33                 ; kod znaku, ktery se bude tisknout
@@ -1447,6 +1449,7 @@ Current file: subroutine_1.asm
 00000Ar 1  91 58                sta (88), y             ; tisk znaku na první místo na obrazovce
 00000Cr 1  60                   rts
 00000Dr 1               .endproc
+00000Dr 1               
 00000Dr 1               
 00000Dr 1               end:                            ; potrebujeme znat adresu konce kodoveho segmentu
 00000Dr 1               
