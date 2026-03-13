28a1277d6f4140dd60b77ccf5e2fd939fb056251
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-13 08:28:02 +0100

    Reformatted

--- LLVM_IR/simd_dot_product_2.c
+++ LLVM_IR/simd_dot_product_2.c
@@ -2,8 +2,8 @@ typedef float  f32x4 __attribute__((vector_size(16)));
 typedef double f64x2 __attribute__((vector_size(16)));
 typedef __fp16 f16x8 __attribute__((vector_size(16)));
 
-float dot_fp16(f16x8 x , f16x8 y) {
-    int      i;
+float dot_fp16(f16x8 x, f16x8 y) {
+    int i;
     __fp16 result = 0.0;
     for (i = 0; i < sizeof(f16x8) / sizeof(__fp16); i++) {
         result += x[i] * y[i];
@@ -12,7 +12,7 @@ float dot_fp16(f16x8 x , f16x8 y) {
 }
 
 float dot_float(f32x4 x, f32x4 y) {
-    int      i;
+    int   i;
     float result = 0.0;
     for (i = 0; i < sizeof(f32x4) / sizeof(float); i++) {
         result += x[i] * y[i];
@@ -21,7 +21,7 @@ float dot_float(f32x4 x, f32x4 y) {
 }
 
 double dot_double(f64x2 x, f64x2 y) {
-    int      i;
+    int    i;
     double result = 0.0;
     for (i = 0; i < sizeof(f64x2) / sizeof(double); i++) {
         result += x[i] * y[i];
