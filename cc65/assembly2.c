e045bc339d53d29b564cab5b8c2e8c2826173ed9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-16 08:11:44 +0100

    Reformatted

--- cc65/assembly2.c
+++ cc65/assembly2.c
@@ -1,9 +1,8 @@
-void main(void)
-{
+void main(void) {
     register unsigned char a;
 
-    __asm__ ("lda #1");
-    __asm__ ("clc");
-    __asm__ ("adc #2");
-    __asm__ ("sta %v", a);
+    __asm__("lda #1");
+    __asm__("clc");
+    __asm__("adc #2");
+    __asm__("sta %v", a);
 }
