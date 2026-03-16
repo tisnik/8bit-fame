de996cf9c53fce2fef74c3120ff35b96410d22f8
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-16 08:11:44 +0100

    Reformatted

--- cc65/assembly4.c
+++ cc65/assembly4.c
@@ -2,10 +2,9 @@ unsigned char a;
 unsigned char b;
 unsigned char c;
 
-void main(void)
-{
-    __asm__ ("lda %v", a);
-    __asm__ ("clc");
-    __asm__ ("adc %v", b);
-    __asm__ ("sta %v", c);
+void main(void) {
+    __asm__("lda %v", a);
+    __asm__("clc");
+    __asm__("adc %v", b);
+    __asm__("sta %v", c);
 }
