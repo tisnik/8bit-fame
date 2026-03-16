0cbd93f91a253d5209a7eb53c382b5861e5b91ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-16 08:11:44 +0100

    Reformatted

--- cc65/assembly3.c
+++ cc65/assembly3.c
@@ -1,12 +1,10 @@
-void main(void)
-{
+void main(void) {
     register unsigned char a;
     register unsigned char b;
     register unsigned char c;
 
-
-    __asm__ ("lda %v", a);
-    __asm__ ("clc");
-    __asm__ ("adc %v", b);
-    __asm__ ("sta %v", c);
+    __asm__("lda %v", a);
+    __asm__("clc");
+    __asm__("adc %v", b);
+    __asm__("sta %v", c);
 }
