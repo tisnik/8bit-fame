16bbe2ae9e5fb19795b50c3bb4f739ebd643972c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-17 08:54:53 +0100

    Reformatted

--- cc65/memset2.c
+++ cc65/memset2.c
@@ -1,7 +1,6 @@
 #include <stdint.h>
 
-void memset8(uint8_t * dest, uint8_t c, uint8_t n)
-{
+void memset8(uint8_t *dest, uint8_t c, uint8_t n) {
     register uint8_t i;
 
     for (i = 0; i < n; i++) {
@@ -9,11 +8,10 @@ void memset8(uint8_t * dest, uint8_t c, uint8_t n)
     }
 }
 
-int main(void)
-{
-    uint8_t *dest = (uint8_t *) 0x0600;
-    uint8_t bytes = 0xff;
-    uint8_t fill = 0x00;
+int main(void) {
+    uint8_t *dest = (uint8_t *)0x0600;
+    uint8_t  bytes = 0xff;
+    uint8_t  fill = 0x00;
     memset8(dest, fill, bytes);
 
     return 0;
