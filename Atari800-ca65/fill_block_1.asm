86ffa256593770065dcdfd4da7b2251190b5101c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-19 08:27:16 +0100

    Block fill, 1st variant

---
+++ Atari800-ca65/fill_block_1.asm
@@ -0,0 +1,33 @@
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        lda #33                 ; kod znaku, ktery se bude tisknout
+        ldx #40*5               ; počet zápisů
+        ldy #0                  ; offset
+clear:
+        sta (88), y             ; tisk znaku "A" na zvolené místo na obrazovce
+                                ; (adresa Video RAM je na adresách 88 a 89)
+        iny                     ; zvýšit hodnotu offsetu
+        dex                     ; zmenšit hodnotu počitadla
+        bne clear               ; skok, pokud X>0
+
+loop:   jmp loop
+end:
+.endproc
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   main::end - 1           ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
