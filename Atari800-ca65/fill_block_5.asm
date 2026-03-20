09b016bd6452c571db2de08491ca709c2e7e0e9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-20 08:39:27 +0100

    Filling block from bottom to top, assembly source

---
+++ Atari800-ca65/fill_block_5.asm
@@ -0,0 +1,32 @@
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        lda #33                 ; kod znaku, ktery se bude tisknout
+        ldy #0                  ; počitadlo zápisů
+clear:
+        sta (88), y             ; tisk znaku "A" na zvolené místo na obrazovce
+                                ; (adresa Video RAM je na adresách 88 a 89)
+        iny                     ; zvětšit hodnotu počitadla a offsetu
+	cpy #40*5               ; test na koncovou hodnotu počitadla
+        bne clear               ; skok, pokud Y>40*5
+
+loop:   jmp loop
+end:
+.endproc
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   main::end - 1           ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
