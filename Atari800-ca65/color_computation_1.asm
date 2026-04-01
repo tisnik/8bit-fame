336f13aacf1053617b9f9eb7dfcc2d50308f5c9a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-01 08:36:48 +0200

    Updated description

--- Atari800-ca65/color_computation_1.asm
+++ Atari800-ca65/color_computation_1.asm
@@ -1,3 +1,12 @@
+; ---------------------------------------------------------------------
+; Výpočet barvy pozadí na základě odstínu barvy a intenzity
+;
+; Tento zdrojový kód byl použit v článku:
+;
+; Programování pro osmibitová Atari: volání instrukcí procesoru MOS 6502
+; https://www.root.cz/clanky/programovani-pro-osmibitova-atari-volani-instrukci-procesoru-mos-6502/
+; ---------------------------------------------------------------------
+
 .include "atari.inc"
 
 HUE_COLOR_GRAY        = 16 * $0
