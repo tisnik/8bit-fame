b80c71b7053a3684f26e67d0c8d3c2c2dcf8c659
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-02 10:17:05 +0200

    Added description + link to article

--- Atari800-ca65/hex_number_7.asm
+++ Atari800-ca65/hex_number_7.asm
@@ -1,3 +1,12 @@
+; ---------------------------------------------------------------------
+; Realizace tisku dvouciferné hexadecimální hodnoty
+;
+; Tento zdrojový kód byl použit v článku:
+;
+; Programování pro osmibitová Atari: makra asm CA65, trik s instrukcí RTS
+; https://www.root.cz/clanky/programovani-pro-osmibitova-atari-makra-asm-ca65-trik-s-instrukci-rts/
+; ---------------------------------------------------------------------
+
 .include "atari.inc"
 
 .CODE
