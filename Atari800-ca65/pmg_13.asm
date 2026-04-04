ba964ffce642d6a23660635c9a0663065e6c514a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-04 09:05:46 +0200

    Vertical movement of the first player - not correct solution

---
+++ Atari800-ca65/pmg_13.asm
@@ -0,0 +1,120 @@
+; ---------------------------------------------------------------------
+; Vertikální posun spritu nahoru.
+; ---------------------------------------------------------------------
+
+.include "atari.inc"
+
+.CODE
+
+PLAYER_0_OFFSET = 512
+PLAYER_1_OFFSET = PLAYER_0_OFFSET + 128
+PLAYER_2_OFFSET = PLAYER_1_OFFSET + 128
+PLAYER_3_OFFSET = PLAYER_2_OFFSET + 128
+
+
+.proc main
+        lda #80                 ; horizontální pozice prvního hráče
+        sta HPOSP0              ; uložit do řídicího registru HPOSP0 na čipu GTIA
+
+        lda #100                ; horizontální pozice druhého hráče
+        sta HPOSP1              ; uložit do řídicího registru HPOSP1 na čipu GTIA
+
+        lda #120                ; horizontální pozice třetího hráče
+        sta HPOSP2              ; uložit do řídicího registru HPOSP2 na čipu GTIA
+
+        lda #140                ; horizontální pozice čtvrtého hráče
+        sta HPOSP3              ; uložit do řídicího registru HPOSP3 na čipu GTIA
+
+        lda #HUE_GREEN<<4 + 12  ; barva prvního hráče (odstín+intenzita)
+        sta PCOLR0              ; uložit do řídicího registru PCOLR0 na čipu GTIA
+
+        lda #HUE_YELLOW<<4 + 12 ; barva druhého hráče (odstín+intenzita)
+        sta PCOLR1              ; uložit do řídicího registru PCOLR1 na čipu GTIA
+
+        lda #HUE_MAGENTA<<4 + 12 ; barva třetího hráče (odstín+intenzita)
+        sta PCOLR2              ; uložit do řídicího registru PCOLR2 na čipu GTIA
+
+        lda #HUE_CYAN<<4 + 12   ; barva čtvrtého hráče (odstín+intenzita)
+        sta PCOLR3              ; uložit do řídicího registru PCOLR3 na čipu GTIA
+
+        lda #3                  ; bitové pole: povolení hráčů i střel
+        sta GRACTL              ; uložit do řídicího registru GRACTL na čipu GTIA
+
+        lda #0                  ; priorita hráčů a pozadí
+        sta GPRIOR              ; uložit do řídicího registru GPRIOR na čipu GTIA
+
+        lda #152                ; paměťová stránka číslo 152
+        sta PMBASE
+
+        addr = 152*256
+        ldx #8                  ; začneme na hodnotě o 1 vyšší
+next_line:
+        lda sprite-1, x         ; načíst
+        sta addr+PLAYER_1_OFFSET+50, x  ; uložit byte - druhý hráč
+        sta addr+PLAYER_2_OFFSET+50, x  ; uložit byte - třetí hráč
+        sta addr+PLAYER_3_OFFSET+50, x  ; uložit byte - čtvrtý hráč
+        dex                     ; snížit offset + nastavit příznaky
+        bne next_line           ; další byte spritu
+
+        lda #46                 ; povolení PMG DMA
+        sta SDMCTL
+
+	ldy #0                  ; počáteční vertikální pozice spritu
+
+loop:
+        jsr _wait_vsync         ; čekat na překreslení snímku
+        jsr _wait_vsync         ; dtto
+        jsr _wait_vsync         ; dtto
+
+        jsr draw_at_y
+	iny                     ; vertikální posun
+        jmp loop
+
+.endproc
+
+
+.proc  draw_at_y
+        addr = 152*256          ; adresa první stránky se sprity
+        tya
+	pha                     ; uložit Y na zásobník
+
+        ldx #8                  ; začneme na hodnotě o 1 vyšší
+next_line:
+        lda sprite-1, x         ; načíst
+        sta addr+PLAYER_0_OFFSET, y  ; uložit byte - první hráč
+	dey
+        dex                     ; snížit offset + nastavit příznaky
+        bne next_line           ; další byte spritu
+
+        pla
+	tay                     ; obnovit Y ze zásobníku
+        rts                     ; návrat z podprogramu
+.endproc
+
+
+.proc   _wait_vsync
+        ldx     RTCLOK+2        ; čekání na konec snímku
+@wt:    cpx     RTCLOK+2
+        beq     @wt
+        rts                     ; návrat z podprogramu
+.endproc
+
+
+; data
+sprite:   .byte 24, 60, 126, 219, 255, 36, 90, 165
+
+end:
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                 ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
