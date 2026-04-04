17de14c9c9165a3a43294250758c12a50e2b37e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-04 09:05:46 +0200

    Updated description (listing)

--- Atari800-ca65/subroutine_2_list.asm
+++ Atari800-ca65/subroutine_2_list.asm
@@ -2,6 +2,15 @@ ca65 V2.18 - Fedora 2.19-12.fc42
 Main file   : subroutine_2.asm
 Current file: subroutine_2.asm
 
+000000r 1               ; ---------------------------------------------------------------------
+000000r 1               ; Definice subrutin, volání subrutiny s předáním parametru v registru.
+000000r 1               ;
+000000r 1               ; Tento zdrojový kód byl použit v článku:
+000000r 1               ;
+000000r 1               ; Programování pro osmibitová Atari: volání instrukcí procesoru MOS 6502
+000000r 1               ; https://www.root.cz/clanky/programovani-pro-osmibitova-atari-volani-instrukci-procesoru-mos-6502/
+000000r 1               ; ---------------------------------------------------------------------
+000000r 1               
 000000r 1               .include "atari.inc"
 000000r 2               ;-------------------------------------------------------------------------
 000000r 2               ; Atari System Equates
