327ca52d7d97945d780dc7d20e942ca0729663d7
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-07 08:06:17 +0200

    Updated comments

--- Atari800-ca65/pmg_04.asm
+++ Atari800-ca65/pmg_04.asm
@@ -3,9 +3,9 @@
 ; Zákaz DMA. Postupná změna horizontální velikosti hráče.
 ;
 ; Tento zdrojový kód byl použit v článku:
-;
 ; 
-; 
+; Programování pro osmibitová Atari: čip GTIA a práce se sprity  
+; https://www.root.cz/clanky/programovani-pro-osmibitova-atari-cip-gtia-a-prace-se-sprity/
 ; ---------------------------------------------------------------------
 
 .include "atari.inc"
