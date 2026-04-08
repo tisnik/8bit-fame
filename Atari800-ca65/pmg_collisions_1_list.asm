c800464225f6d60272d23dca162f0f92f608e3a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-08 08:14:29 +0200

    Updated description

--- Atari800-ca65/pmg_collisions_1_list.asm
+++ Atari800-ca65/pmg_collisions_1_list.asm
@@ -3,7 +3,7 @@ Main file   : pmg_collisions_1.asm
 Current file: pmg_collisions_1.asm
 
 000000r 1               ; ---------------------------------------------------------------------
-000000r 1               ; Ovládání PMG joystickem.
+000000r 1               ; Ovládání PMG joystickem. Detekce kolize prvního hráče s dalšími hráči.
 000000r 1               ;
 000000r 1               ; Tento zdrojový kód byl použit v článku:
 000000r 1               ;
