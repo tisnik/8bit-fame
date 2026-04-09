0c07deed8623b58f3950a302635bed366effdb9b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-09 08:53:28 +0200

    Updated description

--- Atari800-ca65/pmg_stick_2.asm
+++ Atari800-ca65/pmg_stick_2.asm
@@ -1,5 +1,5 @@
 ; ---------------------------------------------------------------------
-; Ovládání PMG joystickem.
+; Ovládání PMG joystickem v horizontálním směru, zobrazení čtyř střel.
 ;
 ; Tento zdrojový kód byl použit v článku:
 ;
