b76111e7356475a9b804f2b71953d8e7ba6a7b50
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-12 09:19:31 +0200

    Setup ANTIC mode, example #3

---
+++ Atari800-ca65/antic_3.asm
@@ -0,0 +1,64 @@
+; ---------------------------------------------------------------------
+; ---------------------------------------------------------------------
+
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        lda #<dlist             ; nižší byte adresy display listu
+        sta SDLSTL
+        lda #>dlist             ; vyšší byte adresy display listu
+        sta SDLSTH
+
+        ldy #0                  ; počitadlo zápisů
+        lda #0                  ; kód vypisovaného znaku
+clear:
+        sta screen, y           ; tisk znaku na zvolené místo na obrazovce
+        clc
+        adc #1
+        iny                     ; zvětšit hodnotu počitadla a offsetu
+        cpy #40*6               ; test na koncovou hodnotu počitadla
+        bne clear               ; skok, pokud Y>40*6
+
+loop:   jmp loop
+.endproc
+
+dlist:
+.byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
+.byte DL_LMS+DL_CHR20x16x2      ; určení počáteční adresy obrazové paměti + jeden řádek režimu 7
+.byte <screen, >screen          ; počáteční adresa obrazové paměti 
+.byte DL_CHR20x16x2             ; jeden řádek textového režimu 7 (GR.2)
+.byte DL_CHR40x8x1              ; jeden řádek textového režimu 2 (GR.0)
+.byte DL_CHR20x8x2              ; jeden řádek textového režimu 6 (GR.1)
+.byte DL_CHR40x8x1              ; jeden řádek textového režimu 2 (GR.0)
+.byte DL_CHR20x16x2             ; jeden řádek textového režimu 7 (GR.2)
+.byte DL_CHR20x16x2             ; jeden řádek textového režimu 7 (GR.2)
+.byte DL_MAP40x8x4              ; jeden řádek grafického režimu 8 (GR.3)
+.byte DL_MAP40x8x4              ; jeden řádek grafického režimu 8 (GR.3)
+.byte DL_CHR20x16x2             ; jeden řádek textového režimu 7 (GR.2)
+.byte DL_CHR20x16x2             ; jeden řádek textového režimu 7 (GR.2)
+.byte DL_CHR20x16x2             ; jeden řádek textového režimu 7 (GR.2)
+.byte DL_CHR40x8x1              ; jeden řádek textového režimu 2 (GR.0)
+.byte DL_JVB, <dlist, >dlist    ; skok na začátek display listu
+
+end:
+
+.BSS
+screen: .res 20*24
+
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
