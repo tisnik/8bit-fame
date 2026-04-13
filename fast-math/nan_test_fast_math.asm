caa6bb5fd95e21e5238ece3176625f0775c0ab3e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-13 11:01:38 +0200

    Testing fast math and NaN handling

---
+++ fast-math/nan_test_fast_math.asm
@@ -0,0 +1,16 @@
+	.file	"nan_test.c"
+	.intel_syntax noprefix
+	.text
+	.p2align 4
+	.globl	nan_test
+	.type	nan_test, @function
+nan_test:
+.LFB0:
+	.cfi_startproc
+	xor	eax, eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	nan_test, .-nan_test
+	.ident	"GCC: (GNU) 15.2.1 20251111 (Red Hat 15.2.1-4)"
+	.section	.note.GNU-stack,"",@progbits
