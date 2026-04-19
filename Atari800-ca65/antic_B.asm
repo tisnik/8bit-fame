35ad87b7e00414d26fec35735477fe42c0bec9c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-19 09:09:14 +0200

    Graphics mode 80x48x2

---
+++ Atari800-ca65/antic_B.asm
@@ -0,0 +1,73 @@
+; ---------------------------------------------------------------------
+; ---------------------------------------------------------------------
+
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        lda #<dlist             ; nižší byte adresy display listu
+        sta SDLSTL
+        lda #>dlist             ; vyšší byte adresy display listu
+        sta SDLSTH
+
+        ldy #0                  ; počitadlo zápisů
+clear:
+        tya                     ; kód tištěných pixelů
+        sta screen, y           ; tisk pixelu na zvolené místo na obrazovce
+        iny                     ; zvětšit hodnotu počitadla a offsetu
+        bne clear               ; skok
+
+loop:   jmp loop
+.endproc
+
+dlist:
+.byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
+.byte DL_LMS+DL_MAP80x4x2       ; určení počáteční adresy obrazové paměti + jeden řádek režimu 9 (GR.4)
+.byte <screen, >screen          ; počáteční adresa obrazové paměti 
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_JVB, <dlist, >dlist    ; skok na začátek display listu
+
+end:
+
+.BSS
+screen: .res 20*24
+
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
