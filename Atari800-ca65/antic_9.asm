337e0d0e52f34bb16b0cdce53423194ea4c32d07
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-21 09:29:29 +0200

    Added link to article where this example is used

--- Atari800-ca65/antic_9.asm
+++ Atari800-ca65/antic_9.asm
@@ -1,4 +1,10 @@
 ; ---------------------------------------------------------------------
+; Výpis 256 kódů znaků v režimu GR.12. 
+; 
+; Tento zdrojový kód byl použit v článku:
+;
+; Praktické použití textových režimů nabízených čipem ANTIC
+; https://www.root.cz/clanky/prakticke-pouziti-textovych-rezimu-nabizenych-cipem-antic/
 ; ---------------------------------------------------------------------
 
 .include "atari.inc"
