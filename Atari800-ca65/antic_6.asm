ca1287ec0af3da6af517f7493b225c434248831f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-04-21 09:29:29 +0200

    Added link to article where this example is used

--- Atari800-ca65/antic_6.asm
+++ Atari800-ca65/antic_6.asm
@@ -1,4 +1,11 @@
 ; ---------------------------------------------------------------------
+; Výpis všech 128 znaků v přímé i inverzní podobě v režimu GR.0.
+; Náhodná stránka pro znakovou sadu.
+; 
+; Tento zdrojový kód byl použit v článku:
+;
+; Praktické použití textových režimů nabízených čipem ANTIC
+; https://www.root.cz/clanky/prakticke-pouziti-textovych-rezimu-nabizenych-cipem-antic/
 ; ---------------------------------------------------------------------
 
 .include "atari.inc"
