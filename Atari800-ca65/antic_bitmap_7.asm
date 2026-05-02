c5e5e9c2b67ad1f1fb85d45578497bb9333e5d3e
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-02 09:41:03 +0200

    Bitmap modes supported by ANTIC, article #7

---
+++ Atari800-ca65/antic_bitmap_7.asm
@@ -0,0 +1,63 @@
+; ---------------------------------------------------------------------
+; Grafický režim 320x192 se dvěma barvami.
+; Zarovnání bitmapy do bloků 4096 bajtů.
+; Korektní display list
+; 
+; Tento zdrojový kód byl použit v článku:
+;
+; ---------------------------------------------------------------------
+
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        lda #0                  ; kod barvy
+        sta COLOR1              ; ulozit do registru COLOR4
+        lda #14                 ; kod barvy
+        sta COLOR2              ; ulozit do registru COLOR4
+
+        lda #<dlist             ; nižší byte adresy display listu
+        sta SDLSTL
+        lda #>dlist             ; vyšší byte adresy display listu
+        sta SDLSTH
+
+loop:   jmp loop
+.endproc
+
+dlist:
+.byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
+.byte DL_LMS+DL_MAP320x1x1      ; určení počáteční adresy obrazové paměti + jeden řádek režimu F (GR.8)
+.byte <screen1, >screen1        ; počáteční adresa obrazové paměti (první blok)
+.res 95, DL_MAP320x1x1          ; opakovat řádky grafického režimu F (GR.8)
+.byte DL_LMS+DL_MAP320x1x1      ; určení počáteční adresy obrazové paměti + jeden řádek režimu F (GR.8)
+.byte <screen2, >screen2        ; počáteční adresa obrazové paměti (druhý blok)
+.res 95, DL_MAP320x1x1          ; opakovat řádky grafického režimu F (GR.8)
+.byte DL_JVB, <dlist, >dlist    ; skok na začátek display listu
+
+
+.segment "IMAGESEG1"
+.org $3000
+screen1:
+.incbin "image_320x192_1.bin"
+
+.segment "IMAGESEG2"
+.org $4000
+screen2:
+.incbin "image_320x192_2.bin"
+
+end:
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                 ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
