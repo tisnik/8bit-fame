89038a47a77d497063a8c02f8fdae2b5b2544687
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-02 09:41:03 +0200

    Bitmap modes supported by ANTIC, article #1

---
+++ Atari800-ca65/antic_bitmap_1.asm
@@ -0,0 +1,98 @@
+; ---------------------------------------------------------------------
+; Grafický režim 80x48 se dvěma barvami.
+; 
+; Tento zdrojový kód byl použit v článku:
+;
+; ---------------------------------------------------------------------
+
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        lda #0                  ; kod barvy
+        sta COLOR0              ; ulozit do registru COLOR2
+
+        lda #10                 ; kod barvy
+        sta COLOR4              ; ulozit do registru COLOR4
+
+        lda #<dlist             ; nižší byte adresy display listu
+        sta SDLSTL
+        lda #>dlist             ; vyšší byte adresy display listu
+        sta SDLSTH
+
+loop:   jmp loop
+.endproc
+
+dlist:
+.byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
+.byte DL_LMS+DL_MAP80x4x2       ; určení počáteční adresy obrazové paměti + jeden řádek režimu 9 (GR.4)
+.byte <screen, >screen          ; počáteční adresa obrazové paměti 
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_MAP80x4x2              ; jeden řádek grafického režimu 9 (GR.4)
+.byte DL_JVB, <dlist, >dlist    ; skok na začátek display listu
+
+screen:
+.include "image_80x48.asm"
+end:
+
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
