fc90a1471a7a27391f315725ae59799e2844bceb
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-02 09:41:03 +0200

    Bitmap modes supported by ANTIC, article #4

---
+++ Atari800-ca65/antic_bitmap_4.asm
@@ -0,0 +1,52 @@
+; ---------------------------------------------------------------------
+; Grafický režim 160x192 se dvěma barvami.
+; 
+; Tento zdrojový kód byl použit v článku:
+;
+; ---------------------------------------------------------------------
+
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        lda #0                  ; kod barvy
+        sta COLOR0              ; ulozit do registru COLOR2
+
+        lda #10                 ; kod barvy
+        sta COLOR4              ; ulozit do registru COLOR4
+
+        lda #<dlist             ; nižší byte adresy display listu
+        sta SDLSTL
+        lda #>dlist             ; vyšší byte adresy display listu
+        sta SDLSTH
+
+loop:   jmp loop
+.endproc
+
+dlist:
+.byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
+.byte DL_LMS+DL_MAP160x1x2      ; určení počáteční adresy obrazové paměti + jeden řádek režimu C (GR.14)
+.byte <screen, >screen          ; počáteční adresa obrazové paměti 
+.res 191, DL_MAP160x1x2          ; opakovat řádky grafického režimu C (GR.14)
+.byte DL_JVB, <dlist, >dlist    ; skok na začátek display listu
+
+screen:
+.include "image_160x192.asm"
+end:
+
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
