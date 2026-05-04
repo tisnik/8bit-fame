a4a9172b85c63381280218129ac88a0eb2a74bd6
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-04 08:46:51 +0200

    Updated description in listing file

--- Atari800-ca65/antic_8_list.asm
+++ Atari800-ca65/antic_8_list.asm
@@ -3,6 +3,13 @@ Main file   : antic_8.asm
 Current file: antic_8.asm
 
 000000r 1               ; ---------------------------------------------------------------------
+000000r 1               ; Výpis všech 64 znaků ve čtyřech barvách v režimu GR.1.
+000000r 1               ; Použití prvních 64 znaků z ATASCII.
+000000r 1               ;
+000000r 1               ; Tento zdrojový kód byl použit v článku:
+000000r 1               ;
+000000r 1               ; Praktické použití textových režimů nabízených čipem ANTIC
+000000r 1               ; https://www.root.cz/clanky/prakticke-pouziti-textovych-rezimu-nabizenych-cipem-antic/
 000000r 1               ; ---------------------------------------------------------------------
 000000r 1               
 000000r 1               .include "atari.inc"
