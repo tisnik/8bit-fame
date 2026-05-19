f96b7326a407265ec6918a3cd5cb0fca6cd4f7fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-05-19 11:52:23 +0200

    Updated

--- Atari800-ca65/antic_vbi_dli_1_list.asm
+++ Atari800-ca65/antic_vbi_dli_1_list.asm
@@ -6,6 +6,11 @@ Current file: antic_vbi_dli_1.asm
 000000r 1               ; Ovládání PMG joystickem v horizontálním směru.
 000000r 1               ; Změna pozice PMG realizovaná ve VBI.
 000000r 1               ; Změna barvy herního pole realizovaná v DLI.
+000000r 1               ;
+000000r 1               ; Tento zdrojový kód byl použit v článku:
+000000r 1               ;
+000000r 1               ; VBI (Vertical Blank Interrupt) na osmibitových mikropočítačích Atari
+000000r 1               ; https://www.root.cz/clanky/vbi-vertical-blank-interrupt-na-osmibitovych-mikropocitacich-atari/
 000000r 1               ; ---------------------------------------------------------------------
 000000r 1               
 000000r 1               .include "atari.inc"
