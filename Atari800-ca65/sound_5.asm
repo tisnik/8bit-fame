8f92939906b13661ba40549289066f7aece35ce9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-17 09:19:19 +0200

    Sound generated by POKEY chip, example #5

---
+++ Atari800-ca65/sound_5.asm
@@ -0,0 +1,68 @@
+; ---------------------------------------------------------------------
+; Generování zvuků čipem POKEY.
+; ---------------------------------------------------------------------
+
+.include "atari.inc"
+
+.CODE
+
+PURE_TONE_MASK   = %10100000
+INPUT_FREQ_15KHZ = %00000001
+
+.proc main
+        jsr get_key             ; čekání na stisk klávesy
+
+        lda #PURE_TONE_MASK+10  ; hlasitost (0-15)
+        sta AUDC1               ; zápis do registru řízení hlasitosti pro zvukový kanál číslo 1 čipu POKEY
+        sta AUDC2               ; zápis do registru řízení hlasitosti pro zvukový kanál číslo 2 čipu POKEY
+
+        lda #255                ; dělič základní frekvence
+        sta AUDF1               ; zápis do registru děliče frekvence pro zvukový kanál číslo 1 čipu POKEY
+        lda #254                ; dělič základní frekvence
+        sta AUDF2               ; zápis do registru děliče frekvence pro zvukový kanál číslo 2 čipu POKEY
+
+        lda #00                 ; základní tón odvozený od 64kHz
+        sta AUDCTL              ; zápis do řídicího registru čipu POKEY
+
+        jsr get_key             ; čekání na stisk klávesy
+
+        lda #INPUT_FREQ_15KHZ   ; základní tón odvozený od 15kHz
+        sta AUDCTL              ; zápis do řídicího registru čipu POKEY
+
+        jsr get_key             ; čekání na stisk klávesy
+
+        lda #0                  ; vypnutí zvuku (nulová hlasitost)
+        sta AUDC1               ; zápis do registru řízení hlasitosti pro zvukový kanál číslo 1 čipu POKEY
+
+loop:   jmp loop
+.endproc
+
+
+; ---------------------------------------------------------------------
+; čekání na stisk klávesy
+; ---------------------------------------------------------------------
+.proc get_key
+        KBHANDLER = $e424       ; rutina pro cteni klavesy
+        lda KBHANDLER+1         ; cteni horni casti adresy ulozene v ROM
+        pha                     ; ulozeni na zasobnik
+        lda KBHANDLER           ; cteni dolni casti adresy ulozene v ROM
+        pha                     ; ulozeni na zasobnik
+        rts                     ; vyber adresy ze zasobniku + skok
+                                ; zde neni nutne mit RTS
+.endproc
+
+
+end:
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                 ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
