eeb1365956e748bdde9d4d647dac1822083ee3f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-18 09:28:07 +0200

    Listing produced by assembler

--- Atari800-ca65/antic_bitmap_7_list.asm
+++ Atari800-ca65/antic_bitmap_7_list.asm
@@ -11,6 +11,9 @@ Current file: antic_bitmap_7.asm
 000000r 1               ;
 000000r 1               ; Praktické použití grafických režimů nabízených čipem ANTIC
 000000r 1               ; https://www.root.cz/clanky/prakticke-pouziti-grafickych-rezimu-nabizenych-cipem-antic/
+000000r 1               ;
+000000r 1               ; Překlad do formátu xex:
+000000r 1               ; make antic_bitmap_1.xex
 000000r 1               ; ---------------------------------------------------------------------
 000000r 1               
 000000r 1               .include "atari.inc"
