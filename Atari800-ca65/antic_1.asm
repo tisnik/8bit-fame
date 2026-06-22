abd62481e5aaba8fa5b8821bdb080be9afa5c03f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-22 09:36:56 +0200

    Updated comments

--- Atari800-ca65/antic_1.asm
+++ Atari800-ca65/antic_1.asm
@@ -34,7 +34,7 @@ clear:
 loop:   jmp loop
 .endproc
 
-dlist:
+dlist:                          ; definice display listu
 .byte 112, 112, 112
 .byte 64+7, <screen, >screen
 .byte 7, 7, 7, 7
@@ -45,19 +45,20 @@ dlist:
 end:
 
 .BSS
-screen: .res 20*24
+screen: .res 20*24              ; rezervace prostoru pro video paměť
 
 
 
 .segment "EXEHDR"
-.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
-.word   main                    ; zacatek kodoveho segmentu
-.word   end - 1                ; konec kodoveho segmentu
+.word   $ffff                   ; úvodní sekvence bajtů v souboru ve formátu XEX
+.word   main                    ; začátek kódového segmentu
+.word   end - 1                 ; konec kódového segmentu
 
 
-.segment "AUTOSTRT"             ; segment s pocatecni adresou
-.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+
+.segment "AUTOSTRT"             ; segment s počáteční adresou
+.word   RUNAD                   ; naplní se pouze adresy RUNAD a RUNAD+1
 .word   RUNAD+1
-.word   main                    ; adresa vstupniho bodu do programu
+.word   main                    ; adresa vstupního bodu do programu
 
 ; finito
