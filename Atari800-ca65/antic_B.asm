fde51f7fafe0e826440f25df6fdef7ee5d9428c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-23 09:01:17 +0200

    Updated comments

--- Atari800-ca65/antic_B.asm
+++ Atari800-ca65/antic_B.asm
@@ -63,20 +63,22 @@ dlist:
 
 end:
 
+
 .BSS
-screen: .res 10*48
+screen: .res 10*48              ; rezervace prostoru pro video paměť
 
 
 
 .segment "EXEHDR"
-.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
-.word   main                    ; zacatek kodoveho segmentu
-.word   end - 1                ; konec kodoveho segmentu
+.word   $ffff                   ; úvodní sekvence bajtů v souboru ve formátu XEX
+.word   main                    ; začátek kódového segmentu
+.word   end - 1                 ; konec kódového segmentu
 
 
-.segment "AUTOSTRT"             ; segment s pocatecni adresou
-.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+
+.segment "AUTOSTRT"             ; segment s počáteční adresou
+.word   RUNAD                   ; naplní se pouze adresy RUNAD a RUNAD+1
 .word   RUNAD+1
-.word   main                    ; adresa vstupniho bodu do programu
+.word   main                    ; adresa vstupního bodu do programu
 
 ; finito
