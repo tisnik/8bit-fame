8ac7ec044016d389ab4c4528942be49dcda46e83
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-25 10:37:58 +0200

    Paddle controller (stub w/o paddle)

---
+++ Atari800-ca65/paddle_1.asm
@@ -0,0 +1,90 @@
+.include "atari.inc"
+
+.CODE
+
+PLAYER_0_OFFSET = 512
+
+
+; ---------------------------------------------------------------------
+; vstupní bod do programu
+; ---------------------------------------------------------------------
+.proc main
+        lda #80                 ; horizontální pozice prvního hráče
+        sta HPOSP0              ; uložit do řídicího registru HPOSP0 na čipu GTIA
+
+        lda #HUE_GREEN<<4 + 12  ; barva prvního hráče (odstín+intenzita)
+        sta PCOLR0              ; uložit do řídicího registru PCOLR0 na čipu GTIA
+
+        lda #3                  ; bitové pole: povolení hráčů i střel
+        sta GRACTL              ; uložit do řídicího registru GRACTL na čipu GTIA
+
+        lda #0                  ; priorita hráčů a pozadí
+        sta GPRIOR              ; uložit do řídicího registru GPRIOR na čipu GTIA
+
+        lda #152                ; paměťová stránka číslo 152
+        sta PMBASE
+
+        addr = 152*256
+        ldx #8                  ; začneme na hodnotě o 1 vyšší
+next_line:
+        lda sprite-1, x         ; načíst
+        sta addr+PLAYER_0_OFFSET+50, x  ; uložit byte - první hráč
+        dex                     ; snížit offset + nastavit příznaky
+        bne next_line           ; další byte spritu
+
+        lda #46                 ; povolení PMG DMA
+        sta SDMCTL
+
+        ; nastavit vektor pro odloženou VBI
+        lda #>horizontal_movement
+        sta VVBLKD+1
+        lda #<horizontal_movement
+        sta VVBLKD
+loop:
+        jmp loop                ; program vlastně nice nedělá - jen cyklí!
+.endproc
+
+
+; ---------------------------------------------------------------------
+; subrutina pro obsluhu VBI
+; ---------------------------------------------------------------------
+.proc   horizontal_movement
+        ldx x_pos               ; původní pozice prvního hráče
+
+        lda STICK0              ; čtení joysticku
+        cmp #11                 ; je nakloněn doleva?
+        bne not_left
+        dex                     ; posun hráče doleva
+not_left:
+        cmp #7                  ; je nakloněn doprava?
+        bne not_right
+        inx                     ; posun hráče doprava
+not_right:
+        stx HPOSP0              ; změna pozice prvního hráče
+        stx x_pos               ; zapamatovat si pozici prvního hráče
+        jmp XITVBV              ; zpracovat zbytek odloženého VBLANKu
+        rts
+.endproc
+
+
+; data
+sprite:   .byte 24, 60, 126, 219, 255, 36, 90, 165
+
+; horizontální pozice hráče
+x_pos:    .byte 80
+
+end:
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                 ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
