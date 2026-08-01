94f793098971152e449e4084d1039b9421901a74
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-01 11:00:44 +0200

    Put (write) block via CIO

---
+++ Atari800-ca65/cio_put_block.asm
@@ -0,0 +1,47 @@
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        ldx #0                  ; offset odvozený od čísla IOCB kanálu (nutno násobit šestnácti)
+        lda #PUTCHR             ; prováděná operace
+        sta ICCOM, x
+
+        ; adresa zapisovaného bloku
+        lda #<message
+        sta ICBAL, x
+        lda #>message
+        sta ICBAH, x
+
+        ; nastavení počtu zapisovaných bajtů
+        lda #(end_of_message-message)
+        sta ICBLL, x
+        lda #0
+        sta ICBLH, x
+
+        ; zavolání rutiny pro CIO
+        jsr CIOV
+
+loop:
+        jmp loop                ; program vlastně nice nedělá - jen cyklí!
+.endproc
+
+message: .byte "Hello, world!"
+end_of_message:
+
+end:
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                 ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
