f209d9e7f560f5499d213a633c8ed5abac4bf6be
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-01 11:00:44 +0200

    Call beep implemented in Atari OS

---
+++ Atari800-ca65/beep.asm
@@ -0,0 +1,27 @@
+.include "atari.inc"
+
+.CODE
+
+
+.proc main
+        jsr #$f058
+loop:
+        jmp loop                ; program vlastně nice nedělá - jen cyklí!
+.endproc
+
+
+end:
+
+
+.segment "EXEHDR"
+.word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
+.word   main                    ; zacatek kodoveho segmentu
+.word   end - 1                 ; konec kodoveho segmentu
+
+
+.segment "AUTOSTRT"             ; segment s pocatecni adresou
+.word   RUNAD                   ; naplni se pouze adresy RUNAD a RUNAD+1
+.word   RUNAD+1
+.word   main                    ; adresa vstupniho bodu do programu
+
+; finito
