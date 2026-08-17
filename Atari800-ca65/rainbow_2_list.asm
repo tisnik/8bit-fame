8f8776af303887d4d0314e9a22b4aca69acf4b7a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-17 09:42:02 +0200

    Updated comments

--- Atari800-ca65/rainbow_2_list.asm
+++ Atari800-ca65/rainbow_2_list.asm
@@ -1440,6 +1440,9 @@ Current file: rainbow_2.asm
 000000r 1               .CODE
 000000r 1               
 000000r 1               
+000000r 1               ; ---------------------------------------------------------------------
+000000r 1               ; vstupní bod do programu
+000000r 1               ; ---------------------------------------------------------------------
 000000r 1               .proc main
 000000r 1  A9 rr                lda #<dlist             ; nižší byte adresy display listu
 000002r 1  8D 30 02             sta SDLSTL
@@ -1478,6 +1481,9 @@ Current file: rainbow_2.asm
 00002Er 1  40                   rti                     ; návrat z DLI
 00002Fr 1               
 00002Fr 1               
+00002Fr 1               ; ---------------------------------------------------------------------
+00002Fr 1               ; display list
+00002Fr 1               ; ---------------------------------------------------------------------
 00002Fr 1               dlist:
 00002Fr 1  70 70 F0     .byte DL_BLK8, DL_BLK8, DL_DLI+DL_BLK8 ; 3x8=24 prázdných obrazových řádků
 000032r 1  42           .byte DL_LMS+DL_CHR40x8x1       ; určení počáteční adresy obrazové paměti + jeden řádek režimu 2 (GR.0)
@@ -1523,6 +1529,9 @@ Current file: rainbow_2.asm
 0003C0r 1               
 0003C0r 1               
 0003C0r 1               
+0003C0r 1               ; ---------------------------------------------------------------------
+0003C0r 1               ; definice segmentů vyžadovaných formátem XEX
+0003C0r 1               ; ---------------------------------------------------------------------
 0003C0r 1               .segment "EXEHDR"
 000000r 1  FF FF        .word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
 000002r 1  rr rr        .word   main                    ; zacatek kodoveho segmentu
