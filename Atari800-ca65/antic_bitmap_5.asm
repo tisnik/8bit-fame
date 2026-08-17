d079315453d973c2423191108d82c018eee0464b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-17 09:42:01 +0200

    Updated comments

--- Atari800-ca65/antic_bitmap_5.asm
+++ Atari800-ca65/antic_bitmap_5.asm
@@ -15,6 +15,9 @@
 .CODE
 
 
+; ---------------------------------------------------------------------
+; vstupní bod do programu
+; ---------------------------------------------------------------------
 .proc main
         lda #0                  ; kod barvy
         sta COLOR1              ; ulozit do registru COLOR4
@@ -29,6 +32,11 @@
 loop:   jmp loop
 .endproc
 
+
+
+; ---------------------------------------------------------------------
+; display list
+; ---------------------------------------------------------------------
 dlist:
 .byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
 .byte DL_LMS+DL_MAP320x1x1      ; určení počáteční adresy obrazové paměti + jeden řádek režimu F (GR.8)
@@ -42,6 +50,9 @@ end:
 
 
 
+; ---------------------------------------------------------------------
+; definice segmentů vyžadovaných formátem XEX
+; ---------------------------------------------------------------------
 .segment "EXEHDR"
 .word   $ffff                   ; úvodní sekvence bajtů v souboru ve formátu XEX
 .word   main                    ; začátek kódového segmentu
