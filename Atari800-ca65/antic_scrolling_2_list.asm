4373adc98edf15490d740f258393587ea4c28f8b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-17 09:42:02 +0200

    Updated comments

--- Atari800-ca65/antic_scrolling_2_list.asm
+++ Atari800-ca65/antic_scrolling_2_list.asm
@@ -1563,6 +1563,10 @@ Current file: antic_scrolling_2.asm
 00006Dr 1               .endproc
 00006Dr 1               
 00006Dr 1               
+00006Dr 1               
+00006Dr 1               ; ---------------------------------------------------------------------
+00006Dr 1               ; display list
+00006Dr 1               ; ---------------------------------------------------------------------
 00006Dr 1               dlist:
 00006Dr 1  70 70 70     .byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
 000070r 1  42           .byte DL_LMS+DL_CHR40x8x1       ; určení počáteční adresy obrazové paměti + jeden řádek režimu 2 (GR.0)
@@ -1612,6 +1616,11 @@ Current file: antic_scrolling_2.asm
 00005Cr 1  xx xx xx xx  
 000060r 1  xx xx xx xx  
 0003C0r 1               
+0003C0r 1               
+0003C0r 1               
+0003C0r 1               ; ---------------------------------------------------------------------
+0003C0r 1               ; definice segmentů vyžadovaných formátem XEX
+0003C0r 1               ; ---------------------------------------------------------------------
 0003C0r 1               .segment "EXEHDR"
 000000r 1  FF FF        .word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
 000002r 1  rr rr        .word   main                    ; zacatek kodoveho segmentu
