2cbb1d2d68ff494b53adf8fac7adedf169f397e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-17 09:42:02 +0200

    Updated comments

--- Atari800-ca65/antic_dli_2.asm
+++ Atari800-ca65/antic_dli_2.asm
@@ -13,6 +13,9 @@
 .CODE
 
 
+; ---------------------------------------------------------------------
+; vstupní bod do programu
+; ---------------------------------------------------------------------
 .proc main
         lda #<dlist             ; nižší byte adresy display listu
         sta SDLSTL
@@ -54,6 +57,10 @@ dli:
         rti                     ; návrat z DLI
 
 
+
+; ---------------------------------------------------------------------
+; display list
+; ---------------------------------------------------------------------
 dlist:
 .byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
 .byte DL_LMS+DL_CHR40x8x1       ; určení počáteční adresy obrazové paměti + jeden řádek režimu 2 (GR.0)
@@ -70,6 +77,9 @@ screen: .res 40*24
 
 
 
+; ---------------------------------------------------------------------
+; definice segmentů vyžadovaných formátem XEX
+; ---------------------------------------------------------------------
 .segment "EXEHDR"
 .word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
 .word   main                    ; zacatek kodoveho segmentu
