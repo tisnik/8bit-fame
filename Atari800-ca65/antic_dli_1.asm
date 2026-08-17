def728b042f56ecd01a3c75a841e7f2e1a20fccc
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-08-17 09:42:02 +0200

    Updated comments

--- Atari800-ca65/antic_dli_1.asm
+++ Atari800-ca65/antic_dli_1.asm
@@ -12,6 +12,9 @@
 .CODE
 
 
+; ---------------------------------------------------------------------
+; vstupní bod do programu
+; ---------------------------------------------------------------------
 .proc main
         lda #<dlist             ; nižší byte adresy display listu
         sta SDLSTL
@@ -29,6 +32,11 @@ clear:
 loop:   jmp loop
 .endproc
 
+
+
+; ---------------------------------------------------------------------
+; display list
+; ---------------------------------------------------------------------
 dlist:
 .byte DL_BLK8, DL_BLK8, DL_BLK8 ; 3x8=24 prázdných obrazových řádků
 .byte DL_LMS+DL_CHR40x8x1       ; určení počáteční adresy obrazové paměti + jeden řádek režimu 2 (GR.0)
@@ -43,6 +51,9 @@ screen: .res 40*24
 
 
 
+; ---------------------------------------------------------------------
+; definice segmentů vyžadovaných formátem XEX
+; ---------------------------------------------------------------------
 .segment "EXEHDR"
 .word   $ffff                   ; uvodni sekvence bajtu v souboru XEX
 .word   main                    ; zacatek kodoveho segmentu
