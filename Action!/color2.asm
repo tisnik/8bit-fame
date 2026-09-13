49a14d7e0c7df2c80cbc9f8b0997caee74f8bd61
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-13 10:28:01 +0200

    Compiled example color2.act

---
+++ Action!/color2.asm
@@ -0,0 +1,20 @@
+; da65 V2.18 - Fedora 2.19-12.fc42
+; Created:    2026-09-05 11:29:24
+; Input file: C222
+; Page:       1
+
+
+        .setcpu "6502"
+
+        lda     #$2A
+        sta     $02C6
+        rts
+
+        rts
+
+        .byte   $E2
+        .byte   $02
+        .byte   $E3
+        .byte   $02
+        ror     a
+        .byte   $0E
