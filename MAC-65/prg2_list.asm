24c046f09b282fae61b2386030499aae06553883
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-14 10:42:06 +0200

    Program listing

---
+++ MAC-65/prg2_list.asm
@@ -0,0 +1,18 @@
+PAGE 1    
+                                                                 
+
+
+
+0000            01       .OPT OBJ
+0000            1000     *=  $9000
+9000 A92A       1001     LDA #42
+9002 8DC802     1002     STA 712
+9005 00         1003     BRK 
+
+ASSEMBLY ERRORS: 0   34065 BYTES FREE
+PAGE 2    
+SYMBOLS                                                          
+
+
+
+
