883bcfc312eb444393e72b41319c20c6c2095eeb
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-14 10:42:06 +0200

    Program listing

---
+++ MAC-65/prg3_list.asm
@@ -0,0 +1,22 @@
+PAGE 1    
+                                                                 
+
+
+
+0000 18         10       CLC 
+0001 9001       20       BCC NEXT
+0003 00         30       BRK 
+0004 A900       40 NEXT  LDA #0
+0006 8DC602     50       STA 710
+0009 F031       60 REPT  BEQ 60
+000B 00         70       BRK 
+
+ASSEMBLY ERRORS: 0   34024 BYTES FREE
+PAGE 2    
+SYMBOLS                                                          
+
+
+
+
+ 0004 NEXT          
+ 0009 REPT          
