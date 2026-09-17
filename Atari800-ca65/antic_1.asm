a621fea50f72ac6e63bbc729922d659b0145f03f
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-17 14:07:03 +0200

    Added link to series of articles

--- Atari800-ca65/antic_1.asm
+++ Atari800-ca65/antic_1.asm
@@ -6,6 +6,9 @@
 ; Grafika na osmibitových Atari: grafický koprocesor ANTIC
 ; https://www.root.cz/clanky/grafika-na-osmibitovych-atari-graficky-koprocesor-antic/
 ;
+; Seriál "Programování pro osmibitová Atari"
+; https://www.root.cz/serialy/programovani-pro-osmibitova-atari/
+;
 ; Překlad do formátu xex:
 ; make antic_1.xex
 ; ---------------------------------------------------------------------
